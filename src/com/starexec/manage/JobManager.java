323084019f4353ea5fd9a45a7e0329f27fcdcf06
cpalmer <clifton-palmer@uiowa.edu>
2011-08-28 16:13:41 +0000

    Moved out jobscript, modifying jobmanager but can't log yet so this doesn't work.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/starexec@7095 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/com/starexec/manage/JobManager.java
+++ src/com/starexec/manage/JobManager.java
@@ -125,14 +125,25 @@ public abstract class JobManager {
 	 * 
 	 */
 	private static void buildJob() throws IOException {
-		/*
-		 * NOTE: uses the 'wget' command in script to call servlet that handles interface to SQL server on starexec
-		 * Ex: wget "starexec/starexec/Results?type=job&id=2&node=starexec1&status=done" -O /dev/null --spider
-		 * 
-		 * Job has the following statuses : 0 (done), 1 (running), 2 (enqueued), 3 (error), 4 (frozen)
-		 * 
-		 */
-		// Open a file on the shared space and write the job script to it.
+		// Reads in the bash script and inserts variables
+		BufferedReader reader = null;
+		String ls = System.getProperty("line.separator");
+		String script = "";
+		
+		try {
+			reader = new BufferedReader(new FileReader(new File("/starexec/src/jobscript")));
+			String line = null;
+			StringBuilder str = new StringBuilder();
+			while( (line = reader.readLine()) != null ) {
+				str.append(line + ls);
+			}
+			script = str.toString();
+		} catch(IOException e) {
+			if(reader != null) reader.close();
+			throw e;
+		}
+		
+		// Opens a file on the shared space and writes the empty job script to it.
 		String filePath = curJobPath;
 		File f = new File(filePath);
 		
@@ -143,7 +154,7 @@ public abstract class JobManager {
 			throw new IOException("Can't change owner's executable permissions on file " + curJobPath);
 		FileWriter out = new FileWriter(f);
 		
-		// Create a new job TO object
+		// Creates a new job TO object
 		jobRecord = new Job();
 		jobRecord.setJobId(curJID);
 		jobRecord.setUserId(curJob.getUser().getUserId());
@@ -152,131 +163,9 @@ public abstract class JobManager {
 		jobRecord.setNode(null);
 		jobRecord.setStatus(R.JOB_STATUS_ENQUEUED);
 		jobRecord.setTimeout(Long.MAX_VALUE); // Might have to be user-set. 
-			
 		
-		// Write the ugly, ugly bash file. Shouldn't be hardcoded like this.
-		out.write("#!/bin/bash\n"
-				+ "# This submits a test bash job to the SGE\n"
-				+ "#$ -j y\n"
-				+ "#$ -o /dev/null\n"
-				+ "#$ -S /bin/bash \n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "# Setup\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "ROOT='/export/starexec'\n"
-				+ "WDIR=$ROOT/workspace\n"
-				+ "SHR=/home/starexec\n"
-				+ "T=\"date +%s.%m\"\n"
-				+ "M=`uname -n`; M=${M%%\\.*}\n"
-				+ "\n"
-				+ "J_ID=" + curJID + "\n"
-				+ "J_STA=" + R.JOB_STATUS_RUNNING + "\n"
-				+ "J_FIN=" + R.JOB_STATUS_DONE + "\n"
-				+ "J_ERR=" + R.JOB_STATUS_ERR + "\n"
-				+ "RESULT=''\n"
-				+ "\n"
-				+ "JOB=job_$J_ID\n"
-				+ "JOBFILE=$WDIR/$JOB.out\n"
-				+ "\n"
-				+ "# Redirect stdout and stderr streams\n"
-				+ "exec 1>$JOBFILE 2>&1\n"
-				+ "\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "# Functions\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "function runPair { # Run a solver on a benchmark\n"
-				+ "\n"
-				+ "	PID=$1\n"
-				+ "	SPATH=$2\n"
-				+ "	BPATH=$3\n"
-				+ "	RUN=$4\n"
-				+ "	ST=''\n"
-				+ "	FT=''\n"
-				+ "	RESULT=''\n"
-				+ "\n"
-				+ "	if [ $# != 4 ]; then\n"
-				+ "		echo Incorrect number of args for $0\n"
-				+ "		sendPairStatus $PID $J_ERR\n"
-				+ "		return 1\n"
-				+ "	fi\n"
-				+ "\n"
-				+ "	sendPairStatus $PID $J_STA\n"
-				+ "\n"
-				+ "	SOL=${SPATH##*/} # Solver directory name\n"
-				+ "	SWD=$WDIR/$SOL   # Solver Working Directory \n"
-				+ "	SWP=$SWD/$RUN\n"
-				+ "\n"
-				+ "	BEN=${BPATH##*/} # Benchmark name\n"
-				+ "	BWP=$WDIR/$BEN   # Benchmark Working Path\n"
-				+ "\n"
-				+ "	# If directory doesn't exist, create it.\n"
-				+ "	if [ ! -d $SWD ]; then\n"
-				+ "		mkdir $SWD\n"
-				+ "	fi\n"
-				+ "\n"
-				+ "	# Copy all contents from the solver's shared bin dir to here\n"
-				+ "	cp $SPATH/bin/* $SWD\n"
-				+ "\n"
-				+ "	# Make sure solver runfile exists\n"
-				+ "	if [ ! -f $SWP ]; then\n"
-				+ "		echo Cannot find runfile $RUN in $SWD\n"
-				+ "		return 1\n"
-				+ "	fi\n"
-				+ "	\n"
-				+ "	# Copy benchmark to workspace. WARNING: can't tell the difference between benchmarks with same name!\n"
-				+ "	cp $BPATH $WDIR\n"
-				+ "\n"
-				+ "	# Change dir to solver working directory for runfile\n"
-				+ "	cd $SWD\n"
-				+ "		\n"
-				+ "	# Running $SOL on $BEN ...\n"
-				+ "	ST=`$T`\n"
-				+ "	RESULT=`$SWP $BWP`\n"
-				+ "	FT=`$T`\n"
-				+ "	\n"
-				+ "	ST=$(((${ST%\\.*} * 1000) + ${ST#*\\.}))\n"
-				+ "	FT=$(((${FT%\\.*} * 1000) + ${FT#*\\.}))\n"
-				+ "\n"
-				+ "	# Calculating total time, seconds/ms\n"
-				+ "	# STIME=$((${FT%\\.*} - ${ST%\\.*}))\n"
-				+ "	#MTIME=$((${FT#*\\.} - ${ST#*\\.}))\n"
-				+ "\n"
-				+ "	# Print runtime for solver/benchmark pair \n"
-				+ "	#echo \"$SOL($BEN) : $RESULT : $STIME.$MTIME\"\n"
-				+ "	echo \"$SOL($BEN) : $RESULT : $(($ST - $FT))\"\n"
-				+ "\n"
-				+ "	sendPairStatus $PID $J_FIN\n"
-				+ "}\n"
-				+ "\n"
-				+ "function toResultServlet {\n"
-				+ "	wget \"starexec/starexec/Results?node=$M&$1\" -o /dev/null  -O /dev/null --spider\n"
-				+ "	return $?\n"
-				+ "}\n"
-				+ "\n"
-				+ "function sendPairStatus {\n"
-				+ "	toResultServlet \"pid=$1&status=$2&result=$RESULT&stime=$ST&etime=$FT\" \n"
-				+ "	return $?\n"
-				+ "}\n"
-				+ "\n"
-				+ "function sendJobStatus {\n"
-				+ "	toResultServlet \"jid=$J_ID&status=$1\"\n"
-				+ "	return $?\n"
-				+ "}\n"
-				+ "\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "# Run job\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "echo '*************************************'\n"
-				+ "echo \"SGE Job ID: $JOB_ID\" \n"
-				+ "echo \"JM Job ID:  $J_ID\"\n"
-				+ "echo \"Directory:  $M:$PWD\"\n"
-				+ "echo \"Shell:      $SHELL\"\n"
-				+ "echo \"Out:        $JOBFILE\"\n"
-				+ "echo '*************************************'\n"
-				+ "echo\n"
-				+ "sendJobStatus $J_STA # Signals that whole job starts.\n"
-				+ "\n" );
-		
+		// Gets all the runpairs
+		String runPairs = "";
 		BenchmarkLink lnk;
 		Configuration c;
 		Benchmark b;
@@ -287,7 +176,7 @@ public abstract class JobManager {
 			while(lnk.getSize() > 0) {
 				c = lnk.getNextConfig();
 				s = curJob.getSolver(c.getSolverId());
-				out.write(String.format("runPair %d '%s' '%s' '%s'\n\n", R.PAIR_ID, s.getPath(), b.getPath(), c.getName(), R.PAIR_ID));
+				runPairs += String.format("runPair %d '%s' '%s' '%s'" + ls, R.PAIR_ID, s.getPath(), b.getPath(), c.getName(), R.PAIR_ID);
 				
 				JobPair jp = new JobPair();
 				jp.setId(R.PAIR_ID);
@@ -302,16 +191,16 @@ public abstract class JobManager {
 			}
 		}
 		
-		out.write("\n"
-				+ "\n"
-				+ "sendJobStatus $J_FIN # Signals that whole job terminates.\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "# Teardown\n"
-				+ "# /////////////////////////////////////////////\n"
-				+ "mv -f $JOBFILE $SHR/jobout\n"
-				+ "\n"
-				+ "# Cleanup. Not necessary if we want to cache solvers/benchmarks\n"
-				+ "rm -rf $WDIR/* \n" );
+		// Writes out the bash file
+		out.write(String.format(
+				script, 
+				curJID, 
+				R.JOB_STATUS_RUNNING, 
+				R.JOB_STATUS_DONE, 
+				R.JOB_STATUS_ERR, 
+				runPairs
+				));
+		
 		out.close();
 	}
 }
