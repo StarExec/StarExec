f13f10cc49a25e5b1c9700a6555484aa224de2a6
tnjensen <tyler-jensen@uiowa.edu>
2011-10-24 18:46:25 +0000

    Adding feature branch 1 (Tyler, Skylar, Todd)
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/starexec@7794 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ fb1/src/org/starexec/data/to/Configuration.java
@@ -0,0 +1,48 @@
+package org.starexec.data.to;
+
+/**
+ * Represents a configuration, which is a run script to execute a solver with specific parameters
+ * 
+ * @author Tyler Jensen
+ */
+public class Configuration extends Identifiable {	
+	private long solverId = -1;
+	private String name;
+	private String description;
+	/**
+	 * @return the id of the solver the configuration belongs to
+	 */
+	public long getSolverId() {
+		return solverId;
+	}
+	/**
+	 * @param solverId the id of the solver to set
+	 */
+	public void setSolverId(long solverId) {
+		this.solverId = solverId;
+	}
+	/**
+	 * @return the name of the configuration
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set for the configuration
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the user defined description for the configuration
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set for the configuration
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}	
+}
