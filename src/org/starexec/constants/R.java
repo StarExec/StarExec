f611e282194d6ed9b18e81dc2b9ab6324d07b777
bmccune <benton-mccune@uiowa.edu>
2012-03-27 18:30:08 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@9516 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/constants/R.java
+++ src/org/starexec/constants/R.java
@@ -46,7 +46,7 @@ public class R {
 	public static String NODE_OUTPUT_DIR = null;							// The path to the directory on the local node where output should be placed by the user in order to be saved by starexec
 	public static String PROCESSOR_DIR = null;								// Where to deposit new processor scripts
 	public static String DOWNLOAD_FILE_DIR = null;							// Where to temporarily store processed files for downloading
-	
+	public static String SPACE_XML_SCHEMA_LOC = null;						// Where the schema for batch space xml is located. 
 	// Job Manager (JM) constants
 	public static String JOBFILE_FORMAT = null;								// The filename format (with standard java string formatting) for generated jobscript files
 	public static String SOLVER_BIN_DIR = null;								// The path to the bin directory to look for runscripts (relative to the solver's toplevel directory)	
