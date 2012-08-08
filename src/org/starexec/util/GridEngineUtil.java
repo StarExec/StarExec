1e09d6f51e523ee8565daa60143b9478cfbd058a
bmccune <benton-mccune@uiowa.edu>
2012-08-08 20:57:48 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@10526 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/util/GridEngineUtil.java
+++ src/org/starexec/util/GridEngineUtil.java
@@ -304,9 +304,9 @@ public class GridEngineUtil {
 				log.debug("Processing job pair " + safeId);
 				
 				// Execute the processing for this id on a thread from the pool
-				//threadPool.execute(new Runnable() {					
-				//	@Override
-				//	public void run() {
+				threadPool.execute(new Runnable() {					
+					@Override
+					public void run() {
 						log.info("Processing pair " + safeId + " on thread " + Thread.currentThread().getName());
 
 						// Process statistics and attributes
@@ -317,8 +317,8 @@ public class GridEngineUtil {
 						Jobs.setSGEPairStatus(safeId, (success) ? StatusCode.STATUS_COMPLETE.getVal() : StatusCode.ERROR_RESULTS.getVal());
 						
 						log.info("Processing complete for pair " + safeId + " on thread " + Thread.currentThread().getName());
-				//	}
-				//});
+					}
+				});
 			}
 			
 			if(idsToProcess != null && idsToProcess.size() > 0) {
