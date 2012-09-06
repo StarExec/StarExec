29a4397672d767b7a552158d6fcd12f21bd3be24
bmccune <benton-mccune@uiowa.edu>
2012-09-06 21:32:52 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@10704 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/util/GridEngineUtil.java
+++ src/org/starexec/util/GridEngineUtil.java
@@ -490,7 +490,7 @@ public class GridEngineUtil {
 				String line = null;
 				while ((line = br.readLine()) != null)   {	
 					// If this is the stats entry we're looking for...
-					log.info("Continuing search for " + sgeId + ". Attempt # " + (61 - hackCount) +". line is really ===" + line + "===");
+					log.debug("Continuing search for " + sgeId + ". Attempt # " + (61 - hackCount) +". line is really ===" + line + "===");
 					if(statsPattern.matcher(line).matches()) {
 						// Split it by colons (the delimiter sge uses) and return it
 						log.info("Pattern found for " + sgeId + " on attempt # " + (61 - hackCount));
