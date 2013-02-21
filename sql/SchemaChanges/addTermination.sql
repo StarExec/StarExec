cfec403f81f1d55b3856e9ecde523bbdfe039b93
bmccune <benton-mccune@uiowa.edu>
2013-02-21 23:07:39 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@11914 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ sql/SchemaChanges/addTermination.sql
@@ -0,0 +1,6 @@
+USE prod_starexec;
+--Give me permission to add communities
+INSERT INTO permissions(add_solver, add_bench, add_user, add_space, add_job, remove_solver, remove_bench, remove_user, remove_space, remove_job, is_leader) VALUES
+	(0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO user_assoc values (1,1,1, (select LAST_INSERT_ID()) );
