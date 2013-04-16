b6dbe866e9b4d511a0c5b2c154aefa4a843397b4
astump <astump@acm.org>
2013-04-16 14:57:10 +0000

    updates to this script
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@12420 bd178f5f-7bea-4014-844f-495ca19b8805

--- scripts-common/starjobs.sh
+++ scripts-common/starjobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /home/starexec/scripts/starsql <<EOF
-select job_id from job_pairs where status_code = 4;
+select first_name from job_pairs , jobs , users where job_pairs.job_id = jobs.id and jobs.user_id = users.id and status_code = 4;
 EOF
