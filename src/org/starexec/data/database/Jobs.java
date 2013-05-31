900a24ca568fdf1b7fc5176a2220da87708b9e7c
eaburns <eric-burns@uiowa.edu>
2013-05-31 17:25:38 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@12868 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/Jobs.java
+++ src/org/starexec/data/database/Jobs.java
@@ -1110,13 +1110,7 @@ public class Jobs {
 		jp.setBlockOutput(result.getDouble("block_output"));
 		jp.setVoluntaryContextSwitches(result.getDouble("vol_contex_swtch"));
 		jp.setInvoluntaryContextSwitches(result.getDouble("invol_contex_swtch"));
-		try {
-			jp.setPath(result.getString("path"));
-		} catch (Exception e) {
-			//temp fix so stardev doesn't die
-			log.debug(e);
-		}
-		
+		jp.setPath(result.getString("path"));
 		log.debug("getting job pair from result set for id " + jp.getId());
 		return jp;
 	}
