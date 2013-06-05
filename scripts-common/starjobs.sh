1c0f45f7997bee861f1b37779447f79f133164f3
astump <astump@acm.org>
2013-06-05 17:38:41 +0000

    more script updates
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@12912 bd178f5f-7bea-4014-844f-495ca19b8805

--- scripts-common/starjobs.sh
+++ scripts-common/starjobs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "---------------------------------------------"
+echo "Users with currently running job pairs (status_code=8):"
 /home/starexec/scripts/starsql <<EOF
-select first_name from job_pairs , jobs , users where job_pairs.job_id = jobs.id and jobs.user_id = users.id and status_code = 4;
+select first_name, job_id from job_pairs , jobs , users where job_pairs.job_id = jobs.id and jobs.user_id = users.id and status_code = 4;
+EOF
+
+echo "---------------------------------------------"
+echo "Users with pending job pairs (status_code=1):"
+/home/starexec/scripts/starsql <<EOF
+select distinct first_name from job_pairs , jobs , users where job_pairs.job_id = jobs.id and jobs.user_id = users.id and status_code = 4;
+EOF
+echo "Number of pending job pairs (status_code=1):"
+/home/starexec/scripts/starsql <<EOF
+select count(*) from job_pairs where status_code = 1;
 EOF
