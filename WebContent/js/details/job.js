27a64210a7db673850e21afd31bf57b7a23070f4
wkaiser <wyatt-kaiser@uiowa.edu>
2013-06-07 00:04:36 +0000

    Fixed the job-pair table so that if the user clicks on the href for a primitive, it doesn't change the current page and only opens the new tab.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@12935 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/details/job.js
+++ WebContent/js/details/job.js
@@ -43,11 +43,19 @@ function initUI(){
 	$('fieldset:first, fieldset:eq(1)').expandable(false);
 	$('fieldset:not(:first, :eq(1))').expandable(true);
 	
-	// Set up row click to send to pair details page
+	$('#pairTbl tbody').delegate("a", "click", function(event) {
+		event.stopPropogation();
+	});
+
+	
+	//Set up row click to send to pair details page
 	$("#pairTbl tbody").delegate("tr", "click", function(){
 		var pairId = $(this).find('input').val();
 		window.location.assign(starexecRoot+"secure/details/pair.jsp?id=" + pairId);
 	});
+
+
+	
 	
 }
 
