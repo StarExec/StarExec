5acd993b4a1e1ad1b313537b447b2eed531cf10d
wkaiser <wyatt-kaiser@uiowa.edu>
2013-06-25 17:00:42 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@13144 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/config/sge/functions.bash
+++ src/org/starexec/config/sge/functions.bash
@@ -75,7 +75,7 @@ function sendStatus {
 }
 
 function sendNode {
-    mysql -u"$DB_USER" -p"$DB_PASS" -h $REPORT_HOST $DB_NAME -e "CALL UpdateNodeId($PAIR_ID, \'$1\' )"
+    mysql -u"$DB_USER" -p"$DB_PASS" -h $REPORT_HOST $DB_NAME -e "CALL UpdateNodeId($PAIR_ID, '$1' )"
 	log "sent Node Id $1 to $REPORT_HOST"
 	return $?
 }
