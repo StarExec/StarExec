58a990792d79c52cf3629195888ce1ce0895c875
wkaiser <wyatt-kaiser@uiowa.edu>
2013-07-11 22:47:02 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@13365 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/SchemaChanges/for-stardev/Status_Code_Changes.sql
+++ sql/SchemaChanges/for-stardev/Status_Code_Changes.sql
@@ -3,5 +3,5 @@ USE starexec;
 -- For updating the jobs table to include a new int
 -- Author: Wyatt Kaiser
 
-INSERT INTO status_codes (20, 'paused', 'the job is paused so all job_pairs that were not complete were sent to this status');
-INSERT INTO status_codes (21, 'killed', 'the job was killed, so all job_pairs that were not complete were sent to this status');
+INSERT INTO status_codes VALUES (20, 'paused', 'the job is paused so all job_pairs that were not complete were sent to this status');
+INSERT INTO status_codes VALUES (21, 'killed', 'the job was killed, so all job_pairs that were not complete were sent to this status');
