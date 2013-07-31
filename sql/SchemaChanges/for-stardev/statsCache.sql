b26e807852dec1ded884b7acd9349ea5dce8ee1e
eaburns <eric-burns@uiowa.edu>
2013-07-31 23:11:54 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@13598 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/SchemaChanges/for-stardev/statsCache.sql
+++ sql/SchemaChanges/for-stardev/statsCache.sql
@@ -1,4 +1,5 @@
-use starexec;
+USE starexec;
+
 CREATE TABLE job_stats (
 	job_id INT NOT NULL,
 	job_space_id INT NOT NULL,
@@ -7,7 +8,7 @@ CREATE TABLE job_stats (
 	failed INT NOT NULL,
 	error INT NOT NULL,
 	wallclock DOUBLE,
-	PRIMARY KEY (job_id,job_space_id),
+	PRIMARY KEY (job_id, job_space_id),
 	FOREIGN KEY (job_id) REFERENCES jobs(id) ON DELETE CASCADE,
 	FOREIGN KEY (config_id) REFERENCES configurations(id) ON DELETE CASCADE,
 	FOREIGN KEY (job_space_id) REFERENCES job_spaces(id) ON DELETE CASCADE
