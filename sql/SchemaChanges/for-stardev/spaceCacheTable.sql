c5173b74ab343d366a14b52fe1173e26d2797a71
eaburns <eric-burns@uiowa.edu>
2013-08-22 23:06:34 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@13755 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ sql/SchemaChanges/for-stardev/spaceCacheTable.sql
@@ -0,0 +1,13 @@
+USE starexec;
+
+-- This file adds the space cache table to the database
+-- Author: Eric Burns
+
+
+-- Associates space IDs with the cache of their downloads, with multiple possibilities(maybe do solvers, benchmarks, solvers+benchmarks?)
+CREATE TABLE space_cache (
+	space_id INT NOT NULL,
+	path TEXT NOT NULL,
+	UNIQUE KEY (space_id),
+	FOREIGN KEY (space_id) REFERENCES spaces(id) ON DELETE CASCADE
+);
