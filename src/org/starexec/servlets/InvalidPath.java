b4a72030f5b3241f72cb981ffab88d219ed9bdd7
eaburns <eric-burns@uiowa.edu>
2013-08-31 21:50:30 +0000

    Simple servlet that prevents users from directly accessing our secure/files or secure/cache directories.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@13817 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ src/org/starexec/servlets/InvalidPath.java
@@ -0,0 +1,33 @@
+package org.starexec.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Very simple servlet that simply blocks access to the files and cache directories.
+ * We might decide to move these in the future, which will make this unnecessary.
+ * @author Eric Burns
+ */
+@SuppressWarnings("serial")
+public class InvalidPath extends HttpServlet {		
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		// Make sure the request is valid
+		response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+		
+	}
+}
