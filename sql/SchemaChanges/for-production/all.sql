3dc39dcb659057cda0eea269ddf5fb136670e4d5
astump <astump@acm.org>
2013-11-14 18:16:25 +0000

    removing parts of all.q that have been applied
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/deployed/11-14-13/starexec@14430 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/SchemaChanges/for-production/all.sql
+++ sql/SchemaChanges/for-production/all.sql
@@ -1,7 +1,3 @@
-source addConfigTimestamp.sql
-source attrPrimaryKey.sql 
-source recycledColumn.sql
-source removeCommentTable.sql
 source reserveQueueTables.sql
 source reserveQueueKeys.sql
 source statsCacheChange.sql
