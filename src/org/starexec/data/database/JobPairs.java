ab2e8232a5433cc5aa2bf6bd5e77970c9b3c35aa
eaburns <eric-burns@uiowa.edu>
2013-12-13 02:55:04 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@14680 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/JobPairs.java
+++ src/org/starexec/data/database/JobPairs.java
@@ -376,8 +376,20 @@ public class JobPairs {
 			for (String space : pathSpaces) {
 				file=new File(file,space);
 			}
+			File testFile=new File(file,pair.getSolver().getName());
+			testFile=new File(testFile,pair.getConfiguration().getName());
+			
 			file=new File(file,pair.getSolver().getName()+"___"+pair.getConfiguration().getName());
+			
+			
+			
 			file=new File(file,pair.getBench().getName());
+			testFile=new File(testFile,pair.getBench().getName());
+			if (!file.exists()) {	    // if the job output could not be found
+				if (testFile.exists()) {  //check the alternate path some pairs are still stored at
+					FileUtils.copyFile(testFile, file);
+				}
+			}
 			log.debug("found the path "+file.getAbsolutePath()+" for the job pair");
 			return file.getAbsolutePath();
 		} catch(Exception e) {
