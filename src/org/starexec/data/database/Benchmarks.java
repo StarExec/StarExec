c078f8f90d62ebe5cd6adb8202e24e2c9e702b2d
astump <astump@acm.org>
2014-05-20 10:21:38 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@17570 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/Benchmarks.java
+++ src/org/starexec/data/database/Benchmarks.java
@@ -894,14 +894,16 @@ public class Benchmarks {
 					Uploads.incrementTotalBenchmarks(statusId);//for upload status page
 					// Make sure that the benchmark has a unique name in the space.
 					if(Spaces.notUniquePrimitiveName(b.getName(), space.getId(), 2)) {
-					    Uploads.setErrorMessage(statusId, "\""+b.getName() + "\" is not a unique name in the space.");
-					    return null;
+					    String msg = "\""+b.getName() + "\" is not a unique name in the space.";
+					    Uploads.setErrorMessage(statusId, msg);
+					    throw new Exception(msg);
 					}
 
 					space.addBenchmark(b);
 				} else {
-				    Uploads.setErrorMessage(statusId, "\""+f.getName() + "\" is not accepted as a legal benchmark name.");
-				    return null;
+				    String msg = "\""+f.getName() + "\" is not accepted as a legal benchmark name.";
+				    Uploads.setErrorMessage(statusId, msg);
+				    throw new Exception(msg);
 				}
 
 			}
