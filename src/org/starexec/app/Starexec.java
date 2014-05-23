ef89d63e62f2f9581196e882b55a0d2bb38f7b90
astump <astump@acm.org>
2014-05-23 19:02:02 +0000

    removing one-time code for copying processors over to the new format
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@17610 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/app/Starexec.java
+++ src/org/starexec/app/Starexec.java
@@ -221,13 +221,6 @@ public class Starexec implements ServletContextListener {
 		    taskScheduler.scheduleAtFixedRate(checkQueueReservations, 0, 30, TimeUnit.SECONDS);
 		    taskScheduler.scheduleAtFixedRate(postProcessJobsTask,0,45,TimeUnit.SECONDS);
 		}
-		//TODO: this is a one time task! We should remove it after we have run it on production!
-		try {
-			ProcessorManager.copyAllProcessorsToNewFormat();
-			ProcessorManager.setAllProcessorsExecutable();
-		} catch (Exception e) {
-			log.error(e.getMessage(),e);
-		}
 		
 
 	}
