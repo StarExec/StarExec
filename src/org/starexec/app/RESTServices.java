d21c3972c4b89237712ee14c40be080ff6584270
astump <astump@acm.org>
2014-06-17 17:33:38 +0000

    trying to stop the system from pausing jobs every so often (still not quite sure why this is happening, so adding more log info)
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@17794 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/app/RESTServices.java
+++ src/org/starexec/app/RESTServices.java
@@ -3780,13 +3780,13 @@ public class RESTServices {
 	@Path("/admin/pauseAll")
 	@Produces("application/json")
 	public String pauseAll(@Context HttpServletRequest request) {
-		// Permissions check; if user is NOT the owner of the job, deny pause request
 		int userId = SessionUtil.getUserId(request);
 		
 		int status=JobSecurity.canUserPauseAllJobs(userId);
 		if (status!=0) {
 			return gson.toJson(status);
 		}
+		log.info("Pausing all jobs in admin/pauseAll REST service");
 		return Jobs.pauseAll() ? gson.toJson(0) : gson.toJson(ERROR_DATABASE);
 	}
 	
