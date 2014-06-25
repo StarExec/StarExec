def7b527708d4545d649fb1a01728a1cd67e1c24
astump <astump@acm.org>
2014-06-25 18:35:10 +0000

    improving our query which clears zombie job pairs from nodes when new job pairs start running there; increasing default user quota to 1gb; documentation corrections
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@17856 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/procedures/JobPairs.sql
+++ sql/procedures/JobPairs.sql
@@ -52,7 +52,7 @@ CREATE PROCEDURE UpdateNodeId(IN _jobPairId INT, IN _nodeName VARCHAR(128))
 		SELECT id FROM nodes WHERE name=_nodeName INTO _nodeId;
 
                 -- record an error for job pairs that we think are still running on this node (they should not be, with 1 sandbox only)
-		UPDATE job_pairs SET status_code = 10 where node_id = _nodeID and status_code = 4;
+		UPDATE job_pairs SET status_code = 10 where node_id = _nodeID and (status_code=3 or status_code=4 or status_code=5);
 
 		UPDATE job_pairs SET node_id=_nodeId WHERE id = _jobPairId;
 	END //
