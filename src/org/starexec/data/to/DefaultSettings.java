4a559b12c483502003a40b9793006ba17ec03e81
eaburns <eric-burns@uiowa.edu>
2014-10-27 21:45:36 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@18595 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/to/DefaultSettings.java
+++ src/org/starexec/data/to/DefaultSettings.java
@@ -206,7 +206,17 @@ public class DefaultSettings extends Identifiable {
 
 		log.debug(this.toString());
 		log.debug(set.toString());
-
+		log.debug(this.getId()==set.getId());
+		log.debug(Util.stringsEqual(this.getName(), set.getName()));
+		log.debug(this.getPrimId()==set.getPrimId());
+		log.debug(this.getPreProcessorId()==set.getPreProcessorId());
+		log.debug(this.getBenchProcessorId()==set.getBenchProcessorId());
+		log.debug(this.getPostProcessorId()==set.getPostProcessorId());
+		log.debug(this.getCpuTimeout()==set.getCpuTimeout());
+		log.debug(this.getWallclockTimeout()==set.getWallclockTimeout());
+		log.debug(this.getMaxMemory()==set.getMaxMemory());
+		log.debug(this.getSolverId()==set.getSolverId());
+		log.debug(this.getBenchId()==set.getBenchId());
 		return (this.getId()==set.getId() &&
 				Util.stringsEqual(this.getName(), set.getName()) &&
 				this.getPrimId()==set.getPrimId() &&
