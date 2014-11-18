7fc6d143b79d8ef3e065b40cb890645f25cadf5f
eaburns <eric-burns@uiowa.edu>
2014-11-18 16:28:55 +0000

    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@18743 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/TestManager.java
+++ src/org/starexec/test/TestManager.java
@@ -61,7 +61,7 @@ public class TestManager {
 		//tests.add(new LoginTests());
 		//tests.add(new UploadSolverTests());
 		//tests.add(new UploadBenchmarksTests());
-		tests.add(new SpaceExplorerTests());
+		//tests.add(new SpaceExplorerTests());
 	}
 	
 	public static boolean areTestsRunning() {
