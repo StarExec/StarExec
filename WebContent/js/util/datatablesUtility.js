b3043f15cf0d9e6ee5a2043e0de07bd94473b538
agiegerich <albert.a.giegerich@gmail.com>
2015-02-15 16:41:26 +0000

    Added datatablesUtility
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@19395 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ WebContent/js/util/datatablesUtility.js
@@ -0,0 +1,17 @@
+
+/**
+ * For a given dataTable, this extracts the id's of the rows that have been
+ * selected by the user
+ * 
+ * @param dataTable the particular dataTable to extract the id's from
+ * @returns {Array} list of id values for the selected rows
+ * @author Todd Elvers
+ */
+function getSelectedRows(dataTable){
+	var idArray = new Array();
+	var rows = $(dataTable).children('tbody').children('tr.row_selected');
+	$.each(rows, function(i, row) {
+		idArray.push($(this).children('td:first').children('input').val());
+	});
+	return idArray;
+}
