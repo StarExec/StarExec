dfe33e7a53a88b0ec94720d4fe066bc1dceb3d6c
eaburns <eric-burns@uiowa.edu>
2015-03-05 00:34:07 +0000

    Updating one of the test resources.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@19638 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ sql/SchemaChanges/for-production/stageDataChanges.sql
@@ -0,0 +1,9 @@
+USE starexec;
+
+ALTER TABLE jobpair_stage_data ADD COLUMN status_code TINYINT DEFAULT 0;
+
+UPDATE jobpair_stage_data JOIN (SELECT id, status_code FROM job_pairs) AS pairs ON pairs.id=jobpair_stage_data.jobpair_id SET jobpair_stage_data.status_code = pairs.status_code;
+
+ALTER TABLE job_attributes ADD COLUMN jobpair_data INT NOT NULL DEFAULT 0;
+
+UPDATE job_attributes JOIN (SELECT id, primary_jobpair_data FROM job_pairs) AS pairs ON pairs.id=job_attributes.pair_id SET jobpair_data=primary_jobpair_data;
