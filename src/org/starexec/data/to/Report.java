f08223eb20ad3523eee33a67d5ffa68193c57a43
agiegerich <albert.a.giegerich@gmail.com>
2015-03-09 14:19:25 +0000

    Added Report class to data/to
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@19741 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ src/org/starexec/data/to/Report.java
@@ -0,0 +1,66 @@
+package org.starexec.data.to;
+
+/**
+ * Class that represents a report in the weekly reports system.
+ * Author: Albert Giegerich
+ */
+public class Report {
+	// The name of the event the report is about.
+	private final String eventName;
+	// The number of times the event occurred.
+	private final Integer occurrences;
+	// The id of the queue the report is related to, null if the report is not related to a queue.
+	private final Integer queueId;
+	// The name of the queue that the report is related to, null if the report is not related to a queue.
+	private final String queueName;
+
+	public Report(String eventName, int occurrences, int queueId, String queueName) {
+		this.eventName = eventName;
+		this.occurrences = occurrences;
+		this.queueId = queueId;
+		this.queueName = queueName;
+	}
+
+	public Report(String eventName, int occurrences) {
+		this.eventName = eventName;
+		this.occurrences = occurrences;
+		this.queueId = null;
+		this.queueName = null;
+	}
+
+	/**
+	 * Gets the name of the event the report is about.
+	 * @return the name of the event the report is a bout
+	 * @author Albert Giegerich
+	 */
+	public String getEventName() {
+		return eventName;
+	}
+
+	/**
+	 * Gets the number of time the report's event occurred.
+	 * @return the number times the report's event occurred.
+	 * @author Albert Giegerich
+	 */
+	public int getOccurrences() {
+		return occurrences;
+	}
+
+	/**
+	 * Gets the id of the queue related to the report.
+	 * @return the id of the queue related to the report, null if the report is not related to a queue.
+	 * @author Albert Giegerich
+	 */
+	public int getQueueId() {
+		return queueId;
+	}
+
+	/**
+	 * Gets the name of the queue related to the report.
+	 * @return the name of the queue related to the report, null if the report is not related to a queue.
+	 * @author Albert Giegerich
+	 */
+	public String getQueueName() {
+		return queueName;
+	}
+}
