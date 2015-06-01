298638d095a8726f2c9d95e30a9d8f2bc6e79e21
agiegerich <albert.a.giegerich@gmail.com>
2015-06-01 15:05:04 +0000

    Added NameableComparators
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@20652 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ src/org/starexec/data/to/compare/NameableComparators.java
@@ -0,0 +1,29 @@
+package org.starexec.data.to.compare;
+
+import java.util.Comparator;
+import org.starexec.data.to.Nameable;
+
+/**
+ * Class that builds comparators for Nameable objects.
+ * @author Albert Giegerich
+ */
+public class NameableComparators {
+
+	/**
+	 * Builds a comparator for Nameable objects that compares two objects alphabetically
+	 * insensitive to case.
+	 * @return The new Comparator
+	 * @author Albert Giegerich
+	 */
+	public static Comparator<Nameable> caseInsensitiveAlphabeticalComparator() {
+		return new Comparator<Nameable>() {
+			public int compare(Nameable a, Nameable b) {
+				String aName = a.getName();
+				String bName = b.getName();
+				return aName.compareToIgnoreCase(bName);
+			}
+		};
+	}
+
+}
+
