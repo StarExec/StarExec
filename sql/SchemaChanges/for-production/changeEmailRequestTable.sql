1d1866253d23016e5817c5e2f6c5fbaa88cc61e0
agiegerich <albert.a.giegerich@gmail.com>
2015-06-03 16:31:30 +0000

    Added code field to schema change.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@20665 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/SchemaChanges/for-production/changeEmailRequestTable.sql
+++ sql/SchemaChanges/for-production/changeEmailRequestTable.sql
@@ -1,8 +1,9 @@
 CREATE TABLE change_email_requests (
-	id INT NOT NULL AUTO_INCREMENT,
 	user_id INT NOT NULL,
 	new_email VARCHAR(64) NOT NULL,
-	PRIMARY KEY (id),
+	code VARCHAR(36) NOT NULL,
+	PRIMARY KEY (user_id),
+	UNIQUE KEY (code),
 	CONSTRAINT change_email_request_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
