672f07f48c0ba47b77aff44c257237a0bdad94b7
agiegerich <albert.a.giegerich@gmail.com>
2015-07-21 21:05:53 +0000

    Fixed bug where unknown pairs were being displayed as solved.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@20812 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/config/pagination/PairInJobSpaceHierarchyPagination.sql
+++ src/org/starexec/config/pagination/PairInJobSpaceHierarchyPagination.sql
@@ -42,8 +42,8 @@
 				( :pairType = "incomplete" AND job_pairs.status_code!=7 AND !(job_pairs.status_code>=14 AND job_pairs.status_code<=17)) OR
 				( :pairType="failed" AND ((job_pairs.status_code>=8 AND job_pairs.status_code<=13) OR job_pairs.status_code=18)) OR
 				( :pairType ="complete" AND (job_pairs.status_code=7 OR (job_pairs.status_code<=14 ANd job_pairs.status_code<=17))) OR
-				( :pairType= "unknown" AND job_pairs.status_code=7 AND job_attributes.attr_value="starexec-unknown") OR
-				( :pairType = "solved" AND job_pairs.status_code=7 AND (job_attributes.attr_value=bench_attributes.attr_value OR bench_attributes.attr_value is null)) OR
+				( :pairType= "unknown" AND job_pairs.status_code=7 AND (job_attributes.attr_value="starexec-unknown"OR bench_attributes.attr_value is null)) OR
+				( :pairType = "solved" AND job_pairs.status_code=7 AND job_attributes.attr_value=bench_attributes.attr_value) OR
 				( :pairType = "wrong" AND job_pairs.status_code=7 AND (bench_attributes.attr_value is not null) and (job_attributes.attr_value!=bench_attributes.attr_value)))
 				
 				
