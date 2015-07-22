e1bf248a4c271d6647190f04a08dc46b5f87f2f6
agiegerich <albert.a.giegerich@gmail.com>
2015-07-22 16:58:24 +0000

    Made all file paths for pagination queries constants to aid with debugging.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@20813 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/constants/PaginationQueries.java
+++ src/org/starexec/constants/PaginationQueries.java
@@ -7,19 +7,43 @@ import org.apache.commons.io.FileUtils;
 
 public class PaginationQueries {
 	
+	public static String GET_PAIRS_IN_SPACE_PATH = "/pagination/PairInJobSpacePagination.sql";
 	public static String GET_PAIRS_IN_SPACE_QUERY = "";
+
+	public static String GET_PAIRS_IN_SPACE_HIERARCHY_PATH = "/pagination/PairInJobSpaceHierarchyPagination.sql";
 	public static String GET_PAIRS_IN_SPACE_HIERARCHY_QUERY = "";
 
+	public static String GET_BENCHMARKS_IN_SPACE_PATH = "/pagination/BenchInSpacePagination.sql";
 	public static String GET_BENCHMARKS_IN_SPACE_QUERY = "";
+
+	public static String GET_BENCHMARKS_BY_USER_PATH = "/pagination/BenchForUserPagination.sql";
 	public static String GET_BENCHMARKS_BY_USER_QUERY = "";
+
+	public static String GET_JOBS_IN_SPACE_PATH = "/pagination/JobInSpacePagination.sql";
 	public static String GET_JOBS_IN_SPACE_QUERY = "";
+
+	public static String GET_JOBS_BY_USER_PATH = "/pagination/JobForUserPagination.sql";
 	public static String GET_JOBS_BY_USER_QUERY = "";
+
+	public static String GET_USERS_IN_SPACE_PATH ="/pagination/UserInSpacePagination.sql";
 	public static String GET_USERS_IN_SPACE_QUERY = "";
+
+	public static String GET_SUBSPACES_IN_SPACE_PATH = "/pagination/SubspacesInSpacePagination.sql";
 	public static String GET_SUBSPACES_IN_SPACE_QUERY = "";
+
+	public static String GET_INCOMPLETE_JOBS_PATH = "/pagination/IncompleteJobPagination.sql";
 	public static String GET_INCOMPLETE_JOBS_QUERY = "";
+
+	public static String GET_SOLVERS_IN_SPACE_PATH = "/pagination/SolverInSpacePagination.sql";
 	public static String GET_SOLVERS_IN_SPACE_QUERY = "";
+
+	public static String GET_SOLVERS_BY_USER_PATH ="/pagination/SolverForUserPagination.sql";
 	public static String GET_SOLVERS_BY_USER_QUERY = "";
+
+	public static String GET_PAIRS_RUNNING_PATH = "/pagination/RunningPairPagination.sql";
 	public static String GET_PAIRS_RUNNING_QUERY = "";
+
+	public static String GET_PAIRS_ENQUEUED_PATH = "/pagination/EnqueuedPairPagination.sql";
 	public static String GET_PAIRS_ENQUEUED_QUERY = "";
 
 	/**
@@ -27,19 +51,19 @@ public class PaginationQueries {
 	 * @throws IOException
 	 */
 	public static void loadPaginationQueries() throws IOException {
-		GET_PAIRS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/PairInJobSpacePagination.sql"));
-		GET_BENCHMARKS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/BenchInSpacePagination.sql"));
-		GET_BENCHMARKS_BY_USER_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/BenchForUserPagination.sql"));
-		GET_JOBS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/JobInSpacePagination.sql"));
-		GET_JOBS_BY_USER_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/JobForUserPagination.sql"));
-		GET_USERS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/UserInSpacePagination.sql"));
-		GET_SUBSPACES_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/SubspacesInSpacePagination.sql"));
-		GET_INCOMPLETE_JOBS_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/IncompleteJobPagination.sql"));
-		GET_SOLVERS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/SolverInSpacePagination.sql"));
-		GET_SOLVERS_BY_USER_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/SolverForUserPagination.sql"));
-		GET_PAIRS_IN_SPACE_HIERARCHY_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/PairInJobSpaceHierarchyPagination.sql"));
-		GET_PAIRS_RUNNING_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/RunningPairPagination.sql"));
-		GET_PAIRS_ENQUEUED_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, "/pagination/EnqueuedPairPagination.sql"));
+		GET_PAIRS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_PAIRS_IN_SPACE_PATH));
+		GET_BENCHMARKS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_BENCHMARKS_IN_SPACE_PATH));
+		GET_BENCHMARKS_BY_USER_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_BENCHMARKS_BY_USER_PATH));
+		GET_JOBS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_JOBS_IN_SPACE_PATH));
+		GET_JOBS_BY_USER_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_JOBS_BY_USER_PATH));
+		GET_USERS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_USERS_IN_SPACE_PATH));
+		GET_SUBSPACES_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_SUBSPACES_IN_SPACE_PATH));
+		GET_INCOMPLETE_JOBS_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_INCOMPLETE_JOBS_PATH));
+		GET_SOLVERS_IN_SPACE_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_SOLVERS_IN_SPACE_PATH));
+		GET_SOLVERS_BY_USER_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_SOLVERS_BY_USER_PATH));
+		GET_PAIRS_IN_SPACE_HIERARCHY_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_PAIRS_IN_SPACE_HIERARCHY_PATH));
+		GET_PAIRS_RUNNING_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_PAIRS_RUNNING_PATH));
+		GET_PAIRS_ENQUEUED_QUERY = FileUtils.readFileToString(new File(R.CONFIG_PATH, GET_PAIRS_ENQUEUED_PATH));
 
 
 	}
