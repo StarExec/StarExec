7e0c1fe4e0da4bdb2631e6ab9baea61c55132dd7
eaburns <eric-burns@uiowa.edu>
2015-09-22 17:31:03 +0000

    Fixing typo.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@21340 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/new-install/StarSchema.sql
+++ sql/new-install/StarSchema.sql
@@ -369,7 +369,7 @@ CREATE TABLE jobpair_time_delta (
 	queue_id INT NOT NULL,
 	time_delta INT DEFAULT 0,
 	PRIMARY KEY (user_id, queue_id),
-	CONSTRAINT jobpair_time_delta_queue_id FOREIGN KEY (queue_id) REFERENCES queues(id) ON DELETE CASCASE,
+	CONSTRAINT jobpair_time_delta_queue_id FOREIGN KEY (queue_id) REFERENCES queues(id) ON DELETE CASCADE,
 	CONSTRAINT jobpair_time_delta_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
