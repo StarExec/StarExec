475dd9930dc1039f8f37e67036785b6e345bea24
eaburns <eric-burns@uiowa.edu>
2015-12-16 19:12:09 +0000

    One more modification to getting pending pairs, and changing security so that developers can change logging levels.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@22294 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/procedures/Jobs.sql
+++ sql/procedures/Jobs.sql
@@ -432,7 +432,7 @@ CREATE PROCEDURE GetTimelessJobPairsByStatus(IN _jobId INT, IN _statusCode INT)
 DROP PROCEDURE IF EXISTS GetPendingJobPairsByJob;
 CREATE PROCEDURE GetPendingJobPairsByJob(IN _id INT, IN _limit INT)
 	BEGIN
-		SELECT * FROM job_pairs FORCE INDEX (status_code) 
+		SELECT * FROM job_pairs FORCE INDEX (job_id_2) 
 		JOIN jobpair_stage_data ON jobpair_stage_data.jobpair_id = job_pairs.id 
 		LEFT JOIN benchmarks ON benchmarks.id = job_pairs.bench_id 
 		LEFT JOIN solvers ON solvers.id = jobpair_stage_data.solver_id 
