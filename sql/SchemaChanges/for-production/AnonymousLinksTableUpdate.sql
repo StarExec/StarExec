812afb2bd18bebf6a23d335bcec835517f3353ed
agiegerich <albert.a.giegerich@gmail.com>
2016-02-29 22:11:00 +0000

    Changed order of enum strings so it matches StarSchema exactly.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@22680 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/SchemaChanges/for-production/AnonymousLinksTableUpdate.sql
+++ sql/SchemaChanges/for-production/AnonymousLinksTableUpdate.sql
@@ -5,7 +5,7 @@ DROP TABLE anonymous_links;
 CREATE TABLE anonymous_links (
 	unique_id VARCHAR(36) NOT NULL,
 	primitive_id INT NOT NULL,
-	primitive_type ENUM('solver', 'bench', 'job') NOT NULL,
+	primitive_type ENUM('solver', 'job', 'bench') NOT NULL,
 	primitives_to_anonymize ENUM('all', 'allButBench', 'none') NOT NULL,
 	date_created DATE NOT NULL,
 
