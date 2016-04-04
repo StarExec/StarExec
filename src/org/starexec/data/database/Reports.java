4523e6a10d6681f03518e647a9b7eb2ad73634a7
eaburns <eric-burns@uiowa.edu>
2016-04-04 17:37:15 +0000

    Deleting unused reports code.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@22875 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/Reports.java
+++ src/org/starexec/data/database/Reports.java
@@ -33,17 +33,6 @@ public class Reports {
 	}
 
 	/**
-	 * Set the number of occurrences for an event related to a queue.
-	 * @param eventName the name of the event.
-	 * @param occurrences the number of times the event occurred.
-	 * @param queueName the name of the queue related to the event.
-	 * @author Albert Giegerich
-	 */
-	public static void setEventOccurrencesForQueue(String eventName, int occurrences, String queueName) {
-		setEventOccurrences(eventName, occurrences, queueName);
-	}
-
-	/**
 	 * Add occurrences to an event not related to a queue.
 	 * @param eventName the name of the event.
 	 * @param occurrences the number of times the event occurred.
@@ -62,30 +51,6 @@ public class Reports {
 	public static void addToEventOccurrencesForQueue(String eventName, int occurrences, String queueName) {
 		addToEventOccurrences(eventName, occurrences, queueName);
 	}
-
-
-	/**
-	 * Get the number of times an event has occurred.
-	 * @param eventName the name of the event.
-	 * @return the number of times the event has occurred.
-	 * @author Albert Giegerihc
-	 */
-	public static Integer getEventOccurrencesNotRelatedToQueue(String eventName) {
-		return getEventOccurrences(eventName, null);
-	}
-
-	/**
-	 * Get the number of times an event has occurred not related to a queue.
-	 * @param eventName the name of the event.
-	 * @param queueName the name of the queue the event is related to.
-	 * @return the number of times the event occurred
-	 * @author Albert Giegerich
-	 */
-	public static Integer getEventOccurrencesForQueue(String eventName, String queueName) {
-		return getEventOccurrences(eventName, queueName);
-	}
-
-
 
 	/**
 	 * Gets every event and the number of times it occurred for events that are not related to a queue.
@@ -147,7 +112,7 @@ public class Reports {
 				reportsForAllQueues.add(report);
 			}
 
-			List<List<Report>> reportsByQueue = seperateReportsByQueue(reportsForAllQueues);
+			List<List<Report>> reportsByQueue = separateReportsByQueue(reportsForAllQueues);
 
 			return reportsByQueue;
 		} catch (Exception e) {
@@ -243,54 +208,14 @@ public class Reports {
 			Common.safeClose(procedure);
 		}
 	}
-
-	/**
-	 * Inner method that gets the number of event occurrences not related to a queue
-	 * if queueName is null, otherwise gets the number of event occurrences related to
-	 * the specified queue.
-	 * @param eventName the name of the event.
-	 * @param queueName the name of the queue the event is related to. Null if not related to a queue.
-	 * @return the number of times the event occurred.
-	 * @author Albert Giegerich
-	 */
-	private static Integer getEventOccurrences(String eventName, String queueName) {
-		Connection con = null;
-		CallableStatement procedure = null;
-		ResultSet results = null;
-		try {
-			con = Common.getConnection();
-
-			if (queueName == null) {
-				procedure = con.prepareCall("{CALL GetEventOccurrencesNotRelatedToQueue(?)}");
-			} else {
-				procedure = con.prepareCall("{CALL GetEventOccurrencesForQueue(?, ?)}");
-				procedure.setString(2, queueName);
-			}
-			procedure.setString(1, eventName);
-
-			results = procedure.executeQuery();
-			results.first();
-
-			int occurrences = results.getInt("occurrences");
-
-			return occurrences;
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-		} finally {
-			Common.safeClose(con);
-			Common.safeClose(procedure);
-			Common.safeClose(results);
-		}
-		return null;
-	}
-
+	
 	/**
 	 * Turns a list of reports related to queues into a list of lists related to queues where 
 	 * each inner list is made up of reports related to a different queue.
 	 * @param reports a list of reports related to queues
 	 * @author Albert Giegerich
 	 */
-	private static List<List<Report>> seperateReportsByQueue(List<Report> reports) {
+	private static List<List<Report>> separateReportsByQueue(List<Report> reports) {
 		// Build a map that seperates all the reports into lists based on which queue they're related to.
 		Map<String,List<Report>> reportMap = new HashMap<String,List<Report>>();
 		for (Report report : reports) {
