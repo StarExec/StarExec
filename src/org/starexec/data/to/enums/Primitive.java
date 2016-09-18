3cca575f0b650c5b0212d3c595ab23e3b0f782b0
agiegerich <albert.a.giegerich@gmail.com>
2016-09-18 20:39:24 +0000

    Added configuration as a primitive type.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@23573 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/to/enums/Primitive.java
+++ src/org/starexec/data/to/enums/Primitive.java
@@ -4,5 +4,5 @@ package org.starexec.data.to.enums;
  * Created by agieg on 9/18/2016.
  */
 public enum Primitive {
-    JOB, USER, SOLVER, BENCHMARK, SPACE, JOB_PAIR, JOB_STATS, NODE, QUEUE
+    JOB, USER, SOLVER, BENCHMARK, SPACE, JOB_PAIR, JOB_STATS, NODE, QUEUE, CONFIGURATION
 }
