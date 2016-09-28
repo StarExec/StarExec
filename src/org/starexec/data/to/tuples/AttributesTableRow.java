d4efaf5d97db4c6394f4305e1228ae505f4167b0
agiegerich <albert.a.giegerich@gmail.com>
2016-09-28 22:06:07 +0000

    Added getters and setters to AttributesTableRow tuple so attributes could be accessed by JSP.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@23612 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/to/tuples/AttributesTableRow.java
+++ src/org/starexec/data/to/tuples/AttributesTableRow.java
@@ -14,4 +14,44 @@ public class AttributesTableRow {
     public int configId;
     public String configName;
     public List<Triple<Integer, Double, Double>> countAndTimes = new ArrayList<>();
+
+    public int getSolverId() {
+        return solverId;
+    }
+
+    public void setSolverId(int solverId) {
+        this.solverId = solverId;
+    }
+
+    public String getSolverName() {
+        return solverName;
+    }
+
+    public void setSolverName(String solverName) {
+        this.solverName = solverName;
+    }
+
+    public int getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(int configId) {
+        this.configId = configId;
+    }
+
+    public String getConfigName() {
+        return configName;
+    }
+
+    public void setConfigName(String configName) {
+        this.configName = configName;
+    }
+
+    public List<Triple<Integer, Double, Double>> getCountAndTimes() {
+        return countAndTimes;
+    }
+
+    public void setCountAndTimes(List<Triple<Integer, Double, Double>> countAndTimes) {
+        this.countAndTimes = countAndTimes;
+    }
 }
