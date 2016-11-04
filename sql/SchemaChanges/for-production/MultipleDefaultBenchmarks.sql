41ad096a1c29fcbe722507f66fc318e942e72246
agiegerich <albert.a.giegerich@gmail.com>
2016-11-04 01:40:05 +0000

    Updated schema change to link to the setting profile instead of the community
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@23785 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/SchemaChanges/for-production/MultipleDefaultBenchmarks.sql
+++ sql/SchemaChanges/for-production/MultipleDefaultBenchmarks.sql
@@ -1,15 +1,15 @@
 USE starexec;
 
 CREATE TABLE default_bench_assoc(
-	community_id INT NOT NULL,
+	setting_id INT NOT NULL,
 	bench_id INT NOT NULL,
-	PRIMARY KEY(community_id, bench_id),
-	CONSTRAINT default_bench_community_id FOREIGN KEY (community_id) REFERENCES spaces(id) ON DELETE CASCADE,
+	PRIMARY KEY(setting_id, bench_id),
+	CONSTRAINT default_setting_id FOREIGN KEY (setting_id) REFERENCES default_settings(id) ON DELETE CASCADE,
 	CONSTRAINT default_bench_id FOREIGN KEY (bench_id) REFERENCES benchmarks(id) ON DELETE CASCADE
 );
 
-INSERT INTO default_bench_assoc (community_id, bench_id)
-SELECT prim_id, default_benchmark
+INSERT INTO default_bench_assoc (setting_id, bench_id)
+SELECT id, default_benchmark
 FROM default_settings
 WHERE prim_id IS NOT NULL 
 AND default_benchmark IS NOT NULL
