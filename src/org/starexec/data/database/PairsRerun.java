ddb97b51ed1f51efb29a403b7624074deb3d9bbb
agiegerich <albert.a.giegerich@gmail.com>
2016-12-01 02:19:13 +0000

    Added method stubs and documentation for PairsRerun interface. Wrote tests for PairsRerun.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@23911 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/PairsRerun.java
+++ src/org/starexec/data/database/PairsRerun.java
@@ -2,22 +2,54 @@ package org.starexec.data.database;
 
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 
+/**
+ * Responsible for issuing queries and updates to the pairs_rerun table.
+ */
 public class PairsRerun {
-    public static boolean hasPairBeenRerun(int pairId) {
+    /**
+     * Checks whether or not a pair has been rerun.
+     * @param pairId the id of the pair to check.
+     * @return true if the pair has been rerun, false otherwise.
+     * @throws SQLException on database error.
+     */
+    public static boolean hasPairBeenRerun(int pairId) throws SQLException {
         return false;
     }
 
-    public static boolean pairHasBeenRerun(Connection con, int pairId) {
+    /**
+     * Checks whether or not a pair has been rerun.
+     * @param con an open database connection to use for the procedure.
+     * @param pairId the id of the pair to check.
+     * @return true if the pair has been rerun, false otherwise.
+     * @throws SQLException on database error.
+     */
+    public static boolean pairHasBeenRerun(Connection con, int pairId) throws SQLException {
         return false;
     }
 
-    public static void markPairAsRerun(int pairId) {
+    /**
+     * Marks a pair as having been rerun in the pairs_rerun table.
+     * @param pairId the id of the pair to mark as having been rerun.
+     * @throws SQLException on database error.
+     */
+    public static void markPairAsRerun(int pairId) throws SQLException {
+
+    }
+    /**
+     * Marks a pair as having been rerun in the pairs_rerun table.
+     * @param con an open database connection to use for the procedure.
+     * @param pairId the id of the pair to mark as having been rerun.
+     * @throws SQLException on database error.
+     */
+    public static void markPairAsRerun(Connection con, int pairId) throws SQLException {
 
     }
 
-    public static void markPairAsRerun(Connection con, int pairId) {
-        
+    // Currently only used for tests.
+    public static void unmarkPairAsRerun(int pairId) throws SQLException {
+
     }
 }
