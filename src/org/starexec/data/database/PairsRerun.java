34acf753805c38b404694b00849158014aa09f4e
agiegerich <albert.a.giegerich@gmail.com>
2016-12-01 16:18:12 +0000

    Filled in implementation of methods interacting with pairs_rerun table.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@23916 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/PairsRerun.java
+++ src/org/starexec/data/database/PairsRerun.java
@@ -2,6 +2,7 @@ package org.starexec.data.database;
 
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -16,7 +17,9 @@ public class PairsRerun {
      * @throws SQLException on database error.
      */
     public static boolean hasPairBeenRerun(int pairId) throws SQLException {
-        return false;
+        return Common.query("{CALL HasPairBeenRerun(?)}"
+                , procedure -> procedure.setInt(1, pairId)
+                , ResultSet::next);
     }
 
     /**
@@ -27,7 +30,9 @@ public class PairsRerun {
      * @throws SQLException on database error.
      */
     public static boolean pairHasBeenRerun(Connection con, int pairId) throws SQLException {
-        return false;
+        return Common.queryUsingConnection(con, "{CALL HasPairBeenRerun(?)}"
+                , procedure -> procedure.setInt(1, pairId)
+                , ResultSet::next);
     }
 
     /**
@@ -36,7 +41,7 @@ public class PairsRerun {
      * @throws SQLException on database error.
      */
     public static void markPairAsRerun(int pairId) throws SQLException {
-
+        Common.update("{CALL MarkPairAsRerun(?)}", procedure -> procedure.setInt(1, pairId));
     }
     /**
      * Marks a pair as having been rerun in the pairs_rerun table.
@@ -45,11 +50,11 @@ public class PairsRerun {
      * @throws SQLException on database error.
      */
     public static void markPairAsRerun(Connection con, int pairId) throws SQLException {
-
+        Common.updateUsingConnection(con, "{CALL MarkPairAsRerun(?)}", procedure -> procedure.setInt(1, pairId));
     }
 
     // Currently only used for tests.
     public static void unmarkPairAsRerun(int pairId) throws SQLException {
-
+        Common.update("{CALL UnmarkPairAsRerun(?)}", procedure -> procedure.setInt(1, pairId));
     }
 }
