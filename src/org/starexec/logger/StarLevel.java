abe5729b1fd990a13eea09458ff8118a867da64c
agiegerich <albert.a.giegerich@gmail.com>
2017-02-06 21:07:19 +0000

    Added procedure for adding error reports. Deleted name field from StarLevel (Java allows name to be automatically retrieved with toString.)
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@24490 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/logger/StarLevel.java
+++ src/org/starexec/logger/StarLevel.java
@@ -9,31 +9,22 @@ import java.io.Serializable;
  * Wrapper class for logging levels.
  */
 public enum StarLevel {
-    OFF(Level.OFF, "OFF"),
-    FATAL(Level.FATAL, "FATAL"),
-    ERROR(Level.ERROR, "ERROR"),
-    WARN(Level.WARN, "WARN"),
-    INFO(Level.INFO, "INFO"),
-    DEBUG(Level.DEBUG, "DEBUG"),
-    TRACE(Level.TRACE, "TRACE"),
-    ALL(Level.ALL, "ALL");
+    OFF(Level.OFF),
+    FATAL(Level.FATAL),
+    ERROR(Level.ERROR),
+    WARN(Level.WARN),
+    INFO(Level.INFO),
+    DEBUG(Level.DEBUG),
+    TRACE(Level.TRACE),
+    ALL(Level.ALL);
 
     private final Level level;
 
-    // Corresponds to values in logging_levels database table.
-    private final String name;
-
-    StarLevel(Level level, String name) {
+    StarLevel(Level level) {
         this.level = level;
-        this.name = name;
     }
 
     Level get() {
         return level;
-    }
-
-    @Override
-    public String toString() {
-        return name;
     }
 }
