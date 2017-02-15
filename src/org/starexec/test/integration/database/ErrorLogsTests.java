3a95c19744fe86686e72443449e72f4433121630
agiegerich <albert.a.giegerich@gmail.com>
2017-02-15 14:47:21 +0000

    Added test
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@24643 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/integration/database/ErrorLogsTests.java
+++ src/org/starexec/test/integration/database/ErrorLogsTests.java
@@ -12,6 +12,7 @@ import org.starexec.util.Util;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -36,23 +37,53 @@ public class ErrorLogsTests extends TestSequence {
 
     }
 
+//    This would delete all logs in production.
+//
+//    @StarexecTest
+//    private void deleteBeforeTest() {
+//        try {
+//            int firstId = ErrorLogs.add("test", StarLevel.DEBUG);
+//            int secondId = ErrorLogs.add("test", StarLevel.DEBUG);
+//            int thirdId = ErrorLogs.add("test", StarLevel.DEBUG);
+//
+//
+//            // This should guarantee that a second passes.
+//            Thread.sleep(1500);
+//            ErrorLogs.deleteBefore(Timestamp.from(Instant.now()));
+//
+//            Assert.assertFalse("First log was still in database.", ErrorLogs.getById(firstId).isPresent());
+//            Assert.assertFalse("Second log was still in database.", ErrorLogs.getById(secondId).isPresent());
+//            Assert.assertFalse("Third log was still in database.", ErrorLogs.getById(thirdId).isPresent());
+//        } catch (SQLException e) {
+//            Assert.fail("Caught SQLException:\n"+ Util.getStackTrace(e));
+//        } catch (InterruptedException e) {
+//            Assert.fail("Thread couldn't sleep.\n" + Util.getStackTrace(e));
+//        }
+//    }
+
     @StarexecTest
-    private void clearBeforeTest() {
+    private void getSinceTest() {
         try {
+            Timestamp begin = Timestamp.from(Instant.now());
+
             int firstId = ErrorLogs.add("test", StarLevel.DEBUG);
             int secondId = ErrorLogs.add("test", StarLevel.DEBUG);
             int thirdId = ErrorLogs.add("test", StarLevel.DEBUG);
 
-            Thread.sleep(500);
-            ErrorLogs.deleteBefore(Timestamp.from(Instant.now()));
+            List<ErrorLog> logs = ErrorLogs.getSince(begin);
 
-            Assert.assertFalse("First log was still in database.", ErrorLogs.getById(firstId).isPresent());
-            Assert.assertFalse("Second log was still in database.", ErrorLogs.getById(secondId).isPresent());
-            Assert.assertFalse("Third log was still in database.", ErrorLogs.getById(thirdId).isPresent());
+
+            // Check that logs we got contain all three logs we added.
+            Assert.assertTrue(logs.stream().anyMatch(l -> l.getId() == firstId));
+            Assert.assertTrue(logs.stream().anyMatch(l -> l.getId() == secondId));
+            Assert.assertTrue(logs.stream().anyMatch(l -> l.getId() == thirdId));
+
+            ErrorLogs.deleteWithId(firstId);
+            ErrorLogs.deleteWithId(secondId);
+            ErrorLogs.deleteWithId(thirdId);
+
         } catch (SQLException e) {
             Assert.fail("Caught SQLException:\n"+ Util.getStackTrace(e));
-        } catch (InterruptedException e) {
-            Assert.fail("Thread couldn't sleep.\n" + Util.getStackTrace(e));
         }
     }
 
