7ba00003320aed3a4c60b88ad963b0e7834950e4
hawks <patrick-hawks@uiowa.edu>
2017-02-20 15:07:00 +0000

    Revert r24693
    `permalink` is const in the sense that it never changes once set, but it is not
    static, and so probably doesn't need SCREAMING_SNAKE_CASE
    
    This is a convenience variable that only exists to aid readability, and should
    be inlined by a JavaScript compiler/preprocessor
    
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@24700 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/explore/spaces.js
+++ WebContent/js/explore/spaces.js
@@ -598,7 +598,7 @@ function initSpaceExplorer(){
 		 * The URL linking directly to this space
 		 * @const {string}
 		 */
-		var PERMALINK = "?id=" + String(id);
+		var permalink = "?id=" + String(id);
 
 		// Update global variable `isLeafSpace`
 		window.isLeafSpace = spaceIsLeaf(id);
@@ -611,7 +611,7 @@ function initSpaceExplorer(){
 		// Replace the current URL with a URL linking directly to this space
 		// We replace rather than push so the Back button still navigates away
 		//   from the Space Explorer
-		window.history.replaceState(null, "Space Explorer", PERMALINK);
+		window.history.replaceState(null, "Space Explorer", permalink);
 
 		// Remove all non-permanent tooltips from the page; helps keep
 		// the page from getting filled with hundreds of qtip divs
