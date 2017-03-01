b1931585ca60e06df87fc36509cbc39bb76a9f03
hawks <patrick-hawks@uiowa.edu>
2017-03-01 04:26:43 +0000

    Use new paginator when running tests
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@24915 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/admin/testing.js
+++ WebContent/js/admin/testing.js
@@ -1,81 +1,54 @@
-var testTable;
-
 $(document).ready(function(){
+	var $tableTests=$("#tableTests");
 	initUI();
 	setInterval(function() {
-		rows = $(testTable).children('tbody').children('tr.row_selected');
+		rows = $tableTests.children("tbody").children("tr.row_selected");
 		if (rows.length==0) {
-			testTable.api().ajax.reload(null,false);
+			$tableTests.dataTable().api().ajax.reload(null,false);
 		}
 	},5000);
 });
 
 
-
 function initUI(){
-	
-	$("#runAll").button({
-		icons: {
-			primary: "ui-icon-check"
-		}
+	var $tableTests=$("#tableTests");
+	var tableConfig = new window.star.DataTableConfig({
+		"sAjaxSource"  : starexecRoot + "services/tests/pagination",
+		"fnServerData" : fnPaginationHandler // included in this file
 	});
-	$("#runSelected").button({
-		icons: {
-			primary: "ui-icon-check"
-		}
-	});
-	$("#runStress").button({
-		icons: {
-			primary: "ui-icon-check"
-		}
-	});
-	
-	$("#runAll").click(function() {
-		$.post(
-				starexecRoot+"services/test/runAllTests",
-				{},
-				function(returnCode) {
-					s=parseReturnCode(returnCode);
 
-					
-				},
-				"json"
+	var buttonStyle = {
+		icons: {
+			primary: "ui-icon-check"
+		}
+	};
+
+	$("#runAll").button(buttonStyle).click(function() {
+		$.post(
+			starexecRoot+"services/test/runAllTests",
+			{},
+			parseReturnCode,
+			"json"
 		);
-		unselectAllRows(testTable);
-		
+		$tableTests.find("tr").removeClass("row_selected");
 	});
-	
-	$("#runSelected").click(function() {
-		nameArray=getSelectedRows(testTable);
+
+	$("#runSelected").button(buttonStyle).click(function() {
+		nameArray=getSelectedRows($tableTests);
 		$.post(
 			starexecRoot+"services/test/runTests",
 			{testNames : nameArray},
-			function(returnCode) {
-				s=parseReturnCode(returnCode);
-
-				
-			},
+			parseReturnCode,
 			"json"
 		);
-		unselectAllRows(testTable);
+		$tableTests.find("tr").removeClass("row_selected");
 	});
-	$("#runStress").click(function() {
+
+	$("#runStress").button(buttonStyle).click(function() {
 		window.open(starexecRoot+"secure/admin/stressTest.jsp");
 	});
-	
-	testTable=$('#tableTests').dataTable( {
-        "sDom"			: getDataTablesDom(),
-        "iDisplayStart"	: 0,
-        "iDisplayLength": defaultPageSize,
-        "bSort": true,
-        "bPaginate": true,
-        "sAjaxSource"	: starexecRoot+"services/tests/pagination",
-        "sServerMethod" : "POST",
-        "fnServerData" : fnPaginationHandler
-    });
 
-	
-	$("#tableTests").on( "click", "tr", function() {
+	$tableTests.dataTable(tableConfig).on( "click", "tr", function() {
 		$(this).toggleClass("row_selected");
 	});
 
@@ -85,38 +58,31 @@ function initUI(){
 /**
  * For a given dataTable, this extracts the id's of the rows that have been
  * selected by the user
- * 
- * @param dataTable the particular dataTable to extract the id's from
+ *
+ * @param {jQuery} $dataTable the particular dataTable to extract the id's from
  * @returns {Array} list of id values for the selected rows
  * @author Todd Elvers
  */
-function getSelectedRows(dataTable){
-	var nameArray = new Array();
-	var rows = $(dataTable).children('tbody').children('tr.row_selected');
+function getSelectedRows($dataTable){
+	var nameArray = [];
+	var rows = $dataTable.children("tbody").children("tr.row_selected");
 	$.each(rows, function(i, row) {
-		nameArray.push($(this).find('a:first').attr("name"));
+		nameArray.push($(this).find("a:first").attr("name"));
 	});
 	return nameArray;
 }
 
-function unselectAllRows(dataTable) {
-	$(dataTable).find("tr").each(function() {
-		$(this).removeClass("row_selected");
-	});
-}
-
 function fnPaginationHandler(sSource, aoData, fnCallback) {
-	$.get(  
-			sSource,
-			function(nextDataTablePage){
-				s=parseReturnCode(nextDataTablePage);
-				if (s) {
-					fnCallback(nextDataTablePage);						
-				}
-
-			},  
-			"json"
+	$.get(
+		sSource,
+		function(nextDataTablePage){
+			var s = parseReturnCode(nextDataTablePage);
+			if (s) {
+				fnCallback(nextDataTablePage);
+			}
+		},
+		"json"
 	).error(function(){
-		showMessage('error',"Internal error populating data table",5000);
+		showMessage("error","Internal error populating data table",5000);
 	});
 }
