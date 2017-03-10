631a2b37daafe9a4bebe14a29d84fb8455367c84
hawks <patrick-hawks@uiowa.edu>
2017-03-10 23:07:57 +0000

    JobPanelView must also show new solver format
    Everything is duplicated all around the site. Nothing is ever as simple as
    just changing one file.
    
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@25113 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/details/jobPanelView.js
+++ WebContent/js/details/jobPanelView.js
@@ -4,7 +4,7 @@ var panelArray=null;
 var useWallclock=true;
 var stageNumber;
 $(document).ready(function(){
-	jobId=$("#jobId").attr("value");	
+	jobId=$("#jobId").attr("value");
 	jobSpaceId=$("#spaceId").attr("value");
 	stageNumber=$("#stageNumber").attr("value");
 
@@ -53,7 +53,7 @@ function initUI() {
 		icons: {
 			primary: "ui-icon-refresh"
 		}
-	
+
 	});
 	$("#changeTime").click(function() {
 		useWallclock=!useWallclock;
@@ -75,7 +75,7 @@ function initUI() {
 		$(".panelField").each(function() {
 			legend = $(this).children('legend:first');
 			isOpen = $(legend).data('open');
-			
+
 			if (!isOpen) {
 				$(legend).trigger("click");
 			}
@@ -86,7 +86,7 @@ function initUI() {
 function getPanelTable(space) {
 	spaceName=space.attr("name");
 	spaceId=parseInt(space.attr("id"));
-	
+
 	table="<fieldset class=\"panelField\">" +
 			"<legend class=\"panelHeader\">"+spaceName+"</legend>" +
 			"<table id=panel"+spaceId+" spaceId=\""+spaceId+"\" class=\"panel\"><thead>" +
@@ -96,52 +96,117 @@ function getPanelTable(space) {
 			"</thead>" +
 			"<tbody></tbody> </table></fieldset>";
 	return table;
-	
+
 }
 
 function initializePanels() {
+	var SOLVER_ID   =  0,
+	    CONFIG_ID   =  1,
+	    SOLVER_NAME =  2,
+	    CONFIG_NAME =  3,
+	    SOLVED      =  4,
+	    TIME        =  5,
+	    STAGE       =  6,
+	    WRONG       =  7,
+	    RESOURCED   =  8,
+	    FAILED      =  9,
+	    UNKNOWN     = 10,
+	    INCOMPLETE  = 11,
+	    CONFLICTS   = 12;
+
+	var linkTemplate = document.createElement("a");
+	linkTemplate.target = "_blank";
+
+	var link = function(url, text) {
+		linkTemplate.href = url;
+		linkTemplate.textContent = text;
+		return linkTemplate.outerHTML;
+	}
+
+/*	var pairsTemplate = ["pairsInSpace.jsp?type=",null,"&sid=",DETAILS_JOB.rootJobSpaceId,"&configid=",null,"&stagenum=",null];
+	var getPairsInSpaceLink = function(type, configId, stageNumber) {
+		pairsTemplate[1] = type;
+		pairsTemplate[5] = configId;
+		pairsTemplate[7] = stageNumber;
+		return pairsTemplate.join("");
+	}
+*/
+
+	var solverTemplate = ["solver.jsp?id=",null]
+	var getSolverLink = function(solver) {
+		solverTemplate[1] = solver;
+		return solverTemplate.join("");
+	}
+
+	var configTemplate = ["configuration.jsp?id=",null]
+	var getConfigLink = function(config) {
+		configTemplate[1] = config;
+		return configTemplate.join("");
+	}
+
+	var formatSolver     = function(row, type, val) {
+		var href = getSolverLink(val[SOLVER_ID]);
+		return link(href, val[SOLVER_NAME]);
+	};
+	var formatConfig     = function(row, type, val) {
+		var href = getConfigLink(val[CONFIG_ID]);
+		return link(href, val[CONFIG_NAME]);
+	};
+	var formatSolved     = function(row, type, val) {
+		return val[SOLVED];
+//		var href = getPairsInSpaceLink("solved", val[CONFIG_ID], val[STAGE]);
+		//		return link(href, val[SOLVED]);
+	};
+	var formatTime       = function(row, type, val) {
+		return (val[TIME]/100).toFixed(1);
+	};
+
+	var panelTableInitializer = new window.star.DataTableConfig({
+		"sDom"          : 'rt<"clear">',
+		"iDisplayLength": 1000, // make sure we show every entry
+		"fnServerData"  : fnShortStatsPaginationHandler,
+		"aoColumns"     : [
+			{"mRender"  : formatSolver},
+			{"mRender"  : formatConfig},
+			{"mRender"  : formatSolved},
+			{"mRender"  : formatTime},
+		]
+	});
+
 	$.getJSON(starexecRoot+"services/space/" +jobId+ "/jobspaces/false?id="+jobSpaceId,function(spaces) {
-		panelArray=new Array();		
-		for (i=0;i<spaces.length;i++) {
-			
-			space=$(spaces[i]);
-			spaceName=space.attr("name");
-			spaceId=parseInt(space.attr("id"));
-			
-			child=getPanelTable(space);
+		panelArray = [];
+		for (var i=0;i<spaces.length;i++) {
+			var space = $(spaces[i]);
+			var spaceId = parseInt(space.attr("id"));
+			var child=getPanelTable(space);
+
 			$("#subspaceSummaryField").append(child);
-			panelArray[i]=$("#panel"+spaceId).dataTable({
-		        "sDom"			: 'rt<"clear">',
-		        "iDisplayStart"	: 0,
-		        "iDisplayLength": 1000, // make sure we show every entry
-		        "sAjaxSource"	: starexecRoot+"services/jobs/solvers/pagination/"+spaceId+"/true/",
-		        "sServerMethod" : "POST",
-		        "fnServerData" : fnShortStatsPaginationHandler
-		    });
+			panelTableInitializer["sAjaxSource"] = starexecRoot+"services/jobs/solvers/pagination/"+spaceId+"/true/";
+			panelArray[i]=$("#panel"+spaceId).dataTable(panelTableInitializer);
 		}
 		$(".viewSubspace").each(function() {
 			$(this).click(function() {
 				spaceId=$(this).parents("table.panel").attr("spaceId");
 				window.location=starexecRoot+"secure/details/jobPanelView.jsp?jobid="+jobId+"&spaceid="+spaceId;
 			});
-			
+
 		});
 		$(".panelField").expandable();
 	});
-	
+
 }
 
 function fnShortStatsPaginationHandler(sSource, aoData, fnCallback) {
-	$.post(  
+	$.post(
 			sSource+useWallclock+'/'+stageNumber,
 			aoData,
 			function(nextDataTablePage){
 				//if the user has clicked on a different space since this was called, we want those results, not these
 				s=parseReturnCode(nextDataTablePage);
 				if (s)  {
-					fnCallback(nextDataTablePage);						
+					fnCallback(nextDataTablePage);
 				}
-			},  
+			},
 			"json"
 	).error(function(){
 		showMessage('error',"Internal error populating data table",5000);
