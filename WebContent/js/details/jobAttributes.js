0c2112e9899b0445da0463322ec46bc5f7f1314f
hawks <patrick-hawks@uiowa.edu>
2017-03-13 02:55:31 +0000

    jobAttributes page broken?
    Things were not loading properly due to the order in which the scripts were
    added to the page; scripts are exectued in the order they appear in the HTML.
    Anything we require should load BEFORE the file that requires it.
    
    I have fixed the error messages in the console, but the page still isn't
    displaying any real data. I'm not sure what it is *supposed* to be displaying so
    this is a difficult problem for me to debug.
    
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@25119 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/details/jobAttributes.js
+++ WebContent/js/details/jobAttributes.js
@@ -1,64 +1,49 @@
 
-$(document).ready(function(){
-    'use strict';
-    var currentJobSpaceId=getParameterByName('id');
-    var rootJobSpaceId=getParameterByName('id');
-    // Initialize the jstree plugin for the explorer list
-    var jobId = $('#data').data('jobid');
-    var spaceExplorerJsonData = getSpaceExplorerJsonData(jobId);
-    initSpaceExplorer(rootJobSpaceId, currentJobSpaceId, spaceExplorerJsonData);
-	setupChangeTimeButton();
+"use strict";
 
-    $('#attributeTotalsTable').dataTable({
-        'bSort': true,
-        "bPaginate": true
-    });
+jQuery(function($) {
+	var currentJobSpaceId=getParameterByName('id');
+	var rootJobSpaceId=getParameterByName('id');
+	var jobId = $('#data').data('jobid'); // Initialize the explorer list
+	var spaceExplorerJsonData = getSpaceExplorerJsonData(jobId);
+	var $table = $('attributeTable');
 
-	var table = $('#attributeTable').dataTable({
-		/*
-		'columnDefs': [
-			{ 'width': '120px', 'targets': '_all' }
-		],*/
+	$('#attributeTotalsTable').dataTable({
+		'bSort': true,
+		"bPaginate": true
+	});
+
+	$table.dataTable({
 		'bSort': false,
+		'fixedColumns': true,
 		'scrollY': '300px',
 		'scrollX': '100%',
 		'scrollCollapse': true,
 		'paging': false
 	});
 
-	new $.fn.dataTable.FixedColumns(table);
-	$(window).resize(function() {
-		table.fnDraw();
-	});
+	$(window).resize($table.fnDraw);
+
+	initSpaceExplorer(rootJobSpaceId, currentJobSpaceId, spaceExplorerJsonData);
+	setupChangeTimeButton();
 });
 
 function getSpaceExplorerJsonData(jobId) {
-    'use strict';
-    var url = starexecRoot+"services/space/" +jobId+ "/jobspaces/true";
-    return {
-        "ajax" : {
-            "url" : url, // Where we will be getting json data from
-            "data" : function (n) {
-                return {
-                    id : (n.attr ? n.attr("id") : 0)
-                }; // What the default space id should be
-            }
-        }
-    };
-}
-function setupChangeTimeButton() {
-
-
-	$(".changeTime").button({
-		icons: {
-			primary: "ui-icon-refresh"
+	var url = starexecRoot+"services/space/" +jobId+ "/jobspaces/true";
+	return {
+		"ajax" : {
+			"url" : url, // Where we will be getting json data from
+			"data" : function (n) {
+				return {
+					id : (n.attr ? n.attr("id") : 0)
+				}; // What the default space id should be
+			}
 		}
-	});
+	};
+}
 
+function setupChangeTimeButton() {
 	var isWallclock = true;
-	$('.cpuSum').hide();
-
-
 	var toggleTime = function() {
 		if (isWallclock) {
 			$('.changeTime .ui-button-text').html('use wallclock time');
@@ -73,61 +58,61 @@ function setupChangeTimeButton() {
 		}
 	}
 
+	$('.cpuSum').hide();
 
-	$(".changeTime").click(toggleTime);
-		
+	$(".changeTime")
+		.button({
+			icons: {
+				primary: "ui-icon-refresh"
+			}
+		})
+		.click(toggleTime);
 }
 
 
 function initSpaceExplorer(rootJobSpaceId, currentJobSpaceId, spaceExplorerJsonData) {
-    // Set the path to the css theme for the jstree plugin
+	// Set the path to the css theme for the jstree plugin
 
-    $.jstree._themes = starexecRoot+"css/jstree/";
+	$.jstree._themes = starexecRoot+"css/jstree/";
 
-    // Initialize the jstree plugin for the explorer list
-    /*$("#exploreList").bind("loaded.jstree", function() {
-        log("exploreList tree has finished loading.");
-        $("#exploreList").jstree("select_node", ".rootNode");
-    })*/
-	console.log('Setting up explore list.');
-    $("#exploreList").jstree({
-        "json_data" : spaceExplorerJsonData,
-        "themes" : {
-            "theme" : "default",
-            "dots" : true,
-            "icons" : true
-        },
-        "types" : {
-            "max_depth" : -2,
-            "max_children" : -2,
-            "valid_children" : [ "space" ],
-            "types" : {
-                "space" : {
-                    "valid_children" : [ "space" ],
-                    "icon" : {
-                        "image" : starexecRoot+"images/jstree/db.png"
-                    }
-                }
-            }
-        },
-        "ui" : {
-            "select_limit" : 1,
-            "selected_parent_close" : "select_parent",
-            "initially_select" : [ "#"+rootJobSpaceId ]
-        },
-        "plugins" : [ "types", "themes", "json_data", "ui", "cookies"] ,
-        "core" : { animation : 200 }
-    }).bind("select_node.jstree", function (event, data) {
-        // Change the page to the appropriate jobspace.
-        var newJobSpaceId = data.rslt.obj.attr("id");
+	log('Setting up explore list.');
+	$("#exploreList").jstree({
+		"json_data" : spaceExplorerJsonData,
+		"themes" : {
+		"theme" : "default",
+		"dots" : true,
+		"icons" : true
+		},
+		"types" : {
+			"max_depth" : -2,
+			"max_children" : -2,
+			"valid_children" : [ "space" ],
+			"types" : {
+				"space" : {
+					"valid_children" : [ "space" ],
+					"icon" : {
+						"image" : starexecRoot+"images/jstree/db.png"
+					}
+				}
+			}
+		},
+		"ui" : {
+			"select_limit" : 1,
+			"selected_parent_close" : "select_parent",
+			"initially_select" : [ "#"+rootJobSpaceId ]
+		},
+		"plugins" : [ "types", "themes", "json_data", "ui", "cookies"] ,
+		"core" : { animation : 200 }
+	}).bind("select_node.jstree", function (event, data) {
+		// Change the page to the appropriate jobspace.
+		var newJobSpaceId = data.rslt.obj.attr("id");
 		log("New job space id: "+newJobSpaceId);
 
 		if (newJobSpaceId !== currentJobSpaceId) {
 			window.location.href=starexecRoot+'secure/details/jobAttributes.jsp?id='+newJobSpaceId;
 		}
-    }).on( "click", "a", function (event, data) {
-        event.preventDefault();  // This just disable's links in the node title
-    });
-    log("Initialized exploreList tree.");
-
+	}).on( "click", "a", function (event, data) {
+		event.preventDefault();  // This just disable's links in the node title
+	});
+	log("Initialized exploreList tree.");
 }
