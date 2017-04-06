7391db22bee006c2ef1943f34364d8ff3e06fa90
hawks <patrick-hawks@uiowa.edu>
2017-04-06 18:53:20 +0000

    Do not create Subspace Summaries until requested
    Subspace Summaries can be quite large, and loading them can cause other parts of
    the page to become unresponsive. This commit defers creating these tables until
    the container they are inside is expanded.
    
    Because JavaScript is a Function Scoped language, I had to do a little dancing
    around to get the lambda function to be created with the appropriate scope.
    Without wrapping the body of the `for` loop inside a function, `$panel` and
    `panelTableInitializer` would always refer to the values they contained during
    the *last* iteration of the loop, rather than during the iteration of the loop
    that created the lambda function as necessary.
    
    There are certainly other ways of doing this, and likely a better way of doing
    this, but I think simply wrapping the body of the loop in a function results in
    the most readable code, and has the benefit of behaving how a Java developer
    would expect it to behave. This seems like a better compromise than throwing
    function parameters back and forth with `bind` magic.
    
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@25463 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/details/job.js
+++ WebContent/js/details/job.js
@@ -1074,7 +1074,12 @@ function handleSpacesData(spaces) {
 		$("#subspaceSummaryField").show();
 	}
 
-	for (var i=0; i<spaces.length; ++i) {
+	/* I'm so sorry, but because JavaScript is a Function scoped language, I
+	 * need to wrap the body of this loop inside an anonymous function so that
+	 * the variables we create are limited to the body of the loop.
+	 * Yay for JavaScript!
+	 */
+	for (var i=0; i<spaces.length; ++i) (function () {
 		var space = $(spaces[i]),
 		    spaceId = parseInt(space.attr("id")),
 		    child = getPanelTable(space);
@@ -1089,33 +1094,37 @@ function handleSpacesData(spaces) {
 		var panelTableInitializer = getPanelTableInitializer(jobId, spaceId);
 		var $panel = $("#panel"+spaceId);
 
-		panelArray[i]=$panel.dataTable(panelTableInitializer);
+		$panel.parents(".panelField").one("open.expandable", function() {
+			var $this = $(this);
+			panelArray.push( $panel.dataTable(panelTableInitializer) );
 
-		/* We want each panel to reload its contents every 30 seconds while
-		 * it is open. If it is closed, it should not reload its contents. Upon
-		 * being opened, it should immediately refresh its contents because we
-		 * don't know how long it has been closed and we do not want to show
-		 * stale data for 30 seconds.
-		 * We can keep track of the interval handle internally as
-		 * `panelRefreshInterval`, but we must also expose it to `clearPanels`.
-		 * For this reason, we will let jQuery save the handle also so that we
-		 * can clear the interval if the panel is cleared.
-		 */
-		var panelRefreshInterval;
-		var reload = $panel.dataTable().api().ajax.reload;
-		$panel.parents(".panelField").on("open.expandable", function() {
-			reload();
-			panelRefreshInterval = window.setInterval(reload, 30000);
-			$panel.data("panelRefreshInterval", panelRefreshInterval);
-		}).on("close.expandable", function() {
-			window.clearInterval(panelRefreshInterval);
+			/* We want each panel to reload its contents every 30 seconds while
+		 	 * it is open. If it is closed, it should not reload its contents. Upon
+		 	 * being opened, it should immediately refresh its contents because we
+		 	 * don't know how long it has been closed and we do not want to show
+		 	 * stale data for 30 seconds.
+		 	 * We can keep track of the interval handle internally as
+		 	 * `panelRefreshInterval`, but we must also expose it to `clearPanels`.
+		 	 * For this reason, we will let jQuery save the handle also so that we
+		 	 * can clear the interval if the panel is cleared.
+		 	 */
+			var panelRefreshInterval;
+			var reload = $panel.dataTable().api().ajax.reload;
+			$this.on("open.expandable", function() {
+				reload();
+				panelRefreshInterval = window.setInterval(reload, 30000);
+				$panel.data("panelRefreshInterval", panelRefreshInterval);
+			}).on("close.expandable", function() {
+				window.clearInterval(panelRefreshInterval);
+			});
 		});
-	}
+	})();
 
 	$(".viewSubspace").click(function() {
-		spaceId=$(this).parents("table.panel").attr("spaceId");
+		var spaceId=$(this).parents("table.panel").attr("spaceId");
 		openSpace(spaceId);
 	});
+
 	$(".panelField").expandable(true);
 }
 
