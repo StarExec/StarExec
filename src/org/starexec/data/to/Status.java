f503c36dcbde9ee075f6297a99c994b97cafedbd
agiegerich <albert.a.giegerich@gmail.com>
2017-04-21 16:55:06 +0000

    Added statComplete as a binary enum (named boolean) field of the StatusCode enum so that statComplete and statIncomplete will remain logically opposite even if new StatusCodes are added.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@25770 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/to/Status.java
+++ src/org/starexec/data/to/Status.java
@@ -16,126 +16,155 @@ import com.google.gson.annotations.Expose;
 // Do NOT reuse codes 3, 5, and 6! We should always use completely new codes
 // for new statuses and never reuse deleted codes.
 public class Status {
+	private enum StatCompleteness {
+		COMPLETE, INCOMPLETE
+	}
 	public enum StatusCode {
 		STATUS_UNKNOWN(0,
 				"the job status is not known or has not been set",
-				"unknown"
+				"unknown",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_PENDING_SUBMIT(
 				1,
 				"the job has been added to the starexec database but has not been submitted to the grid engine",
-				"pending submission"
+				"pending submission",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_ENQUEUED(
 				2,
 				"the job has been submitted to the grid engine and is waiting for an available execution host",
-				"enqueued"
+				"enqueued",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_RUNNING(
 				4,
 				"the job is currently being ran on an execution host",
-				"running"
+				"running",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_COMPLETE(
 				7,
 				"the job has successfully completed execution and its statistics have been received from the grid engine",
-				"complete"
+				"complete",
+				StatCompleteness.COMPLETE
 		),
 		ERROR_SGE_REJECT(
 				8,
 				"the job was sent to the grid engine for execution but was rejected. "
 						+ "this can indicate that there were no available queues or the grid engine is in an unclean state",
-				"rejected"
+				"rejected",
+				StatCompleteness.INCOMPLETE
 		),
 		ERROR_SUBMIT_FAIL(
 				9,
 				"there was an issue submitting your job to the grid engine. "
 						+"this can be caused be unexpected errors raised by the grid engine",
-				"submit failed"
+				"submit failed",
+				StatCompleteness.INCOMPLETE
 		),
 		ERROR_RESULTS(
 				10,
 				"the job completed execution but there was a problem acquiring its statistics or attributes from the grid engine",
-				"results error"
+				"results error",
+				StatCompleteness.INCOMPLETE
 		),
 		ERROR_RUNSCRIPT(
 				11,
 				"the job could not be executed because a valid run script was not present",
-				"run script error"
+				"run script error",
+				StatCompleteness.INCOMPLETE
 		),
 		ERROR_BENCHMARK(
 				12,
 				"the job could not be executed because the benchmark could not be found",
-				"benchmark error"
+				"benchmark error",
+				StatCompleteness.INCOMPLETE
 		),
 		ERROR_DISK_QUOTA_EXCEEDED(
 				13,
 				"the job could not be executed because its execution environment could not be properly set up",
-				"environment error"
+				"environment error",
+				StatCompleteness.INCOMPLETE
 		),
 		EXCEED_RUNTIME(
 				14,
 				"the job was terminated because it exceeded its run time limit",
-				"timeout (wallclock)"
+				"timeout (wallclock)",
+				StatCompleteness.COMPLETE
 		),
 		EXCEED_CPU(
 				15,
 				"the job was terminated because it exceeded its cpu time limit",
-				"timeout (cpu)"
+				"timeout (cpu)",
+				StatCompleteness.COMPLETE
 		),
 		EXCEED_FILE_WRITE(
 				16,
 				"the job was terminated because it exceeded its file write limit",
-				"file write exceeded"
+				"file write exceeded",
+				StatCompleteness.COMPLETE
 		),
 		EXCEED_MEM(
 				17,
 				"the job was terminated because it exceeded its virtual memory limit",
-				"memout"
+				"memout",
+				StatCompleteness.COMPLETE
 		),
 		ERROR_GENERAL(
 				18,
 				"an unknown error occurred which indicates a problem at any point in the job execution pipeline",
-				"error"
+				"error",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_PROCESSING_RESULTS(
 				19,
 				"the job results are currently being processed",
-				"processing results"
+				"processing results",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_PAUSED(
 				20,
 				"the job is paused so all job_pairs that were not complete were sent to this status",
-				"paused"
+				"paused",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_KILLED(
 				21,
 				"the job was killed, so all job_pairs that were not complete were sent to this status",
-				"killed"
+				"killed",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_PROCESSING(
 				22,
 				"this job is being processed by a new post-processor, and this pair is awaiting processing",
-				"awaiting processing"
+				"awaiting processing",
+				StatCompleteness.INCOMPLETE
 		),
 		STATUS_NOT_REACHED(
 				23,
 				"this stage was not reached because a previous stage had some sort of error",
-				"stage not reached"
+				"stage not reached",
+				StatCompleteness.INCOMPLETE
 		),
 		ERROR_BENCH_DEPENDENCY_MISSING(
 				24,
 				"this job pair has a missing benchmark dependency.",
-				"benchmark dependency missing"
+				"benchmark dependency missing",
+				StatCompleteness.INCOMPLETE
 		);
 		
 		public final int val;
 		private int count;
 		public final String description;
 		public final String status;
-
+		private final StatCompleteness statComplete;
 		
-		StatusCode(final int val, final String description, final String status) {
+		StatusCode(
+				final int val,
+				final String description,
+				final String status,
+				final StatCompleteness statComplete) {
 			// Old codes that have been deleted should not be reused.
 			if (val == 3 || val == 5 || val == 6) {
 				throw new IllegalArgumentException("This code has been deleted and should not be reused.");
@@ -144,6 +173,7 @@ public class Status {
 			this.val = val;
 			this.description = description;
 			this.status = status;
+			this.statComplete = statComplete;
 		}		
 		
 		public int getVal() {
@@ -163,10 +193,12 @@ public class Status {
 		}
 		//incomplete as it is defined for stats
 		public boolean statIncomplete(){
-			return (val!=7 && !(val>=14 &&val<=17));
+			//return (val!=7 && !(val>=14 &&val<=17));
+			return this.statComplete == StatCompleteness.INCOMPLETE;
 		}
 		public boolean statComplete() {
-		    return val==7 || (val>=14 &&val<=17);
+		    //return val==7 || (val>=14 &&val<=17);
+			return this.statComplete == StatCompleteness.COMPLETE;
 		}
 		public boolean finishedRunning() {
 			return val>=7;
