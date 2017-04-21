66850b688a8f96f0284f01448fbb8efaec262df8
agiegerich <albert.a.giegerich@gmail.com>
2017-04-21 16:06:34 +0000

    Wrote tests asserting which statuses are complete and are not complete
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@25764 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/junit/data/to/StatusTests.java
+++ src/org/starexec/test/junit/data/to/StatusTests.java
@@ -1,6 +1,7 @@
 package org.starexec.test.junit.data.to;
 
-import java.util.List;
+import java.util.EnumSet;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.starexec.data.to.Status;
@@ -9,10 +10,47 @@ import org.starexec.data.to.Status.StatusCode;
 public class StatusTests {
 
 	@Test
-	public void runningStatusIsRunning() {
-		StatusCode running = StatusCode.STATUS_RUNNING;
-		assertTrue(running.running());
-	}	
+	public void runningTest() {
+		for (StatusCode status : EnumSet.allOf(StatusCode.class)) {
+			if (status == StatusCode.STATUS_RUNNING) {
+				assertTrue(status.running());
+			} else {
+				assertFalse(status.running());
+			}
+		}
+	}
 
-	
+	@Test
+	public void completeTest() {
+		for (StatusCode status : EnumSet.allOf(StatusCode.class)) {
+			if (status == StatusCode.STATUS_COMPLETE 
+					|| status == StatusCode.EXCEED_RUNTIME 
+					|| status == StatusCode.EXCEED_CPU
+					|| status == StatusCode.EXCEED_FILE_WRITE 
+					|| status == StatusCode.EXCEED_MEM) {
+				assertTrue("The "+status.toString()+" status should be complete.", status.complete());
+			} else {
+				assertFalse("The " + status.toString() + " status should not be complete.", status.complete());
+			}
+		}
+	}
+
+	/*
+	@Test
+	public void completeTest() {
+		for (StatusCode status : EnumSet.allOf(StatusCode.class)) {
+			switch(status) {
+				case STATUS_COMPLETE:
+				case EXCEED_RUNTIME:
+				case EXCEED_CPU:
+				case EXCEED_FILE_WRITE:
+				case EXCEED_MEM:
+					assertTrue("The "+status.toString()+" status should be complete.", status.complete());
+					break;
+				default:
+					assertFalse("The "+status.toString()+" status should not be complete.", status.complete());
+			}
+		}
+	}
+	*/
 }
