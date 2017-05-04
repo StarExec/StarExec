6861fe4a657d3f676750d9df12dd0fda2aa99d66
agiegerich <albert.a.giegerich@gmail.com>
2017-05-04 01:13:28 +0000

    Factored out copied and pasted code.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@25971 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/command/Connection.java
+++ src/org/starexec/command/Connection.java
@@ -1101,7 +1101,7 @@ public class Connection {
 	 */
 
 	public int rerunJob(Integer jobID) {
-		return rerunPairOrJobHelper(baseURL + C.URL_RERUNJOB, jobID);
+		return makePostWithId(baseURL + C.URL_RERUNJOB, jobID);
 	}
 
 	/**
@@ -1112,10 +1112,10 @@ public class Connection {
 	 */
 
 	public int rerunPair(Integer pairID) {
-		return rerunPairOrJobHelper(baseURL + C.URL_RERUNPAIR, pairID);
+		return makePostWithId(baseURL + C.URL_RERUNPAIR, pairID);
 	}
 
-	private int rerunPairOrJobHelper(final String url, final Integer id) {
+	private int makePostWithId(final String url, final Integer id) {
 		HttpResponse response = null;
 		try {
 			String URL = url;
@@ -1159,23 +1159,7 @@ public class Connection {
 			} else {
 				URL = URL.replace("{method}", "resume");
 			}
-			URL = URL.replace("{id}", jobID.toString());
-			HttpPost post = new HttpPost(URL);
-			post = (HttpPost) setHeaders(post);
-			post.setEntity(new UrlEncodedFormEntity(new ArrayList<>(), "UTF-8"));
-			response = executeGetOrPost(post);
-			JsonObject obj = JsonHandler.getJsonObject(response);
-
-			boolean success = JsonHandler.getSuccessOfResponse(obj);
-			String message = JsonHandler.getMessageOfResponse(obj);
-
-			if (success) {
-				return 0;
-			} else {
-				setLastError(message);
-				return Status.ERROR_SERVER;
-			}
-
+			return makePostWithId(URL, jobID);
 		} catch (Exception e) {
 			return Status.ERROR_INTERNAL;
 		} finally {
