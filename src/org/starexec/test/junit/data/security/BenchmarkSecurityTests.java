c7af46563b4814a65f7bd02a7e46c187d33e5011
agiegerich <albert.a.giegerich@gmail.com>
2017-05-11 23:22:22 +0000

    added test
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26079 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
+++ src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
@@ -1,11 +1,37 @@
 package org.starexec.test.junit.data.security;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertFalse;
+import static org.mockito.BDDMockito.given;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.starexec.app.RESTHelpers;
+import org.starexec.data.database.Benchmarks;
+import org.starexec.data.security.BenchmarkSecurity;
+import org.starexec.data.to.Benchmark;
+import org.starexec.util.Util;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Benchmarks.class})
 public class BenchmarkSecurityTests {
+    @Before
+    public void initialize() {
+        PowerMockito.mockStatic(Benchmarks.class);
+    }
 
     @Test
-    public void canUserDownloadBenchmarkTest() {
-
+    public void canUserDownloadBenchmarkGetsNullBenchTest() {
+		int benchId = 1;
+		int userId = 2;
+        given(Benchmarks.get(benchId)).willReturn(null);
+        assertFalse(BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
     }
 }
+
