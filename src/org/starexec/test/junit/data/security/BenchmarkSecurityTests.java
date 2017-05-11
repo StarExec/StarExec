2c8f4dae214446a5876052492b808015a6137cbf
agiegerich <albert.a.giegerich@gmail.com>
2017-05-11 23:11:27 +0000

    Added stub for test.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26078 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
@@ -0,0 +1,11 @@
+package org.starexec.test.junit.data.security;
+
+import org.junit.Test;
+
+public class BenchmarkSecurityTests {
+
+    @Test
+    public void canUserDownloadBenchmarkTest() {
+
+    }
+}
