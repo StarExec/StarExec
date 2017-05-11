5d7ca1849bd37ce0e6dea6b05f1a691b7be7dac9
agiegerich <albert.a.giegerich@gmail.com>
2017-05-11 13:12:10 +0000

    Added test stub
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26065 bd178f5f-7bea-4014-844f-495ca19b8805

---
+++ src/org/starexec/test/junit/app/RESTServicesTests.java
@@ -0,0 +1,12 @@
+package org.starexec.test.junit.app;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RESTServicesTests {
+    @Test
+    public void copyToStarDevTest() {
+        assertTrue(true);
+    }
+}
