9ad3ad0ac1e76879e27281253923f9b288371d12
agiegerich <albert.a.giegerich@gmail.com>
2017-05-11 23:53:59 +0000

    100% code coverage for canUserDownloadBenchmark as well as covering additional possibilities.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26080 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
+++ src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
@@ -3,35 +3,116 @@ package org.starexec.test.junit.data.security;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertFalse;
-import static org.mockito.BDDMockito.given;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.starexec.app.RESTHelpers;
 import org.starexec.data.database.Benchmarks;
+import org.starexec.data.database.Permissions;
 import org.starexec.data.security.BenchmarkSecurity;
+import org.starexec.data.security.GeneralSecurity;
 import org.starexec.data.to.Benchmark;
-import org.starexec.util.Util;
 
-import static org.mockito.Matchers.anyInt;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Benchmarks.class})
+@PrepareForTest({Benchmarks.class, Permissions.class, GeneralSecurity.class})
 public class BenchmarkSecurityTests {
+
     @Before
     public void initialize() {
         PowerMockito.mockStatic(Benchmarks.class);
+        PowerMockito.mockStatic(Permissions.class);
+        PowerMockito.mockStatic(GeneralSecurity.class);
     }
 
     @Test
     public void canUserDownloadBenchmarkGetsNullBenchTest() {
-		int benchId = 1;
-		int userId = 2;
+        int benchId = 1;
+        int userId = 2;
         given(Benchmarks.get(benchId)).willReturn(null);
         assertFalse(BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
     }
-}
 
+    @Test
+    public void canUserDownloadBenchmarkWhenUserCantSeeBenchTest() {
+        int benchId = 1;
+        int userId = 2;
+        given(Benchmarks.get(benchId)).willReturn(mock(Benchmark.class));
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(false);
+        assertFalse(BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void canUserDownloadBenchmarkWhenUserHasAdminPrivileges() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        given(bench.isDownloadable()).willReturn(false);
+        // Then benchmark is not the users benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(true);
+        // The user has admin privileges.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(true);
+
+        assertTrue("The user is an admin so should be able to download the benchmark.",
+                BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void canUserDownloadBenchmarkWhenUserIsOwner() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        given(bench.isDownloadable()).willReturn(false);
+        // Then benchmark is not the users benchmark
+        given(bench.getUserId()).willReturn(userId);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(true);
+        // The user has admin privileges.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+
+        assertTrue("The user is owner so should be able to download the benchmark.",
+                BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void canUserDownloadBenchmarkWhenBenchmarkIsDownloadable() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        given(bench.isDownloadable()).willReturn(true);
+        // Then benchmark is not the users benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(true);
+        // The user has admin privileges.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+
+        assertTrue("The benchmark is downloadable so user should be able to download.",
+                BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
+    }
+    @Test
+    public void canUserDownloadBenchmarkWhenUserIsNotAdminBenchIsNotDownloadableAndUserIsNotOwner() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        given(bench.isDownloadable()).willReturn(false);
+        // Then benchmark is not the users benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(true);
+        // The user has admin privileges.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+
+        assertFalse("The user should not be able to download the benchmark.",
+                BenchmarkSecurity.canUserDownloadBenchmark(benchId, userId).isSuccess());
+    }
+}
