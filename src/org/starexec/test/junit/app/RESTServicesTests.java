f50423ce948df0eece31e79bd168c02eba3b01b3
agiegerich <albert.a.giegerich@gmail.com>
2017-05-11 14:02:19 +0000

    Added unit test for copy-to-stardev.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26068 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/junit/app/RESTServicesTests.java
+++ src/org/starexec/test/junit/app/RESTServicesTests.java
@@ -1,12 +1,43 @@
 package org.starexec.test.junit.app;
-
-
+import com.google.gson.Gson;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.starexec.app.RESTHelpers;
+import org.starexec.app.RESTServices;
+import org.starexec.data.security.ValidatorStatusCode;
+import org.starexec.data.to.enums.Primitive;
+import org.starexec.util.Util;
 
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({RESTHelpers.class, Util.class})
 public class RESTServicesTests {
+
+	Gson gson = new Gson();
+	@Before
+	public void initialize() {
+		PowerMockito.mockStatic(RESTHelpers.class);
+		PowerMockito.mockStatic(Util.class);
+	}
+
     @Test
-    public void copyToStarDevTest() {
-        assertTrue(true);
+    public void copyToStarDevFailValidationTest() {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		final String type = Primitive.BENCHMARK.toString();
+		ValidatorStatusCode failedValidation = new ValidatorStatusCode(false);
+		given(RESTHelpers.validateCopyToStardev(request, type)).willReturn(failedValidation);
+
+		RESTServices services = new RESTServices();
+		final String instance = "test_instance";
+		assertEquals("Should fail validation.", services.copyToStarDev(instance, type, 10, request), gson.toJson(failedValidation));
     }
 }
