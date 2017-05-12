11aa37da7cd1ed50c61374ee1dd2b5581037c96e
agiegerich <albert.a.giegerich@gmail.com>
2017-05-12 19:20:45 +0000

    Finished writing tests for all methods in BenchmarkSecurity.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26098 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
+++ src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
@@ -6,16 +6,14 @@ import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.starexec.data.database.Benchmarks;
-import org.starexec.data.database.Permissions;
-import org.starexec.data.database.Processors;
+import org.starexec.data.database.*;
 import org.starexec.data.security.BenchmarkSecurity;
 import org.starexec.data.security.GeneralSecurity;
 import org.starexec.data.security.ProcessorSecurity;
 import org.starexec.data.security.ValidatorStatusCode;
-import org.starexec.data.to.Benchmark;
-import org.starexec.data.to.Processor;
+import org.starexec.data.to.*;
 import org.starexec.data.to.enums.ProcessorType;
+import org.starexec.servlets.BenchmarkUploader;
 import org.starexec.util.Validator;
 
 import static org.junit.Assert.assertFalse;
@@ -25,7 +23,16 @@ import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.mock;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Benchmarks.class, Permissions.class, GeneralSecurity.class, Validator.class, Processors.class, ProcessorSecurity.class})
+@PrepareForTest({
+        Benchmarks.class,
+        Permissions.class,
+        GeneralSecurity.class,
+        Validator.class,
+        Processors.class,
+        ProcessorSecurity.class,
+        Users.class,
+        Uploads.class
+})
 public class BenchmarkSecurityTests {
 
     @Before
@@ -36,6 +43,8 @@ public class BenchmarkSecurityTests {
         PowerMockito.mockStatic(Validator.class);
         PowerMockito.mockStatic(Processors.class);
         PowerMockito.mockStatic(ProcessorSecurity.class);
+        PowerMockito.mockStatic((Users.class));
+        PowerMockito.mockStatic((Uploads.class));
     }
 
     @Test
@@ -599,9 +608,164 @@ public class BenchmarkSecurityTests {
         assertTrue("User has admin privileges so should be true.", success);
     }
 
-    // TODO: still needs tests for:
-    // canUserRecycleOrphanedBenchmarks
-    // canGetJsonBenchmark
-    // canUserSeeBenchmarkStatus
-}
+    @Test
+    public void userCantRecycleOrphanedBenchmarksIfUserCantBeFound() {
+        // given
+        given(Users.get(anyInt())).willReturn(null);
 
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canUserRecycleOrphanedBenchmarks(1, 2);
+
+        // then
+        assertFalse("User can't recycle orphaned benchmarks if owner cant be found.", status.isSuccess());
+    }
+
+    @Test
+    public void userCantRecycleOrphanedBenchmarksIfUserIsNotAdminOrOwner() {
+        // given
+        int owner = 1;
+        int userId = 2;
+        given(Users.get(anyInt())).willReturn(mock(User.class));
+        given(GeneralSecurity.hasAdminWritePrivileges(userId)).willReturn(false);
+
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canUserRecycleOrphanedBenchmarks(owner, userId);
+
+        // then
+        assertFalse("User is neither owner or admin so cannot recycle orphaned benchmarks.", status.isSuccess());
+    }
+
+    @Test
+    public void userCanRecycleOrphanedBenchmarksIfAdmin() {
+        // given
+        int owner = 1;
+        int userId = 2;
+        given(Users.get(anyInt())).willReturn(mock(User.class));
+
+        // user is admin
+        given(GeneralSecurity.hasAdminWritePrivileges(userId)).willReturn(true);
+
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canUserRecycleOrphanedBenchmarks(owner, userId);
+
+        // then
+        assertTrue("User is admin so should be able to recycle orphaned benchmarks.", status.isSuccess());
+    }
+
+    @Test
+    public void userCanRecycleOrphanedBenchmarksIfOwner() {
+        // given
+
+        // user is owner
+        int owner = 1;
+        int userId = owner;
+        given(Users.get(anyInt())).willReturn(mock(User.class));
+        // user is not admin
+        given(GeneralSecurity.hasAdminWritePrivileges(userId)).willReturn(false);
+
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canUserRecycleOrphanedBenchmarks(owner, userId);
+
+        // then
+        assertTrue("User is owner so should be able to recycle orphaned benchmarks.", status.isSuccess());
+    }
+
+    @Test
+    public void userCantGetBenchmarkJsonIfUserCantSeeBenchmark() {
+        // given
+        given(Permissions.canUserSeeBench(anyInt(), anyInt())).willReturn(false);
+
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canGetJsonBenchmark(1, 2);
+
+        // then
+        assertFalse("User cannot see benchmark so they cannot get the JSON.", status.isSuccess());
+    }
+
+    @Test
+    public void userCantGetBenchmarkJsonIfBenchmarkCantBeFound() {
+        // given
+        given(Permissions.canUserSeeBench(anyInt(), anyInt())).willReturn(true);
+        given(Benchmarks.getIncludeDeletedAndRecycled(anyInt(), anyBoolean())).willReturn(null);
+
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canGetJsonBenchmark(1, 2);
+
+        // then
+        assertFalse("Benchmark cant be found so user cannot get the JSON.", status.isSuccess());
+    }
+
+    @Test
+    public void userCanGetBenchmarkJsonIfUserCanSeeBenchmarkAndItCanBeFound() {
+        // given
+        given(Permissions.canUserSeeBench(anyInt(), anyInt())).willReturn(true);
+        given(Benchmarks.getIncludeDeletedAndRecycled(anyInt(), anyBoolean())).willReturn(mock(Benchmark.class));
+
+        // when
+        ValidatorStatusCode status = BenchmarkSecurity.canGetJsonBenchmark(1, 2);
+
+        // then
+        assertTrue("User should be able to get Benchmark JSON since they can see it.", status.isSuccess());
+    }
+
+    @Test
+    public void userCantSeeBenchmarkUploadStatusIfItCantBeFound() {
+        // given
+        given(Uploads.getBenchmarkStatus(anyInt())).willReturn(null);
+
+        // when
+        boolean success = BenchmarkSecurity.canUserSeeBenchmarkStatus(1, 2);
+
+        // then
+        assertFalse("User should not be able to see benchmark upload status if it cannot be found.", success);
+    }
+
+    @Test
+    public void userCanSeeBenchmarkUploadStatusIfUserHasAdminPrivileges() {
+        // given
+        given(Uploads.getBenchmarkStatus(anyInt())).willReturn(mock(BenchmarkUploadStatus.class));
+        given(GeneralSecurity.hasAdminReadPrivileges(anyInt())).willReturn(true);
+
+        // when
+        boolean success = BenchmarkSecurity.canUserSeeBenchmarkStatus(1, 2);
+
+        // then
+        assertTrue("User is admin so should be able to see bench upload status.", success);
+    }
+
+    @Test
+    public void userCantSeeBenchmarkUploadStatusIfUserIsNotOwner() {
+        // given
+        int userId = 2;
+        int owner = 3;
+        BenchmarkUploadStatus uploadStatus = mock(BenchmarkUploadStatus.class);
+        given(uploadStatus.getUserId()).willReturn(owner);
+        given(Uploads.getBenchmarkStatus(anyInt())).willReturn(uploadStatus);
+        // User is not admin.
+        given(GeneralSecurity.hasAdminReadPrivileges(anyInt())).willReturn(false);
+
+        // when
+        boolean success = BenchmarkSecurity.canUserSeeBenchmarkStatus(1, userId);
+
+        // then
+        assertFalse("User is not owner so should not be able to see upload status.", success);
+    }
+
+    @Test
+    public void userCanSeeBenchmarkUploadStatusIfUserIsOwner() {
+        // given
+        int userId = 2;
+        int owner = userId;
+        BenchmarkUploadStatus uploadStatus = mock(BenchmarkUploadStatus.class);
+        given(uploadStatus.getUserId()).willReturn(owner);
+        given(Uploads.getBenchmarkStatus(anyInt())).willReturn(uploadStatus);
+        // User is not admin.
+        given(GeneralSecurity.hasAdminReadPrivileges(anyInt())).willReturn(false);
+
+        // when
+        boolean success = BenchmarkSecurity.canUserSeeBenchmarkStatus(1, userId);
+
+        // then
+        assertTrue("User is owner so should be able to see upload status.", success);
+    }
+}
