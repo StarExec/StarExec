9dd7d94c58906deb2a31b14e0b253d24b637e6b0
agiegerich <albert.a.giegerich@gmail.com>
2017-05-12 00:39:26 +0000

    Added six new tests for 100% code coverage of canUserSeeBenchmarkContents.
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26083 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
+++ src/org/starexec/test/junit/data/security/BenchmarkSecurityTests.java
@@ -139,5 +139,95 @@ public class BenchmarkSecurityTests {
         assertFalse("Only admins and owners should be able to get the anon link.",
                 BenchmarkSecurity.canUserGetAnonymousLink(benchId, userId).isSuccess());
     }
-}
 
+    @Test
+    public void canUserSeeBenchmarkContentsWhenBenchIsNull() {
+        int benchId = 1;
+        int userId = 2;
+        given(Benchmarks.get(benchId)).willReturn(null);
+        assertFalse("Should be false for null benchmark.", BenchmarkSecurity.canUserSeeBenchmarkContents(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void userCanSeeBenchmarkContentsIfUserIsOwner() {
+        int benchId = 1;
+        int userId = 2;
+        Benchmark bench = mock(Benchmark.class);
+        // User owns this benchmark.
+        given(bench.getUserId()).willReturn(userId);
+        // user is not admin
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        assertTrue("Should be true since user owns the benchmark", BenchmarkSecurity.canUserSeeBenchmarkContents(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void userCanSeeBenchmarkContentsIfUserIsAdmin() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        // User does not own this benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        // user is admin
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(true);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        assertTrue("Should be true since user is admin", BenchmarkSecurity.canUserSeeBenchmarkContents(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void userCanSeeBenchmarkContentsIfBenchIsDownloadableAndUserCanSeeBench() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        // User does not own this benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        // bench is downloadable
+        given(bench.isDownloadable()).willReturn(true);
+        given(bench.getId()).willReturn(benchId);
+        // user is not admin.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(true);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        assertTrue("Should be true since bench is downloadable and user can see the benchmark.",
+                BenchmarkSecurity.canUserSeeBenchmarkContents(benchId, userId).isSuccess());
+
+    }
+
+    @Test
+    public void userCannotSeeBenchmarkContentsIfBenchIsNotDownloadable() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        // User does not own this benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        // bench is downloadable
+        given(bench.isDownloadable()).willReturn(false);
+        // user is not admin.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(true);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        assertFalse("Should be false since bench is not downloadable.",
+                BenchmarkSecurity.canUserSeeBenchmarkContents(benchId, userId).isSuccess());
+    }
+
+    @Test
+    public void userCannotSeeBenchmarkContentsIfUserCannotSeeBench() {
+        int benchId = 1;
+        int userId = 2;
+        int otherUserId = 3;
+        Benchmark bench = mock(Benchmark.class);
+        // User does not own this benchmark
+        given(bench.getUserId()).willReturn(otherUserId);
+        // bench is downloadable
+        given(bench.isDownloadable()).willReturn(true);
+        // user is not admin.
+        given(GeneralSecurity.hasAdminReadPrivileges(userId)).willReturn(false);
+        given(Permissions.canUserSeeBench(benchId, userId)).willReturn(false);
+        given(Benchmarks.get(benchId)).willReturn(bench);
+        assertFalse("Should be true false since user cant see bench.",
+                BenchmarkSecurity.canUserSeeBenchmarkContents(benchId, userId).isSuccess());
+    }
+}
