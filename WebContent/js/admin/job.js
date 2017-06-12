c3aff2962b21fb3b9ba984c7beefa1dd3d63247c
hawks <patrick-hawks@uiowa.edu>
2017-06-12 18:15:00 +0000

    General JS cleanup
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26206 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/admin/job.js
+++ WebContent/js/admin/job.js
@@ -1,8 +1,6 @@
 "use strict";
 
 jQuery(function($) {
-	initButton();
-
 	var formatName = function(row, type, val) {
 		return val["name"];
 	};
@@ -20,95 +18,93 @@ jQuery(function($) {
 	};
 
 	// Setup the DataTable objects
-	var config = new star.DataTableConfig({
-		"sAjaxSource"   : starexecRoot+"services/jobs/admin/pagination",
-		"aoColumns"     : [
-			{"mRender"  : formatName },
-			{"mRender"  : formatUser },
-			{"mRender"  : formatStatus },
-			{"mRender"  : formatQueue },
-		]
-	});
-	$("#jobs").dataTable(config);
+	$("#jobs").dataTable(
+		new star.DataTableConfig({
+			"sAjaxSource"   : starexecRoot+"services/jobs/admin/pagination",
+			"aoColumns"     : [
+				{"mRender"  : formatName },
+				{"mRender"  : formatUser },
+				{"mRender"  : formatStatus },
+				{"mRender"  : formatQueue },
+			]
+		})
+	);
 
-	$('#jobs tbody').on('click', "tr", function () {
-		   $(this).toggleClass( 'row_selected' );
-		} );
+	$("#jobs tbody").on("click", "tr", function() {
+//		.click(function() {
+			$(this).toggleClass("row_selected");
+		})
+	;
 
+	$("#pauseAll")
+		.button({
+			icons: {
+				primary: "ui-icon-pause"
+			}
+		})
+		.click(function() {
+			$("#dialog-confirm-pause-txt").text("are you sure you want to pause all running jobs?");
+
+			$("#dialog-confirm-pause").dialog({
+				modal: true,
+				width: 380,
+				height: 165,
+				buttons: {
+					"OK": function() {
+						$("#dialog-confirm-pause").dialog("close");
+						$.post(
+								starexecRoot+"services/admin/pauseAll/",
+								function(returnCode) {
+									s=parseReturnCode(returnCode);
+									if (s) {
+										setTimeout(function(){document.location.reload(true);}, 1000);
+									}
+
+								},
+								"json"
+						);
+					},
+					"cancel": function() {
+						$(this).dialog("close");
+					}
+				}
+			});
+		})
+	;
+
+	$("#resumeAll")
+		.button({
+			icons: {
+				primary: "ui-icon-play"
+			}
+		})
+		.click(function() {
+			$("#dialog-confirm-pause-txt").text("are you sure you want to resume all admin paused jobs?");
+
+			$("#dialog-confirm-pause").dialog({
+				modal: true,
+				width: 380,
+				height: 165,
+				buttons: {
+					"OK": function() {
+						$("#dialog-confirm-pause").dialog("close");
+						$.post(
+								starexecRoot+"services/admin/resumeAll/",
+								function(returnCode) {
+									s=parseReturnCode(returnCode);
+									if (s) {
+										setTimeout(function(){document.location.reload(true);}, 1000);
+
+									}
+								},
+								"json"
+						);
+					},
+					"cancel": function() {
+						$(this).dialog("close");
+					}
+				}
+			});
+		})
+	;
 });
-
-function initButton() {
-	$("#pauseAll").button({
-		icons: {
-			primary: "ui-icon-pause"
-		}
-	});
-
-	$("#resumeAll").button({
-		icons: {
-			primary: "ui-icon-play"
-		}
-	});
-
-	$("#pauseAll").click(function() {
-		$('#dialog-confirm-pause-txt').text('are you sure you want to pause all running jobs?');
-
-		$('#dialog-confirm-pause').dialog({
-			modal: true,
-			width: 380,
-			height: 165,
-			buttons: {
-				'OK': function() {
-					log('user confirmed to pause all running jobs');
-					$('#dialog-confirm-pause').dialog('close');
-					$.post(
-							starexecRoot+"services/admin/pauseAll/",
-							function(returnCode) {
-								s=parseReturnCode(returnCode);
-								if (s) {
-									setTimeout(function(){document.location.reload(true);}, 1000);
-								}
-
-							},
-							"json"
-					);
-				},
-				"cancel": function() {
-					log('user canceled pause all running jobs');
-					$(this).dialog("close");
-				}
-			}
-		});
-	});
-
-	$("#resumeAll").click(function() {
-		$('#dialog-confirm-pause-txt').text('are you sure you want to resume all admin paused jobs?');
-
-		$('#dialog-confirm-pause').dialog({
-			modal: true,
-			width: 380,
-			height: 165,
-			buttons: {
-				'OK': function() {
-					log('user confirmed to resume all admin paused jobs');
-					$('#dialog-confirm-pause').dialog('close');
-					$.post(
-							starexecRoot+"services/admin/resumeAll/",
-							function(returnCode) {
-								s=parseReturnCode(returnCode);
-								if (s) {
-									setTimeout(function(){document.location.reload(true);}, 1000);
-
-								}
-							},
-							"json"
-					);
-				},
-				"cancel": function() {
-					log('user canceled resume all admin paused jobs');
-					$(this).dialog("close");
-				}
-			}
-		});
-	});
-}
