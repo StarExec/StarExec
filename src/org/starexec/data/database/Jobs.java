5505301ab3b98c828af70414901a6f12d0f1a4c9
hawks <patrick-hawks@uiowa.edu>
2017-06-16 17:04:21 +0000

    Centralize Job creation
    Only one `new Job()`
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26232 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/Jobs.java
+++ src/org/starexec/data/database/Jobs.java
@@ -1000,22 +1000,46 @@ public class Jobs {
 		return get(jobId, false, true);
 	}
 
-	public static Job resultsToJob(ResultSet results) throws SQLException {
-		Job j = new Job();
+	/**
+	 * Converts SQL result into a Job with minimal fields set
+	 *
+	 * @param results
+	 * @return Job with minimal information set
+	 * @throws SQLException
+	 */
+	public static Job resultsToLiteJob(ResultSet results) throws SQLException {
+		final boolean deleted = results.getBoolean("deleted");
+		final String name = results.getString("name") + (deleted ? " (deleted)" : "")
+
+		final Job j = new Job();
+		j.setName(name);
+		j.setDeleted(deleted);
 		j.setId(results.getInt("jobs.id"));
 		j.setUserId(results.getInt("user_id"));
-		j.setName(results.getString("name"));
-		j.setPrimarySpace(results.getInt("primary_space"));
 		j.setCreateTime(results.getTimestamp("created"));
 		j.setCompleteTime(results.getTimestamp("completed"));
+		j.setDescription(results.getString("description"));
+		j.setTotalPairs(results.getInt("total_pairs"));
+		j.setDiskSize(results.getLong("disk_size"));
+
+		return j;
+	}
+
+	/**
+	 * Converts SQL results into a Job with all fields set
+	 *
+	 * @param results
+	 * @return Job with all infomation set
+	 * @throws SQLException
+	 */
+	public static Job resultsToJob(ResultSet results) throws SQLException {
+		final Job j = resultsToLiteJob(results);
+		j.setPrimarySpace(results.getInt("primary_space"));
 		j.setCpuTimeout(results.getInt("cpuTimeout"));
 		j.setWallclockTimeout(results.getInt("clockTimeout"));
 		j.setMaxMemory(results.getLong("maximum_memory"));
 		j.setBuildJob(results.getBoolean("buildJob"));
-		j.setDescription(results.getString("description"));
 		j.setSeed(results.getLong("seed"));
-		j.setTotalPairs(results.getInt("total_pairs"));
-		j.setDiskSize(results.getLong("disk_size"));
 		j.setSuppressTimestamp(results.getBoolean("suppress_timestamp"));
 		j.setUsingDependencies(results.getBoolean("using_dependencies"));
 		j.setBenchmarkingFramework(BenchmarkingFramework.valueOf(results.getString("benchmarking_framework")));
@@ -1027,6 +1051,7 @@ public class Jobs {
 		} else {
 			j.setLowPriority();
 		}
+
 		return j;
 	}
 
@@ -2591,19 +2616,7 @@ public class Jobs {
 				Integer errorPercentage = Math.round(100f * results.getInt("errorPairs") / results.getInt("totalPairs"));
 				liteJobPairStats.put("errorPercentage", errorPercentage);
 
-				Job j = new Job();
-
-				j.setId(results.getInt("id"));
-				j.setName(results.getString("name"));
-				j.setUserId(results.getInt("user_id"));
-				if (results.getBoolean("deleted")) {
-					j.setName(j.getName() + " (deleted)");
-				}
-				j.setDeleted(results.getBoolean("deleted"));
-				j.setDescription(results.getString("description"));
-				j.setCreateTime(results.getTimestamp("created"));
-				j.setCompleteTime(results.getTimestamp("completed"));
-				j.setDiskSize(results.getLong("disk_size"));
+				final Job j = resultsToLiteJob(results);
 				j.setLiteJobPairStats(liteJobPairStats);
 
 				try {
