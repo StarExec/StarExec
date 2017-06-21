e9e31a3d468bcb3d0219d439f5d9479ae21ee824
hawks <patrick-hawks@uiowa.edu>
2017-06-21 23:23:04 +0000

    Reformat and cleanup
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26260 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/servlets/UploadJobXML.java
+++ src/org/starexec/servlets/UploadJobXML.java
@@ -31,139 +31,125 @@ import java.util.List;
 /**
  * Allows for the creation of job pairs as represented in xml. Files can come in .zip,
  * .tar, or .tar.gz format.
- * 
+ *
  * @author Tim Smith
  */
 @MultipartConfig
 public class UploadJobXML extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
 	private static final StarLogger log = StarLogger.getLogger(UploadJobXML.class);
 	private static final String UPLOAD_FILE = "f";
-	private DateFormat shortDate = new SimpleDateFormat(R.PATH_DATE_FORMAT);
+	private static final DateFormat shortDate = new SimpleDateFormat(R.PATH_DATE_FORMAT);
 	private static final String[] extensions = {".tar", ".tar.gz", ".tgz", ".zip"};
 	private static final String SPACE_ID = R.SPACE;
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
 		final String method = "doPost";
 		log.entry(method);
-    	int userId = SessionUtil.getUserId(request);
-    	try {	
-    		// If we're dealing with an upload request...
+		int userId = SessionUtil.getUserId(request);
+		try {
+			// If we're dealing with an upload request...
 			if (ServletFileUpload.isMultipartContent(request)) {
-				log.info(method, "Got request to upload job xml from user with id="+userId);
+				log.info(method, "Got request to upload job xml from user with id=" + userId);
 
 				log.info(method, "Parsing job xml upload request.");
-				HashMap<String, Object> form = Util.parseMultipartRequest(request); 
+				HashMap<String, Object> form = Util.parseMultipartRequest(request);
 				log.info(method, "Finished parsing job xml upload request.");
-				
+
 				// Make sure the request is valid
 				log.info(method, "Checking that the job xml upload request is valid.");
-				ValidatorStatusCode status =  this.isValidRequest(form);
+				ValidatorStatusCode status = this.isValidRequest(form);
 				log.info(method, "Finished checking that the job xml upload request is valid.");
 
 				if (!status.isSuccess()) {
 					log.info(method, "Request to upload job xml was not valid. Sending bad request error.");
 					//attach the message as a cookie so we don't need to be parsing HTML in StarexecCommand
 					response.addCookie(new Cookie(R.STATUS_MESSAGE_COOKIE, status.getMessage()));
-				    response.sendError(HttpServletResponse.SC_BAD_REQUEST, status.getMessage());
-				    return;
-				}
-
-
-
-				Integer spaceId = Integer.parseInt((String)form.get(SPACE_ID));
-				if (!userMayUploadJobXML(userId, spaceId)) {
-					response.sendError(HttpServletResponse.SC_FORBIDDEN, "You are not allowed to run jobs in this space.");
+					response.sendError(HttpServletResponse.SC_BAD_REQUEST, status.getMessage());
 					return;
 				}
-				
+
+
+				Integer spaceId = Integer.parseInt((String) form.get(SPACE_ID));
+				if (!userMayUploadJobXML(userId, spaceId)) {
+					response.sendError(HttpServletResponse.SC_FORBIDDEN, "You are not allowed to run jobs in this " +
+					                                                     "space.");
+					return;
+				}
+
 				JobUtil jobUtil = new JobUtil();
 
 				List<Integer> result = this.handleXMLFile(userId, spaceId, form, jobUtil);
-				
+
 				// Redirect based on success/failure
-				if(result!=null) {
+				if (result != null) {
 					//send back new ids to the user
 					log.info(method, "Sending back new job ids to user.");
 					response.addCookie(new Cookie("New_ID", Util.makeCommaSeparatedList(result)));
-
-					
-				    response.sendRedirect(Util.docRoot("secure/explore/spaces.jsp"));	
+					response.sendRedirect(Util.docRoot("secure/explore/spaces.jsp"));
 				} else {
-					response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to upload Job XML:\n" + 
-							   jobUtil.getErrorMessage());
-				}									
+					response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to upload Job XML:\n" +
+				}
 			} else {
 				// Got a non multi-part request, invalid
 				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			}
-    	} catch (Exception e) {
-    		response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+		} catch (Exception e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
 			log.error("Caught Exception in UploadJobXML.doPost", e);
-    	}
+		}
 	}
 
 	private boolean userMayUploadJobXML(int userId, int spaceId) {
 		Permission userPermissions = Permissions.get(userId, spaceId);
-		if (userPermissions == null || !userPermissions.canAddJob()) {
-			return false;	
-		} else {
-			return true;
-		}
+		return (userPermissions != null) && userPermissions.canAddJob();
 	}
 
 	/**
 	 * Gets the XML file from the form and tries to create a job from it
+	 *
 	 * @param userId the ID of the user making the request
 	 * @param form a hashmap representation of the form on secure/add/batchJob.jsp
 	 * @author Tim Smith
 	 */
-    private List<Integer> handleXMLFile(
-    		int userId,
-			int spaceId,
-			HashMap<String, Object> form,
-			JobUtil jobUtil) throws StarExecException {
+	private List<Integer> handleXMLFile(int userId, int spaceId, HashMap<String, Object> form, JobUtil jobUtil) throws
+			StarExecException {
 		final String method = "handleXMLFile";
 		log.entry(method);
 		try {
-			log.info(method,"Handling Upload of XML File from user with id=" + userId);
-			PartWrapper item = (PartWrapper)form.get(UploadJobXML.UPLOAD_FILE);
+			log.info(method, "Handling Upload of XML File from user with id=" + userId);
+			PartWrapper item = (PartWrapper) form.get(UploadJobXML.UPLOAD_FILE);
 			// Don't need to keep file long - just using download directory
-			
+
 			File uniqueDir = new File(R.getBatchSpaceXMLDir(), "Job" + userId);
 			uniqueDir = new File(uniqueDir, "TEMP_JOB_XML_FOLDER_");
 			uniqueDir = new File(uniqueDir, "" + shortDate.format(new Date()));
-			
+
 			uniqueDir.mkdirs();
 			//Process the archive file and extract
-		
-			File archiveFile = new File(uniqueDir,  FilenameUtils.getName(item.getName()));
+
+			File archiveFile = new File(uniqueDir, FilenameUtils.getName(item.getName()));
 			new File(archiveFile.getParent()).mkdir();
 			item.write(archiveFile);
 			ArchiveUtil.extractArchive(archiveFile.getAbsolutePath());
 			archiveFile.delete();
-			
+
 			//Typically there will just be 1 file, but might as well allow more
-			
-			List<Integer> jobIds= new ArrayList<>();
-			List<Integer> current= new ArrayList<>();
+
+			List<Integer> jobIds = new ArrayList<>();
 
 			// Makes sure there are no directories in archive file.
 			checkForIllegalDirectoriesInXMLArchive(uniqueDir);
 
 			log.info(method, "Started creating jobs from XML files");
-			for (File file:uniqueDir.listFiles())
-			{
-				current=jobUtil.createJobsFromFile(
-						file,
-						userId,
-						spaceId,
-						JobXmlType.STANDARD,
-						new ConfigAttrMapPair(ConfigXmlAttribute.ID));
+			for (File file : uniqueDir.listFiles()) {
+				final List<Integer> current =
+						jobUtil.createJobsFromFile(file, userId, spaceId, JobXmlType.STANDARD, new ConfigAttrMapPair
+								(ConfigXmlAttribute.ID));
 
-				if (current!=null) {
-					jobIds.addAll(current);		
+				if (current != null) {
+					jobIds.addAll(current);
 				} else {
 					log.warn("the uploaded job xml was not formatted correctly");
 				}
@@ -172,7 +158,7 @@ public class UploadJobXML extends HttpServlet {
 			if (jobUtil.getJobCreationSuccess()) {
 				log.info(method, "Job(s) created successfully.");
 				return jobIds;
-			} 
+			}
 			log.warn(method, jobUtil.getErrorMessage());
 			log.warn(method, "Job(s) could not be created from XML.");
 			return null;
@@ -185,12 +171,11 @@ public class UploadJobXML extends HttpServlet {
 	private void checkForIllegalDirectoriesInXMLArchive(File extractedArchiveDirectory) throws StarExecException {
 		final String method = "checkForIllegalDirectoriesInXMLArchive";
 		log.info(method, "checking for directories in extracted XML archive.");
-		for (String name: extractedArchiveDirectory.list()) 
-		{
-			log.info(method,"Found file with name="+name);
+		for (String name : extractedArchiveDirectory.list()) {
+			log.info(method, "Found file with name=" + name);
 			File file = new File(extractedArchiveDirectory, name);
 			if (file.isDirectory()) {
-				log.info(method, "Found directrory in JobXML archive file. Throwing exception.");
+				log.info(method, "Found directory in JobXML archive file. Throwing exception.");
 				throw new StarExecException("Directories not allowed in job XML archive.");
 			}
 		}
@@ -199,32 +184,30 @@ public class UploadJobXML extends HttpServlet {
 
 	private ValidatorStatusCode isValidRequest(HashMap<String, Object> form) {
 		try {
-			if (!form.containsKey(UploadJobXML.UPLOAD_FILE) ||
-					!form.containsKey(SPACE_ID) ){
-				return new ValidatorStatusCode(false,"Missing field from the form for the file to upload or the space id");
+			if (!form.containsKey(UploadJobXML.UPLOAD_FILE) || !form.containsKey(SPACE_ID)) {
+				return new ValidatorStatusCode(false, "Missing field from the form for the file to upload or the space" +
+				                                      " id");
 			}
-			
-			if (!Validator.isValidPosInteger((String)form.get(SPACE_ID))) {
+
+			if (!Validator.isValidPosInteger((String) form.get(SPACE_ID))) {
 				return new ValidatorStatusCode(false, "The supplied space ID was not a valid integer");
 			}
-			
-			boolean goodExtension=false;
-			String fileName = FilenameUtils.getName(((PartWrapper)form.get(UploadJobXML.UPLOAD_FILE)).getName());
-			for(String ext : UploadJobXML.extensions) {
-				if(fileName.endsWith(ext)) {
-					goodExtension=true;
+
+			boolean goodExtension = false;
+			String fileName = FilenameUtils.getName(((PartWrapper) form.get(UploadJobXML.UPLOAD_FILE)).getName());
+			for (String ext : UploadJobXML.extensions) {
+				if (fileName.endsWith(ext)) {
+					goodExtension = true;
 					break;
 				}
-			}			
+			}
 			if (!goodExtension) {
 				return new ValidatorStatusCode(false, "Uploaded files must be .zip, .tar, or .tgz");
 			}
 			return new ValidatorStatusCode(true);
 		} catch (Exception e) {
 			log.warn(e.getMessage(), e);
-			
 		}
-		
 		return new ValidatorStatusCode(false, "Internal error uploading job XML");
 	}
 
