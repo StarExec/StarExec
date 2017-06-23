eb02451c773231bb6fadc4561cfb33dafd78dc7c
hawks <patrick-hawks@uiowa.edu>
2017-06-23 18:04:34 +0000

    Clean up logging admin page
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26278 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/admin/logging.js
+++ WebContent/js/admin/logging.js
@@ -1,97 +1,76 @@
-var levelTable;
+"use strict";
 
-$(document).ready(function(){
-	initUI();
+jQuery(function($) {
+	var $levelTable = $('#tableLevels');
+	$levelTable.dataTable(new window.star.DataTableConfig({
+		"bSort"     : false,
+		"bPaginate" : true
+	}));
 
-});
+	var getSelectedRowValue = function () {
+		return $('.row_selected').attr('value');
+	}
 
+	$("#applyAll")
+		.button({
+			icons: {
+				primary: "ui-icon-check"
+			}
+		})
+		.click(function() {
+			var value = getSelectedRowValue();
+			$.post(
+					starexecRoot + "services/logging/" + value,
+					{},
+					parseReturnCode,
+					"json"
+			);
+		})
+	;
 
-
-function initUI(){
-
-	$("#applyAll").button({
-		icons: {
-			primary: "ui-icon-check"
-		}
-	});
-	$("#applyToClass").button({
-		icons: {
-			primary: "ui-icon-check"
-		}
-	});
-	$("#applyToClassAllOthersOff").button({
-		icons: {
-			primary: "ui-icon-check"
-		}
-	});
-
-
-	$("#applyAll").click(function() {
-		value=getSelectedRowValue(levelTable);
-		$.post(
-				starexecRoot+"services/logging/"+value,
+	$("#applyToClass")
+		.button({
+			icons: {
+				primary: "ui-icon-check"
+			}
+		})
+		.click(function() {
+			var value = getSelectedRowValue();
+			var className = $("#className").val();
+			$.post(
+				starexecRoot + "services/logging/" + value + "/" + className,
 				{},
-				function(returnCode) {
-					parseReturnCode(returnCode);
-
-				},
+				parseReturnCode,
 				"json"
-		);
-	});
+			);
+		})
+	;
 
-	$("#applyToClass").click(function() {
-		value=getSelectedRowValue(levelTable);
-		$.post(
-			starexecRoot+"services/logging/"+value+"/"+$("#className").val(),
-			{},
-			function(returnCode) {
-				parseReturnCode(returnCode);
-
-			},
-			"json"
-		);
-	});
-	$("#applyToClassAllOthersOff").click(function() {
-		value=getSelectedRowValue(levelTable);
-		$.post(
-			starexecRoot+"services/logging/allOffExcept/"+value+"/"+$("#className").val(),
-			{},
-			function(returnCode) {
-				parseReturnCode(returnCode);
-
-			},
-			"json"
-		);
-	});
-
-
-	levelTable=$('#tableLevels').dataTable( {
-        "sDom"			: getDataTablesDom(),
-        "iDisplayStart"	: 0,
-        "iDisplayLength": defaultPageSize,
-        "bSort": false,
-        "bPaginate": true
-    });
-
+	$("#applyToClassAllOthersOff")
+		.button({
+			icons: {
+				primary: "ui-icon-check"
+			}
+		})
+		.click(function() {
+			var value = getSelectedRowValue();
+			var className = $("#className").val();
+			$.post(
+				starexecRoot + "services/logging/allOffExcept/" + value + "/" + className,
+				{},
+				parseReturnCode,
+				"json"
+			);
+		})
+	;
 
 	$("#tableLevels").on( "click", "tr", function() {
 		if (!$(this).hasClass("row_selected")) {
-			unselectAllRows(levelTable);
+			$levelTable.find("tr").each(function() {
+				$(this).removeClass("row_selected");
+			});
 		}
 		$(this).toggleClass("row_selected");
 	});
 
-}
-
-function getSelectedRowValue() {
-	log($('.row_selected').attr('value'));
-	return $('.row_selected').attr('value');
-}
-
-function unselectAllRows(dataTable) {
-	$(dataTable).find("tr").each(function() {
-		$(this).removeClass("row_selected");
-	});
-}
-
-
+});
--- WebContent/secure/admin/logging.jsp
+++ WebContent/secure/admin/logging.jsp
@@ -1,7 +1,7 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" import="java.util.List, org.starexec.data.database.*, org.starexec.constants.*, org.starexec.util.*, org.starexec.data.to.*"%>
 <%@taglib prefix="star" tagdir="/WEB-INF/tags" %>
 <star:template title="Logging" js="admin/logging, lib/jquery.dataTables.min, lib/jquery.jstree, lib/jquery.qtip.min, lib/jquery.heatcolor.0.0.1.min,lib/jquery.validate.min" css="common/table, details/shared, explore/common, explore/spaces, admin/admin">
-		<fieldset id="fieldTable">
+		<fieldset id="fieldTable" class="expdContainer">
 			<legend>Logging Levels</legend>
 			<table id="tableLevels" class="shaded contentTbl">
 				<thead>
@@ -24,7 +24,7 @@
 
 		<fieldset id="actionField">
 			<legend>actions</legend>
-			<input id="className" type="text"/>
+			<input id="className" type="text" />
 			<button id="applyAll">Apply Level</button>
 			<button id="applyToClass">Apply Level to Class</button>
 			<button id="applyToClassAllOthersOff">Apply Level to Class And Turn Off All Other Classes</button>
