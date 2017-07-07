6186e0505137d457f858c0c600b5c0f2e1e5325b
hawks <patrick-hawks@uiowa.edu>
2017-07-07 18:00:19 +0000

    Minor formatting changes only
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26360 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/test/integration/database/PipelineTests.java
+++ src/org/starexec/test/integration/database/PipelineTests.java
@@ -19,50 +19,49 @@ import org.starexec.test.integration.TestSequence;
  */
 //TODO: Write tests for getting pipelines by job
 public class PipelineTests extends TestSequence {
-	User u=null;
-	Solver s=null;
 	Space space=null;
 	SolverPipeline pipe=null;
+
 	@Override
 	protected String getTestName() {
 		return "PipelineTests";
 	}
-	
+
 	@StarexecTest
 	private void getFullPipelineTest() {
 		SolverPipeline p=Pipelines.getFullPipeline(pipe.getId());
+
 		Assert.assertNotNull(p);
 		Assert.assertEquals(pipe.getName(), p.getName());
 		Assert.assertEquals(pipe.getId(),p.getId());
 		Assert.assertEquals(pipe.getStages().size(),p.getStages().size());
-		for (int i=0;i<pipe.getStages().size();i++) {
+
+		for (int i=0; i<pipe.getStages().size(); ++i) {
 			PipelineStage actualStage = pipe.getStages().get(i);
 			PipelineStage retrievedStage = p.getStages().get(i);
 			Assert.assertEquals(actualStage.getId(),retrievedStage.getId());
 			Assert.assertEquals(actualStage.getConfigId(),retrievedStage.getConfigId());
-			
-			for (int dep=0;dep<actualStage.getDependencies().size();dep++) {
+
+			for (int dep=0; dep<actualStage.getDependencies().size(); ++dep) {
 				PipelineDependency actualDep = actualStage.getDependencies().get(i);
 				PipelineDependency retrievedDep = retrievedStage.getDependencies().get(i);
 				Assert.assertEquals(actualDep.getInputNumber(), retrievedDep.getInputNumber());
 				Assert.assertEquals(actualDep.getType(), retrievedDep.getType());
 			}
-
-		}	
+		}
 	}
 
 	@Override
 	protected void setup() throws Exception {
-		u=loader.loadUserIntoDatabase();
-		space=loader.loadSpaceIntoDatabase(u.getId(), Communities.getTestCommunity().getId());
-		s=loader.loadSolverIntoDatabase(space.getId(), u.getId());
-		pipe=loader.loadPipelineIntoDatabase(u.getId(), s.getConfigurations());
-		
+		final User user = loader.loadUserIntoDatabase();
+		space = loader.loadSpaceIntoDatabase(user.getId(), Communities.getTestCommunity().getId());
+		final Solver solver = loader.loadSolverIntoDatabase(space.getId(), user.getId());
+		pipe = loader.loadPipelineIntoDatabase(user.getId(), solver.getConfigurations());
 	}
 
 	@Override
 	protected void teardown() throws Exception {
-		loader.deleteAllPrimitives();		
+		loader.deleteAllPrimitives();
 	}
 
 }
