StarExec/StarExec

commit 01801f047e58616f08fe009cca50264f8f65e678

Clean up Admin Cluster code
git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26274 bd178f5f-7bea-4014-844f-495ca19b8805

WebContent/js/admin/cluster.js
@@ -1,267 +1,270 @@
-var requests;
 var type;
 var defaultQueueId;
 var curQueueId;
-$(document).ready(function(){
+
+jQuery(function($) {
+	"use strict";
+
 	// Set the path to the css theme fr the jstree plugin
-	 $.jstree._themes = starexecRoot+"css/jstree/";
-	 
-	 var id = -1;
-	 curQueueId=-1;
+	 $.jstree._themes = starexecRoot + "css/jstree/";
+
+	var reloadOnSucess = function(returnCode) {
+		if (parseReturnCode(returnCode)) {
+			setTimeout(function(){document.location.reload(true);}, 1000);
+		}
+	}
+	var id = -1;
+	curQueueId = -1;
+
+	$confirmRemove = $("<div title='confirm removal' class='hiddenDialog'>")
+		.append("<p><span class='ui-icon ui-icon-alert'></span></p>")
+		.appendTo("body")
+	;
+	$confirmRemoveMessage = $("<span>")
+		.appendTo($confirmRemove.contents("p"))
+	;
+
 	// Initialize the jstree plugin for the community list
-	$("#exploreList").bind("loaded.jstree", function(e, data) {
-		 // Register a callback for when the jstree has finished loading
-		addNodeCountsToTree();
-	}).jstree({  
-		"json_data" : { 
-			"ajax" : { 
-				"url" : starexecRoot+"services/cluster/queues",	// Where we will be getting json data from
-				"data" : function (n) {  							
-					return { id : n.attr ? n.attr("id") : -1 }; // What the default space id should be
-				} 
-			} 
-		},
-		"themes" : { 
-			"theme" : "default", 					
-			"dots" : false, 
-			"icons" : true
-		},			
-		"types" : {				
-			"max_depth" : -2,
-			"max_children" : -2,					
-			"valid_children" : [ "space" ],
-			"types" : {						
-				"active_queue" : {
-					"valid_children" : [ "enabled_node", "disabled_node" ],
-					"icon" : {
-						"image" : starexecRoot+"images/jstree/on.png"
-					}
-				},
-				"inactive_queue" : {
-					"valid_children" : [ "enabled_node", "disabled_node"],
-					"icon" : {
-						"image" : starexecRoot + "images/jstree/off.png"
-					}
-				},
-				"enabled_node" : {
-					"valid_children" : [],
-					"icon" : {
-						"image" : starexecRoot+"images/jstree/on.png"
-					}
-				},
-				"disabled_node" : {
-					"valid_children" : [],
-					"icon" : {
-						"image" : starexecRoot+"images/jstree/off.png"
+	$("#exploreList")
+		.bind("loaded.jstree", addNodeCountsToTree)
+		.jstree({
+			"json_data" : {
+				"ajax" : {
+					"url" : starexecRoot + "services/cluster/queues", // Where we will be getting json data from
+					"data" : function (n) {
+						return { id : n.attr ? n.attr("id") : -1 }; // What the default space id should be
 					}
 				}
+			},
+			"themes" : {
+				"theme" : "default",
+				"dots" : false,
+				"icons" : true
+			},
+			"types" : {
+				"max_depth" : -2,
+				"max_children" : -2,
+				"valid_children" : [ "space" ],
+				"types" : {
+					"active_queue" : {
+						"valid_children" : [ "enabled_node", "disabled_node" ],
+						"icon" : {
+							"image" : starexecRoot+"images/jstree/on.png"
+						}
+					},
+					"inactive_queue" : {
+						"valid_children" : [ "enabled_node", "disabled_node"],
+						"icon" : {
+							"image" : starexecRoot + "images/jstree/off.png"
+						}
+					},
+					"enabled_node" : {
+						"valid_children" : [],
+						"icon" : {
+							"image" : starexecRoot+"images/jstree/on.png"
+						}
+					},
+					"disabled_node" : {
+						"valid_children" : [],
+						"icon" : {
+							"image" : starexecRoot+"images/jstree/off.png"
+						}
+					}
+				}
+			},
+			"plugins" : ["types", "themes", "json_data", "ui"],
+			"core" : { animation : 200 }
+		})
+		.bind("select_node.jstree", function (event, data) {
+			// When a node is clicked, get its ID and display the info in the details pane
+			id = data.rslt.obj.attr("id");
+			window['type'] = data.rslt.obj.attr("rel");
+			var global = data.rslt.obj.attr("global");
+			defaultQueueId = data.rslt.obj.attr("defaultQueueId");
+			updateActionId(id, type, global);
+		})
+		.on("click", "a", function (event, data) {
+			// This just disable's links in the node title
+			event.preventDefault();
+		})
+	;
+
+	$("#clearErrorStates a")
+		.button({
+			icons: {
+				primary: "ui-icon-pencil"
 			}
-		},
-		"plugins" : ["types", "themes", "json_data", "ui"] ,
-		"core" : { animation : 200 }
-	}).bind("select_node.jstree", function (event, data) {
-		// When a node is clicked, get its ID and display the info in the details pane		
-	   id = data.rslt.obj.attr("id");
-	   window['type'] = data.rslt.obj.attr("rel");
-	   var global = data.rslt.obj.attr("global");
-	   defaultQueueId = data.rslt.obj.attr("defaultQueueId");
-	   updateActionId(id, type, global);
-	}).on( "click", "a", function (event, data) { event.preventDefault(); });	// This just disable's links in the node title
-	initUI(id);
-	initDataTables();
-	
+		})
+		.click(function() {
+			$.post(
+				starexecRoot + "services/cluster/clearerrors",
+				parseReturnCode,
+				"json"
+			);
+		})
+	;
+
+	$("#CommunityAssoc a").button({
+		icons: {
+			primary: "ui-icon-clipboard"
+		}
+	});
+
+	$("#editQueue a")
+		.button({
+			icons: {
+				primary: "ui-icon-pencil"
+			}
+		})
+		.click(function() {
+			window.open(starexecRoot+"secure/edit/queue.jsp?id="+curQueueId);
+		})
+	;
+
+	$("#makeGlobal a")
+		.button({
+			icons: {
+					primary: "ui-icon-unlocked"
+			}
+		})
+		.click(function() {
+			$confirmRemoveMessage.text('are you sure you want to give global access to this queue?');
+			$confirmRemove.dialog({
+				modal: true,
+				width: 380,
+				height: 165,
+				buttons: {
+					'OK': function() {
+						log('user confirmed giving global access.');
+						$confirmRemove.dialog('close');
+						$.post(
+							starexecRoot + "services/queue/global/" + curQueueId,
+							reloadOnSucess,
+							"json"
+						);
+					},
+					"cancel": function() {
+						log('user canceled giving global access');
+						$confirmRemove.dialog("close");
+					}
+				}
+			});
+		})
+	;
+
+	$("#makeTest a")
+		.button({
+			icons: {
+				primary: "ui-icon-clipboard"
+			}
+		})
+		.click(function(){
+			$.post(
+				starexecRoot + "services/test/queue/" + curQueueId,
+				parseReturnCode,
+				"json"
+			);
+		})
+	;
+
+	$("#manageNodes")
+		.button({
+			icons: {
+				primary: "ui-icon-clipboard"
+			}
+		})
+	;
+
+	$("#moveNodes a")
+		.button({
+			icons: {
+				primary: "ui-icon-locked"
+			}
+		})
+	;
+
+	$("#newQueue")
+		.button({
+			icons: {
+				primary: "ui-icon-plusthick"
+			}
+		})
+	;
+
+	$("#removeGlobal a")
+		.button({
+			icons: {
+				primary: "ui-icon-locked"
+			}
+		})
+		.click(function() {
+			$confirmRemoveMessage.text('are you sure you want to remove global access from this queue?');
+			$confirmRemove.dialog({
+				modal: true,
+				width: 380,
+				height: 165,
+				buttons: {
+					'OK': function() {
+						log('user confirmed removing global access.');
+						$confirmRemove.dialog('close');
+						$.post(
+							starexecRoot+"services/queue/global/remove/" + curQueueId,
+							reloadOnSucess,
+							"json"
+						);
+					},
+					"cancel": function() {
+						log('user canceled removing global access');
+						$confirmRemove.dialog("close");
+					}
+				}
+			});
+		})
+	;
+
+	$("#removeQueue a")
+		.button({
+			icons: {
+				primary: "ui-icon-minusthick"
+			}
+		})
+		.click(function() {
+			$confirmRemoveMessage.text('are you sure you want to remove this queue?');
+			$confirmRemove.dialog({
+				modal: true,
+				width: 380,
+				height: 165,
+				buttons: {
+					'OK': function() {
+						log('user confirmed queue removal.');
+						$confirmRemove.dialog('close');
+						$.post(
+							starexecRoot+"services/remove/queue/" + curQueueId,
+							reloadOnSucess,
+							"json"
+						);
+					},
+					"cancel": function() {
+						log('user canceled queue removal');
+						$confirmRemove.dialog("close");
+					}
+				}
+			});
+		})
+	;
+
+	// Setup the DataTable objects
+	$('#qreserves')
+		.dataTable(new star.DataTableConfig({
+			"bServerSide"   : true,
+			"sAjaxSource"   : starexecRoot + "services/queues/pending/pagination",
+			"fnServerData"  : fnPaginationHandler
+		}))
+	;
+
 });
 
-function initUI(id){
-		
-	$("#newQueue").button({
-		icons: {
-			primary: "ui-icon-plusthick"
-		}
-    });
-	
-	$("#clearErrorStates").button({
-		icons: {
-			primary: "ui-icon-pencil"
-		}
-	});
-	
-	$("#clearErrorStates").click(function() {
-		log('clearing error states');
-		clearErrorStates();
-	});
-	
-	$("#newQueue").button({
-		icons: {
-			primary: "ui-icon-plusthick"
-		}
-	});
-	
-	$("#removeQueue").button({
-		icons: {
-			primary: "ui-icon-minusthick"
-		}
-	});
-	
-	$("#manageNodes").button({
-		icons: {
-			primary: "ui-icon-clipboard"
-		}
-	});
-	
-	
-	
-	$("#moveNodes").button({
-		icons: {
-			primary: "ui-icon-locked"
-		}
-	});
-	
-	$("#CommunityAssoc").button({
-		icons: {
-			primary: "ui-icon-clipboard"
-		}
-	});
-	
-	$("#makeTest").button({
-		icons: {
-			primary: "ui-icon-clipboard"
-		}
-	});
-	
-	$("#makeGlobal").button({
-		icons: {
-			primary: "ui-icon-unlocked"
-		}
-	});
-	
-	$("#removeGlobal").button({
-		icons: {
-			primary: "ui-icon-locked"
-		}
-	});
-	
-	$("#editQueue").button({
-		icons: {
-			primary: "ui-icon-pencil"
-		}
-	});
-
-	$("#editQueue").click(function() {
-		window.open(starexecRoot+"secure/edit/queue.jsp?id="+curQueueId);
-	});
-	
-	$("#removeQueue").click(function(){
-		$('#dialog-confirm-remove-txt').text('are you sure you want to remove this queue?');
-		
-		$('#dialog-confirm-remove').dialog({
-			modal: true,
-			width: 380,
-			height: 165,
-			buttons: {
-				'OK': function() {
-					log('user confirmed queue removal.');
-					$('#dialog-confirm-remove').dialog('close');
-					$.post(
-					       starexecRoot+"services/remove/queue/" + curQueueId,
-					       function(returnCode) {
-					       s=parseReturnCode(returnCode);
-					       if (s) {
-						       setTimeout(function(){document.location.reload(true);}, 1000);
-					       }
-					       },
-					       "json"
-					       );
-				},
-			       "cancel": function() {
-				   log('user canceled queue removal');
-				   $(this).dialog("close");
-			       }
-			}
-		});
-	});	
-	
-	
-	$("#makeTest").click(function(){
-		$.post(
-			starexecRoot+"services/test/queue/" + curQueueId,
-			function(returnCode) {
-				s=parseReturnCode(returnCode);
-								
-			},
-		"json"
-		);
-			
-	});	
-	
-	$("#makeGlobal").click(function(){
-		$('#dialog-confirm-remove-txt').text('are you sure you want to give global access to this queue?');
-		
-		$('#dialog-confirm-remove').dialog({
-			modal: true,
-			width: 380,
-			height: 165,
-			buttons: {
-				'OK': function() {
-					log('user confirmed giving global access.');
-					$('#dialog-confirm-remove').dialog('close');
-					$.post(
-							starexecRoot+"services/queue/global/" + curQueueId,
-							function(returnCode) {
-								s=parseReturnCode(returnCode);
-								if (s) {
-									setTimeout(function(){document.location.reload(true);}, 1000);
-
-								}
-									
-							},
-							"json"
-					);
-				},
-				"cancel": function() {
-					log('user canceled giving global access');
-					$(this).dialog("close");
-				}
-			}
-		});
-	});	
-	
-	$("#removeGlobal").click(function(){
-		$('#dialog-confirm-remove-txt').text('are you sure you want to remove global access from this queue?');
-		
-		$('#dialog-confirm-remove').dialog({
-			modal: true,
-			width: 380,
-			height: 165,
-			buttons: {
-				'OK': function() {
-					log('user confirmed removing global access.');
-					$('#dialog-confirm-remove').dialog('close');
-					$.post(
-							starexecRoot+"services/queue/global/remove/" + curQueueId,
-							function(returnCode) {
-								s=parseReturnCode(returnCode);
-								if (s) {
-									setTimeout(function(){document.location.reload(true);}, 1000);
-
-								}
-							},
-					
-							"json"
-					);
-				},
-				"cancel": function() {
-					log('user canceled removing global access');
-					$(this).dialog("close");
-				}
-			}
-		});
-	});	
-
-
-}
-
-function updateActionId(id, type, global) {	
+function updateActionId(id, type, global) {
 	curQueueId=id;
 	if (id == -1) {
 		$("#removeQueue").hide();
@@ -269,14 +272,12 @@ function updateActionId(id, type, global) {
 		$("#CommunityAssoc").hide();
 		$("#makeGlobal").hide();
 		$("#makeTest").hide();
-
 		$("#removeGlobal").hide();
 		$("#editQueue").hide();
-		
 	} else {
 		$("#editQueue").show();
 	}
-	
+
 	if (type == "active_queue" || type=="inactive_queue") {
 		$("#makeTest").show();
 
@@ -288,7 +289,6 @@ function updateActionId(id, type, global) {
 			$("#removeGlobal").hide();
 		} else {
 			$("#removeQueue").show();
-
 			$("#moveNodes").show();
 
 			if (global == 'true') {
@@ -300,9 +300,6 @@ function updateActionId(id, type, global) {
 				$("#removeGlobal").hide();
 				$("#CommunityAssoc").show();
 			}
-				
-			
-			
 		}
 	} else {
 		$("#removeQueue").hide();
@@ -311,54 +308,25 @@ function updateActionId(id, type, global) {
 		$("#makeGlobal").hide();
 		$("#removeGlobal").hide();
 	}
-	
-	
-	$('#moveNodes').attr('href', starexecRoot+"secure/admin/moveNodes.jsp?id=" + id);
+
+	$('#moveNodes').attr('href', starexecRoot+"secure/admin/moveNodes a.jsp?id=" + id);
 	$('#CommunityAssoc').attr('href', starexecRoot + "secure/admin/assocCommunity.jsp?id=" + id);
 
 }
 
-function initDataTables(){
-	// Setup the DataTable objects
-	requests = $('#qreserves').dataTable( {
-		"sDom"			: getDataTablesDom(),
-		"iDisplayStart"	: 0,
-		"iDisplayLength": defaultPageSize,
-		"bServerSide"	: true,
-		"sAjaxSource"	: starexecRoot+"services/",
-		"sServerMethod" : 'POST',
-		"fnServerData"	: fnPaginationHandler
-	});
-
-
-	
-}
-
 function fnPaginationHandler(sSource, aoData, fnCallback){
 	// Request the next page of primitives from the server via AJAX
-	$.post(  
-			sSource + "queues/pending/pagination",
+	$.post(
+			sSource,
 			aoData,
-			function(nextDataTablePage){
-				s=parseReturnCode(nextDataTablePage);
-				if (s) {
+			function(nextDataTablePage) {
+				if (parseReturnCode(nextDataTablePage)) {
 					// Update the number displayed in this DataTable's fieldset
 					$('#reservationExpd').children('span:first-child').text(nextDataTablePage.iTotalRecords);
-				
-				// Replace the current page with the newly received page
-				fnCallback(nextDataTablePage);
+					// Replace the current page with the newly received page
+					fnCallback(nextDataTablePage);
 				}
-			},  
-			"json"
-	);
-}
-
-function clearErrorStates() {
-	$.post(
-			starexecRoot+"services/cluster/clearerrors",
-			function(returnCode) {
-				parseReturnCode(returnCode);	
 			},
 			"json"
-		);
+	);
 }

WebContent/secure/admin/cluster.jsp
@@ -1,9 +1,6 @@
-<%@page contentType="text/html" pageEncoding="UTF-8" import="org.starexec.data.database.*, org.starexec.data.to.*, org.starexec.util.*, java.util.List, org.starexec.constants.*"%>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
 <%@taglib prefix="star" tagdir="/WEB-INF/tags" %>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-
-<star:template title="${t_user.fullName}" js="admin/cluster, lib/jquery.dataTables.min, lib/jquery.jstree, lib/jquery.qtip.min, lib/jquery.heatcolor.0.0.1.min, shared/sharedFunctions" css="common/table, details/shared, explore/common, explore/spaces, admin/admin, shared/cluster">
+<star:template title="Admin Cluster" js="admin/cluster, lib/jquery.dataTables.min, lib/jquery.jstree, lib/jquery.qtip.min, lib/jquery.heatcolor.0.0.1.min, shared/sharedFunctions" css="common/table, details/shared, explore/common, explore/spaces, admin/admin, shared/cluster">
 	<div id="explorer">
 		<h3>queues</h3>
 		<ul id="exploreList"></ul>
@@ -16,21 +13,17 @@
 
 	<div id="detailPanel">
 		<fieldset>
-		<legend>actions</legend>
+			<legend>actions</legend>
 			<ul id="actionList">
-				<li><button type="button" id="clearErrorStates">clear error states</button></li>
-				<li><button type="button" id="removeQueue">remove queue</button></li>
-				<li><a type="button" id="moveNodes" href="${starexecRoot}/secure/admin/moveNodes.jsp">move nodes to this queue</a></li>
-				<li><a type="button" id="CommunityAssoc" href="${starexecRoot}/secure/admin/assocCommunity.jsp">give communities access</a></li>
-				<li><button type="button" id="makeTest">set queue as test queue</button></li>
-				<li><button type="button" id="makeGlobal">give queue global access</button></li>
-				<li><button type="button" id="removeGlobal">remove global access</button></li>
-				<li><button type="button" id="editQueue">edit queue</button></li>
+				<li id="editQueue"><a>edit queue</a></li>
+				<li id="removeQueue"><a>remove queue</a></li>
+				<li id="removeGlobal"><a>remove global access</a></li>
+				<li id="clearErrorStates"><a>clear error states</a></li>
+				<li id="moveNodes" href="${starexecRoot}/secure/admin/moveNodes.jsp"><a>move nodes to this queue</a></li>
+				<li id="CommunityAssoc" href="${starexecRoot}/secure/admin/assocCommunity.jsp"><a>give communities access</a></li>
+				<li id="makeTest"><a>set queue as test queue</a></li>
+				<li id="makeGlobal"><a>give queue global access</a></li>
 			</ul>
-		</fieldset>	
-
-	</div>	
-	<div id="dialog-confirm-remove" title="confirm removal" class="hiddenDialog">
-				<p><span class="ui-icon ui-icon-alert"></span><span id="dialog-confirm-remove-txt"></span></p>
+		</fieldset>
 	</div>
 </star:template>

commit 136dd0768b6da0018e46106ca3c907ebfb7c7fb5

Fix Cluster Admin page
Error introduced in r26274


git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26381 bd178f5f-7bea-4014-844f-495ca19b8805

WebContent/js/admin/cluster.js
@@ -309,8 +309,8 @@ function updateActionId(id, type, global) {
 		$("#removeGlobal").hide();
 	}
 
-	$('#moveNodes').attr('href', starexecRoot+"secure/admin/moveNodes a.jsp?id=" + id);
-	$('#CommunityAssoc').attr('href', starexecRoot + "secure/admin/assocCommunity.jsp?id=" + id);
+	$('#moveNodes a').attr('href', starexecRoot+"secure/admin/moveNodes.jsp?id=" + id);
+	$('#CommunityAssoc a').attr('href', starexecRoot + "secure/admin/assocCommunity.jsp?id=" + id);
 
 }
 

WebContent/secure/admin/cluster.jsp
@@ -19,8 +19,8 @@
 				<li id="removeQueue"><a>remove queue</a></li>
 				<li id="removeGlobal"><a>remove global access</a></li>
 				<li id="clearErrorStates"><a>clear error states</a></li>
-				<li id="moveNodes" href="${starexecRoot}/secure/admin/moveNodes.jsp"><a>move nodes to this queue</a></li>
-				<li id="CommunityAssoc" href="${starexecRoot}/secure/admin/assocCommunity.jsp"><a>give communities access</a></li>
+				<li id="moveNodes"><a>move nodes to this queue</a></li>
+				<li id="CommunityAssoc"><a>give communities access</a></li>
 				<li id="makeTest"><a>set queue as test queue</a></li>
 				<li id="makeGlobal"><a>give queue global access</a></li>
 			</ul>