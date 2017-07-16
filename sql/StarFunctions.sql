a6d53860e590cf6dfcaa33de6c247022a4064f3a
hawks <patrick-hawks@uiowa.edu>
2017-07-16 17:11:32 +0000

    Whitespace changes only
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26414 bd178f5f-7bea-4014-844f-495ca19b8805

--- sql/StarFunctions.sql
+++ sql/StarFunctions.sql
@@ -6,72 +6,72 @@ USE starexec;
 
 DELIMITER // -- Tell MySQL how we will denote the end of each prepared statement
 
-	
+
 -- Gets the number of completed job pairs for a given job id
 -- Author: Todd Elvers
 DROP FUNCTION IF EXISTS GetCompletePairs;
-CREATE FUNCTION GetCompletePairs(_jobId INT) 
+CREATE FUNCTION GetCompletePairs(_jobId INT)
 	RETURNS INT
 	BEGIN
 		DECLARE completePairs INT;
-		
-		SELECT COUNT(*) INTO completePairs 
-		FROM job_pairs 
-		WHERE job_id=_jobId 
+
+		SELECT COUNT(*) INTO completePairs
+		FROM job_pairs
+		WHERE job_id=_jobId
 		AND status_code=7;
-		
+
 		RETURN completePairs;
 	END //
-	
-	
+
+
 -- Gets the number of errored job pairs for a given job id
 -- Author: Todd Elvers
 DROP FUNCTION IF EXISTS GetErrorPairs;
-CREATE FUNCTION GetErrorPairs(_jobId INT) 
+CREATE FUNCTION GetErrorPairs(_jobId INT)
 	RETURNS INT
 	BEGIN
 		DECLARE errorPairs INT;
-		
-		SELECT COUNT(*) INTO errorPairs 
-		FROM job_pairs 
-		WHERE job_id=_jobId 
+
+		SELECT COUNT(*) INTO errorPairs
+		FROM job_pairs
+		WHERE job_id=_jobId
 		AND (status_code BETWEEN 8 AND 17 OR status_code=0 OR status_code=24);
-		
+
 		RETURN errorPairs;
 	END //
-	
+
 -- Returns "complete" if the job represented by the given id had no pending job pairs,
 -- and returns "incomplete" otherwise
 -- Author: Todd Elvers
 DROP FUNCTION IF EXISTS GetJobStatus;
-CREATE FUNCTION GetJobStatus(_jobId INT) 
+CREATE FUNCTION GetJobStatus(_jobId INT)
 	RETURNS VARCHAR(10)
 	BEGIN
 		DECLARE status VARCHAR(10);
-		
+
 		IF(GetPendingPairs(_jobId) > 0) THEN
 			SET status = "incomplete";
 		ELSE
 			SET status = "complete";
 		END IF;
-		
+
 		RETURN status;
 	END //
-	
-	
+
+
 -- Gets the number of pending job pairs for a given job id
--- Author: Todd Elvers	
+-- Author: Todd Elvers
 DROP FUNCTION IF EXISTS GetPendingPairs;
-CREATE FUNCTION GetPendingPairs(_jobId INT) 
+CREATE FUNCTION GetPendingPairs(_jobId INT)
 	RETURNS INT
 	BEGIN
 		DECLARE pendingPairs INT;
-		
-		SELECT COUNT(*) INTO pendingPairs 
+
+		SELECT COUNT(*) INTO pendingPairs
 		FROM job_pairs
-		WHERE job_id=_jobId 
+		WHERE job_id=_jobId
 		AND (status_code BETWEEN 1 AND 6);
-		
+
 		RETURN pendingPairs;
 	END //
 
@@ -82,12 +82,12 @@ CREATE FUNCTION IsPublic(_spaceId int)
 	RETURNS BOOLEAN
 	BEGIN
 		DECLARE isPublic BOOLEAN;
-	  		select public_access INTO isPublic 
+	  		select public_access INTO isPublic
 	  		from spaces
 	  		where id = _spaceId;
-	  	RETURN isPublic;	
+	  	RETURN isPublic;
 	END //
-	
+
 --  Determines if User is Leader of Space
 --  Author: Benton McCune
 DROP FUNCTION IF EXISTS IsLeader;
@@ -95,10 +95,10 @@ CREATE FUNCTION IsLeader(_spaceId int, _userId int)
 	RETURNS BOOLEAN
 	BEGIN
 		DECLARE isLeader BOOLEAN;
-	  		select is_Leader INTO isLeader  
-	  		from permissions 
+	  		select is_Leader INTO isLeader
+	  		from permissions
 	  		where id = (select permission from user_assoc where space_id=_spaceId and user_id = _userId LIMIT 1);
-	  	RETURN isLeader;	
-	END //	
-	
+	  	RETURN isLeader;
+	END //
+
 DELIMITER ; -- This should always be at the end of this file
