115af490da5ffdd169f5edde74fc149b527f26d7
hawks <patrick-hawks@uiowa.edu>
2017-07-23 20:34:41 +0000

    Simplify
    No need for functions that only call other functions
    
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26470 bd178f5f-7bea-4014-844f-495ca19b8805

--- WebContent/js/details/configuration.js
+++ WebContent/js/details/configuration.js
@@ -1,31 +1,14 @@
-$(document).ready(function(){
-	// Setup the UI buttons/actions
-	initUI();
-});
-
-
-/**
- * Sets up the UI buttons/actions 
- */
-function initUI(){
-	initButtons();
-}
-
-
-/**
- * Sets up button icons and actions
- */
-function initButtons(){
+jQuery(function($) {
 	$('#deleteConfig').button({
 		icons: {
 			secondary: "ui-icon-minus"
 		}
 	});
-	
+
 	// Handles the deletion of this configuration file
-	$("#deleteConfig").click(function(){
+	$("#deleteConfig").click(function() {
 		$('#dialog-confirm-delete-txt').text('are you sure you want to delete this configuration?');
-		
+
 		// Display the confirmation dialog
 		$('#dialog-confirm-delete').dialog({
 			modal: true,
@@ -33,27 +16,25 @@ function initButtons(){
 				'yes': function() {
 					// If the user actually confirms, close the dialog right away
 					$('#dialog-confirm-delete').dialog('close');
-					
-					$.post(  
+
+					$.post(
 						starexecRoot+"services/delete/configuration",
 						{selectedIds: [getParameterByName("id")]},
 						function(returnCode) {
-							s=parseReturnCode(returnCode);
+							var s = parseReturnCode(returnCode);
 							if (s) {
 								window.location = starexecRoot+"secure/details/solver.jsp?id=" + $('#solverId').val();
-
 							}
-							
 						},
 						"json"
 					).error(function(){
 						showMessage('error',"Internal error deleting configuration.",5000);
-					});		
+					});
 				},
 				"cancel": function() {
 					$(this).dialog("close");
 				}
-			}		
-		});				
+			}
+		});
 	});
 }
