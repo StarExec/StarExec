663f4cc9d8012d614381e616934abbb05065b19c
hawks <patrick-hawks@uiowa.edu>
2017-08-04 00:35:46 +0000

    Formatting changes only
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@26529 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/data/database/Processors.java
+++ src/org/starexec/data/database/Processors.java
@@ -5,6 +5,7 @@ import org.starexec.constants.R;
 import org.starexec.data.to.Processor;
 import org.starexec.data.to.enums.ProcessorType;
 import org.starexec.logger.StarLogger;
+import org.starexec.util.Util;
 
 import java.io.File;
 import java.sql.CallableStatement;
@@ -19,47 +20,49 @@ import java.util.List;
  */
 public class Processors {
 	private static final StarLogger log = StarLogger.getLogger(Processors.class);
-	
+
 	/**
 	 * Given a result set where the current row points to a  processor, return the processor
+	 *
 	 * @param results
 	 * @param prefix The table alias given to the processor table in this query. Empty means no prefix.
 	 * @return The processor if it exists
 	 * @throws SQLException If the ResultSet does not contain a required processor attribute
 	 */
 	public static Processor resultSetToProcessor(ResultSet results, String prefix) throws SQLException {
-		if (prefix==null || prefix.isEmpty()) {
-			prefix="";
+		if (Util.isNullOrEmpty(prefix)) {
+			prefix = "";
 		} else {
-			prefix=prefix+".";
+			prefix = prefix + ".";
 		}
-		
+
 		Processor t = new Processor();
 		//if the ID is null, 0 is returned here
-		t.setId(results.getInt(prefix+"id"));
-		t.setCommunityId(results.getInt(prefix+"community"));
-		t.setDescription(results.getString(prefix+"description"));
-		t.setName(results.getString(prefix+"name"));
-		t.setFilePath(results.getString(prefix+"path"));
-		t.setDiskSize(results.getLong(prefix+"disk_size"));
+		t.setId(results.getInt(prefix + "id"));
+		t.setCommunityId(results.getInt(prefix + "community"));
+		t.setDescription(results.getString(prefix + "description"));
+		t.setName(results.getString(prefix + "name"));
+		t.setFilePath(results.getString(prefix + "path"));
+		t.setDiskSize(results.getLong(prefix + "disk_size"));
 		t.setType(ProcessorType.valueOf(results.getInt("processor_type")));
 
 		return t;
 	}
-	
+
 	/**
 	 * Inserts a processor into the database
+	 *
 	 * @param processor The processor to add to the database
 	 * @return The positive integer ID of the new processor if successful, -1 otherwise
 	 * @author Tyler Jensen
 	 */
 	public static int add(Processor processor) {
-		Connection con = null;			
+		Connection con = null;
 		CallableStatement procedure = null;
 		try {
-			con = Common.getConnection();		
-			
-			procedure = con.prepareCall("{CALL AddProcessor(?, ?, ?, ?, ?, ?, ?)}");			
+			con = Common.getConnection();
+
+			procedure = con.prepareCall("{CALL AddProcessor(?, ?, ?, ?, ?, ?, ?)}");
 			procedure.setString(1, processor.getName());
 			procedure.setString(2, processor.getDescription());
 			procedure.setString(3, processor.getFilePath());
@@ -68,34 +71,35 @@ public class Processors {
 			procedure.setLong(6, FileUtils.sizeOf(new File(processor.getFilePath())));
 			procedure.registerOutParameter(7, java.sql.Types.INTEGER);
 			procedure.executeUpdate();
-			
-			int procId = procedure.getInt(7);	
-			log.debug("the new processor has the ID = "+procId + " and community id = "+processor.getCommunityId());
-			return procId;			
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+
+			int procId = procedure.getInt(7);
+			log.debug("the new processor has the ID = " + procId + " and community id = " + processor.getCommunityId());
+			return procId;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 		}
-		
+
 		return -1;
 	}
-	
+
 	/**
 	 * Deletes a given processor from a space
+	 *
 	 * @param processorId the id of the processor to delete
 	 * @return True if the operation was a success, false otherwise
 	 * @author Todd Elvers
 	 */
-	public static boolean delete(int processorId){
-		if (processorId==R.NO_TYPE_PROC_ID) {
+	public static boolean delete(int processorId) {
+		if (processorId == R.NO_TYPE_PROC_ID) {
 			return false; // the no type processor is required for the system
 		}
-		if (Processors.get(processorId)==null) {
+		if (Processors.get(processorId) == null) {
 			return true;
 		}
-		Connection con = null;			
+		Connection con = null;
 		File processorFile = null;
 		CallableStatement procedure = null;
 		try {
@@ -104,38 +108,42 @@ public class Processors {
 			procedure.setInt(1, processorId);
 			procedure.registerOutParameter(2, java.sql.Types.LONGNVARCHAR);
 			procedure.executeUpdate();
-			
+
 			// Get processor_path of processor
 			processorFile = new File(procedure.getString(2));
 			log.debug(String.format("Removal of processor [id=%d] was successful.", processorId));
-			
+
 			// Try and delete file referenced by processor_path and its parent directory
 			if (processorFile.exists()) {
-				if(processorFile.delete()){
-					log.debug(String.format("File [%s] was deleted at [%s] because it was not inter referenced anywhere.", processorFile.getName(), processorFile.getAbsolutePath()));
+				if (processorFile.delete()) {
+					log.debug(String
+							.format("File [%s] was deleted at [%s] because it was not inter referenced anywhere.",
+									processorFile
+									.getName(), processorFile.getAbsolutePath()));
 				}
-				if (processorFile.getParentFile()!=null) {
-					if(processorFile.getParentFile().delete()){
-						log.debug(String.format("Directory [%s] was deleted because it was empty.", processorFile.getParentFile().getAbsolutePath()));
+				if (processorFile.getParentFile() != null) {
+					if (processorFile.getParentFile().delete()) {
+						log.debug(String.format("Directory [%s] was deleted because it was empty.", processorFile
+								.getParentFile().getAbsolutePath()));
 					}
 				}
-				
+
 			}
-			
-			
+
+
 			return true;
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 		}
-		
+
 		log.debug(String.format("Removal of processor [id=%d] failed.", processorId));
 		return false;
 	}
-	
-	/**	 
+
+	/**
 	 * @param con The connection to make the query on
 	 * @param processorId The id of the bench processor to retrieve
 	 * @return The corresponding processor
@@ -144,193 +152,192 @@ public class Processors {
 	protected static Processor get(Connection con, int processorId) {
 		CallableStatement procedure = null;
 		ResultSet results = null;
-					
+
 		try {
 			procedure = con.prepareCall("{CALL GetProcessorById(?)}");
 			procedure.setInt(1, processorId);
 			results = procedure.executeQuery();
-			
-			if(results.next()){							
-				Processor t = Processors.resultSetToProcessor(results, "");
-				
-				return t;					
+
+			if (results.next()) {
+				return Processors.resultSetToProcessor(results, "");
 			}
 		} catch (Exception e) {
-			log.error("Processors.get says "+e.getMessage(),e);
+			log.error("Processors.get says " + e.getMessage(), e);
 		} finally {
 			Common.safeClose(results);
 			Common.safeClose(procedure);
 		}
-		
+
 		return null;
 	}
-	
-	/**	 
+
+	/**
 	 * @param processorId The id of the bench processor to retrieve
 	 * @return The corresponding processor
 	 * @author Tyler Jensen
 	 */
 	public static Processor get(int processorId) {
-		Connection con = null;			
-		
+		Connection con = null;
+
 		try {
-			con = Common.getConnection();					
-			return Processors.get(con, processorId);			
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+			con = Common.getConnection();
+			return Processors.get(con, processorId);
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Gets the list of processors
+	 *
 	 * @param type The type of processors to filter by
 	 * @return the list of processors
 	 * @author Todd Elvers
 	 */
-	public static List<Processor> getAll(ProcessorType type){
-		Connection con = null;			
+	public static List<Processor> getAll(ProcessorType type) {
+		Connection con = null;
 		CallableStatement procedure = null;
 		ResultSet results = null;
 		try {
-			con = Common.getConnection();		
-			 procedure = con.prepareCall("{CALL GetAllProcessors(?)}");
+			con = Common.getConnection();
+			procedure = con.prepareCall("{CALL GetAllProcessors(?)}");
 			procedure.setInt(1, type.getVal());
-			 results = procedure.executeQuery();
+			results = procedure.executeQuery();
 			List<Processor> processors = new LinkedList<>();
-			
-			while(results.next()){
-				Processor bt = Processors.resultSetToProcessor(results, "");
 
+			while (results.next()) {
+				Processor bt = Processors.resultSetToProcessor(results, "");
 				processors.add(bt);
 			}
-			
+
 			return processors;
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 			Common.safeClose(results);
 		}
-		
+
 		return null;
-	}	
-	
+	}
+
 	/**
 	 * @return the system NoType benchmark processor, which is applied when the user has no processor.
 	 */
 	public static Processor getNoTypeProcessor() {
 		return Processors.get(R.NO_TYPE_PROC_ID);
 	}
-	
-	
-	/**	 
+
+
+	/**
 	 * @param communityId The id of the community to retrieve all processors for
 	 * @param type The type of processors to get for the community
 	 * @return A list of all processors of the given type that the community owns
 	 * @author Tyler Jensen
 	 */
 	public static List<Processor> getByCommunity(int communityId, ProcessorType type) {
-		Connection con = null;			
+		Connection con = null;
 		CallableStatement procedure = null;
 		ResultSet results = null;
 		try {
-			con = Common.getConnection();					
-			 procedure = con.prepareCall("{CALL GetProcessorsByCommunity(?, ?)}");
+			con = Common.getConnection();
+			procedure = con.prepareCall("{CALL GetProcessorsByCommunity(?, ?)}");
 			procedure.setInt(1, communityId);
 			procedure.setInt(2, type.getVal());
-			 results = procedure.executeQuery();
+			results = procedure.executeQuery();
 			List<Processor> processors = new LinkedList<>();
-			
-			while(results.next()){							
-				Processor t = Processors.resultSetToProcessor(results, "");
 
-				processors.add(t);						
-			}				
-			
+			while (results.next()) {
+				Processor t = Processors.resultSetToProcessor(results, "");
+				processors.add(t);
+			}
+
 			return processors;
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 			Common.safeClose(results);
 		}
-		
+
 		return null;
-	}	
-	
-	/**	 
+	}
+
+	/**
 	 * Gets all processors that a user can see because they share a community
+	 *
 	 * @param userId the user to retrieve post processors for
 	 * @param type The type of processors to get
 	 * @return A list of all unique processors of the given type that the user can see
 	 * @author Eric Burns
 	 */
 	public static List<Processor> getByUser(int userId, ProcessorType type) {
-		Connection con = null;			
+		Connection con = null;
 		CallableStatement procedure = null;
 		ResultSet results = null;
 		try {
-			con = Common.getConnection();					
-			 procedure = con.prepareCall("{CALL GetProcessorsByUser(?, ?)}");
+			con = Common.getConnection();
+			procedure = con.prepareCall("{CALL GetProcessorsByUser(?, ?)}");
 			procedure.setInt(1, userId);
 			procedure.setInt(2, type.getVal());
-			 results = procedure.executeQuery();
+			results = procedure.executeQuery();
 			List<Processor> processors = new LinkedList<>();
-			
-			while(results.next()){							
+
+			while (results.next()) {
 				Processor t = new Processor();
 				t.setId(results.getInt("id"));
 				t.setName(results.getString("name"));
-				processors.add(t);						
-			}				
+				processors.add(t);
+			}
 			return processors;
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 			Common.safeClose(results);
 		}
-		
+
 		return null;
-	}	
+	}
 
 	/**
 	 * Updates the description of a processor with the given processor id
+	 *
 	 * @param processorId the id of the processor to update
 	 * @param newDesc the new description to update the processor with
 	 * @return True if the operation was a success, false otherwise
 	 * @author Tyler Jensen
 	 */
-	public static boolean updateDescription(int processorId, String newDesc){
-		Connection con = null;			
+	public static boolean updateDescription(int processorId, String newDesc) {
+		Connection con = null;
 		CallableStatement procedure = null;
 		try {
-			con = Common.getConnection();		
-			 procedure = con.prepareCall("{CALL UpdateProcessorDescription(?, ?)}");
-			procedure.setInt(1, processorId);					
+			con = Common.getConnection();
+			procedure = con.prepareCall("{CALL UpdateProcessorDescription(?, ?)}");
+			procedure.setInt(1, processorId);
 			procedure.setString(2, newDesc);
-			
-			procedure.executeUpdate();			
-			return true;			
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+			procedure.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 		}
-		
+
 		return false;
 	}
 
 	/**
 	 * Makes sure that a processor with the given id exists.
+	 *
 	 * @param processorId The id of a processor.
 	 * @return true if the the processor exists, otherwise false.
 	 * @author Albert Giegerich
@@ -340,64 +347,61 @@ public class Processors {
 		return (processor != null);
 	}
 
-	
+
 	/**
 	 * Updates the file path of a processor with the given processor id
-	 * 
+	 *
 	 * @param processorId the id of the processor to update
 	 * @param newPath the new path to the directory containing this processor
 	 * @return True if the operation was a success, false otherwise
 	 * @author Eric Burns
 	 */
-	public static boolean updateFilePath(int processorId, String newPath){
-		Connection con = null;			
+	public static boolean updateFilePath(int processorId, String newPath) {
+		Connection con = null;
 		CallableStatement procedure = null;
 		try {
-			con = Common.getConnection();		
-			 procedure = con.prepareCall("{CALL UpdateProcessorFilePath(?, ?)}");
-			procedure.setInt(1, processorId);					
+			con = Common.getConnection();
+			procedure = con.prepareCall("{CALL UpdateProcessorFilePath(?, ?)}");
+			procedure.setInt(1, processorId);
 			procedure.setString(2, newPath);
-			
-			procedure.executeUpdate();			
-			return true;			
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+			procedure.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 		}
-		
+
 		return false;
-	}	
-		
-	
+	}
+
+
 	/**
 	 * Updates the name of a processor with the given processor id
-	 * 
+	 *
 	 * @param processorId the id of the processor to update
 	 * @param newName the new name to update the processor with
 	 * @return True if the operation was a success, false otherwise
 	 * @author Tyler Jensen
 	 */
-	public static boolean updateName(int processorId, String newName){
-		Connection con = null;			
+	public static boolean updateName(int processorId, String newName) {
+		Connection con = null;
 		CallableStatement procedure = null;
 		try {
-			con = Common.getConnection();		
-			 procedure = con.prepareCall("{CALL UpdateProcessorName(?, ?)}");
-			procedure.setInt(1, processorId);					
+			con = Common.getConnection();
+			procedure = con.prepareCall("{CALL UpdateProcessorName(?, ?)}");
+			procedure.setInt(1, processorId);
 			procedure.setString(2, newName);
-			
-			procedure.executeUpdate();			
-			return true;			
-		} catch (Exception e){			
-			log.error(e.getMessage(), e);		
+			procedure.executeUpdate();
+			return true;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		} finally {
 			Common.safeClose(con);
 			Common.safeClose(procedure);
 		}
-		
+
 		return false;
-	}	
-		
+	}
 }
--- src/org/starexec/data/to/Processor.java
+++ src/org/starexec/data/to/Processor.java
@@ -11,19 +11,18 @@ import java.io.File;
 /**
  * Represents a processor, which is an arbitrary user specified filed that takes
  * input and produces output. This is used at various stages in the job pipeline
- * @author Tyler Jensen 
+ *
+ * @author Tyler Jensen
  */
 public class Processor extends Identifiable implements Nameable, Locatable {
-
-	
 	@Expose private String name = "none";
 	@Expose private String description = "no description";
 	@Expose private String fileName;
 	@Expose private ProcessorType type = ProcessorType.DEFAULT;
-	private String filePath;	
+	private String filePath;
 	private long diskSize;
 	private int communityId;
-	
+
 	/**
 	 * @return the type of the processor (bench, pre or post proessor)
 	 */
@@ -44,32 +43,32 @@ public class Processor extends Identifiable implements Nameable, Locatable {
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * @param name The name to set for the processor
 	 */
 	public void setName(String name) {
-		if(!Util.isNullOrEmpty(name)) {
+		if (!Util.isNullOrEmpty(name)) {
 			this.name = name;
 		}
 	}
-	
+
 	/**
 	 * @return This processors's user-defined description
 	 */
 	public String getDescription() {
 		return description;
 	}
-	
+
 	/**
 	 * @param description The description to set for this processor
 	 */
 	public void setDescription(String description) {
-		if(!Util.isNullOrEmpty(description)) {
+		if (!Util.isNullOrEmpty(description)) {
 			this.description = description;
 		}
 	}
-	
+
 	/**
 	 * @return The file name of the processor
 	 */
@@ -79,35 +78,31 @@ public class Processor extends Identifiable implements Nameable, Locatable {
 
 	/**
 	 * Alias for getFilePath, needed for Locatable interface.
+	 *
 	 * @return
 	 */
 	@Override
 	public String getPath() {
 		return this.getFilePath();
 	}
-	/**
-	 * @return The physical path to the directory containing this processor
-	 */
-	public String getFilePath() {
-		return this.filePath;
-	}
-	
-	/**
-	 * Gets the physical path to the executable script for this processor. Requires filePath to be set.
-	 * @return The path to the process script for this processor on disk
-	 */
-	public String getExecutablePath() {
-		return new File(this.getFilePath(),R.PROCESSOR_RUN_SCRIPT).getAbsolutePath();
-	}
 
 	/**
 	 * Alias for setFilePath, needed for Locatable interface.
+	 *
 	 * @param processorPath The physical path to set for the processor
 	 */
 	@Override
 	public void setPath(String processorPath) {
 		setFilePath(processorPath);
 	}
+
+	/**
+	 * @return The physical path to the directory containing this processor
+	 */
+	public String getFilePath() {
+		return this.filePath;
+	}
+
 	/**
 	 * @param processorPath The physical path to set for the processor
 	 */
@@ -117,31 +112,41 @@ public class Processor extends Identifiable implements Nameable, Locatable {
 			this.fileName = this.filePath.substring(this.filePath.lastIndexOf(File.separator) + 1);
 		}
 	}
+
 	/**
-	 * @return The id of the community this processor belongs to 
+	 * Gets the physical path to the executable script for this processor. Requires filePath to be set.
+	 *
+	 * @return The path to the process script for this processor on disk
+	 */
+	public String getExecutablePath() {
+		return new File(this.getFilePath(), R.PROCESSOR_RUN_SCRIPT).getAbsolutePath();
+	}
+
+	/**
+	 * @return The id of the community this processor belongs to
 	 */
 	public int getCommunityId() {
 		return communityId;
 	}
-	
+
 	/**
 	 * @param communityId The id of the owning community to set for this processor
 	 */
 	public void setCommunityId(int communityId) {
 		this.communityId = communityId;
-	}		
-	
+	}
+
 	/**
 	 * @return the number of bytes this processor consumes on disk
 	 */
-	public long getDiskSize(){
+	public long getDiskSize() {
 		return diskSize;
 	}
-	
+
 	/**
 	 * @param diskSize the number of bytes this processor consumes on disk
 	 */
-	public void setDiskSize(long diskSize){
+	public void setDiskSize(long diskSize) {
 		this.diskSize = diskSize;
 	}
 }
