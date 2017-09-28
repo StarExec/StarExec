54a4959adba3bbf4bf70666267dcc4821421d2a2
hawks <patrick-hawks@uiowa.edu>
2017-09-28 16:16:42 +0000

    Compute if Absent
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@27340 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/util/NamedParameterStatement.java
+++ src/org/starexec/util/NamedParameterStatement.java
@@ -63,11 +63,7 @@ method is non-private so JUnit code can
                     c='?'; // replace the parameter with a question mark
                     i+=name.length(); // skip past the end if the parameter
 
-                    List<Integer> indexList=paramMap.get(name);
-                    if(indexList==null) {
-                        indexList= new LinkedList<>();
-                        paramMap.put(name, indexList);
-                    }
+                    List<Integer> indexList = paramMap.computeIfAbsent(name, k -> new LinkedList<>());
                     indexList.add(index);
 
                     index++;
