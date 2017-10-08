StarExec/StarExec

commit 90d1c2d89a505ae55d69ff3bf21bba45f62ab032

Refactor to make use of `resultsToSpaces`
git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@27563 bd178f5f-7bea-4014-844f-495ca19b8805

src/org/starexec/data/database/Spaces.java
@@ -1358,19 +1358,7 @@ public class Spaces {
 			procedure.setInt("userId", userId);
 			procedure.setString("query", query.getSearchQuery());
 			results = procedure.executeQuery();
-			List<Space> spaces = new LinkedList<>();
-
-			while (results.next()) {
-				Space s = new Space();
-				s.setId(results.getInt("id"));
-				s.setName(results.getString("name"));
-				s.setDescription(results.getString("description"));
-				s.setParentSpace(results.getInt("parent"));
-				spaces.add(s);
-			}
-
-
-			return spaces;
+			return resultsToSpaces(results);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		} finally {
@@ -1628,18 +1616,7 @@ public class Spaces {
 			procedure = con.prepareCall("{CALL GetSubSpaceHierarchyAdmin(?)}");
 			procedure.setInt(1, spaceId);
 			results = procedure.executeQuery();
-			List<Space> subSpaces = new LinkedList<>();
-
-			while (results.next()) {
-				Space s = new Space();
-				s.setName(results.getString("name"));
-				s.setId(results.getInt("id"));
-				s.setDescription(results.getString("description"));
-				s.setLocked(results.getBoolean("locked"));
-				subSpaces.add(s);
-			}
-			log.debug("now returning this many subspaces = " + subSpaces.size());
-			return subSpaces;
+			return resultsToSpaces(results);
 		} catch (Exception e) {
 			log.debug(e.getMessage(), e);
 		} finally {
@@ -1737,20 +1714,7 @@ public class Spaces {
 			procedure = con.prepareCall("{CALL GetSubSpacesAdmin(?)}");
 			procedure.setInt(1, spaceId);
 			results = procedure.executeQuery();
-
-			results = procedure.executeQuery();
-			List<Space> subSpaces = new LinkedList<>();
-
-			while (results.next()) {
-				Space s = new Space();
-				s.setName(results.getString("name"));
-				s.setId(results.getInt("id"));
-				s.setDescription(results.getString("description"));
-				s.setLocked(results.getBoolean("locked"));
-				subSpaces.add(s);
-			}
-
-			return subSpaces;
+			return resultsToSpaces(results);
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		} finally {

commit 3497f0d69cee7d2087bc7a250d4126b7e9d2a463

Fix bug introduced in r27563
Not all results have a 'parent' or 'locked'


git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@27571 bd178f5f-7bea-4014-844f-495ca19b8805

src/org/starexec/data/database/Spaces.java
@@ -576,13 +576,17 @@ public class Spaces {
 
 	protected static List<Space> resultsToSpaces(ResultSet results) throws SQLException {
 		List<Space> spaces = new ArrayList<>();
-
 		while (results.next()) {
 			Space s = new Space();
 			s.setName(results.getString("name"));
 			s.setId(results.getInt("id"));
 			s.setDescription(results.getString("description"));
-			s.setLocked(results.getBoolean("locked"));
+			try {
+				s.setLocked(results.getBoolean("locked"));
+			} catch (SQLException e) {}
+			try {
+				s.setParentSpace(results.getInt("parent"));
+			} catch (SQLException e) {}
 			spaces.add(s);
 		}
 		return spaces;