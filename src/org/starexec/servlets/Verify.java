312eed3f079c1b722c46f622167ab65b5c8de8b8
hawks <patrick-hawks@uiowa.edu>
2017-10-12 05:07:21 +0000

    Fix missing import
    r27803 went too far
    
    
    git-svn-id: https://svn.divms.uiowa.edu/repos/clc/projects/starexec/dev/branches/fb1/starexec@27805 bd178f5f-7bea-4014-844f-495ca19b8805

--- src/org/starexec/servlets/Verify.java
+++ src/org/starexec/servlets/Verify.java
@@ -18,6 +18,7 @@ import org.starexec.util.Mail;
 import org.starexec.util.SessionUtil;
 import org.starexec.util.Util;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
