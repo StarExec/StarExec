068dc46c1c14dcc19a1772c81beee9b151cd3461
Pat Hawks <patrick-hawks@uiowa.edu>
2018-05-16 16:09:56 -0500

    Refacor everything

--- WebContent/js/common/delaySpinner.js
+++ WebContent/js/common/delaySpinner.js
@@ -4,77 +4,71 @@
  * delaySpinner.css provides the styles for the elements added to the DOM
  * Author: Eric Burns
  */
-var delayToken;
-var delayInterval = null;
-var creatingDelaySpinner = false;
 
-//Requests img resource needed for the dialog
 $(document).ready(function() {
-	$("body")
-	.append("<img style='display:none;' alt='spinner' id='spinnerImage' src='" + starexecRoot + "images/ajaxloader.gif' />");
-});
+	"use strict";
+	var delayToken;
+	var delayInterval = null;
+	var creatingDelaySpinner = false;
+	window["$dialog"] = null;
 
-$(window).unload(destroyDialog);
-
-/** Creates a new delay dialog. If one already exists, does nothing. */
-function createDialog(message) {
-	//indicate that we're in the middle of creating the delay
-	creatingDelaySpinner = true;
-	setTimeout(function() {
-		if ($("#delaySpinner").length == 0) {
-			$("body")
-			.append("<div id='delaySpinner'><p id='delayMessage'>" + message + "</p><p id='imageContainer'></p></div>");
-			$("#imageContainer").prepend($("#spinnerImage"));
-			$("#spinnerImage").css("display", "block");
-			$("#delaySpinner").dialog({
-				modal: true,
-				dialogClass: "delaySpinner",
-				title: "Processing Request",
-				draggable: false,
-				resizable: false,
-				show: "fade"
-			});
+	var createDialog = function() {
+		$dialog = $("<div id='delaySpinner'><p id='delayMessage'>no text</p><p id='imageContainer'><img style='display:block' alt='spinner' id='spinnerImage' src='" + starexecRoot + "images/ajaxloader.gif' /></p></div>")
+		.dialog({
+			modal: true,
+			dialogClass: "delaySpinner",
+			title: "Processing Request",
+			draggable: false,
+			resizable: false,
+			show: "fade"
+		});
+	}
+	/** Creates a new delay dialog. If one already exists, does nothing. */
+	window.createDialog = function(message) {
+		//indicate that we're in the middle of creating the delay
+		creatingDelaySpinner = true;
+		if ($dialog == null) {
+			createDialog();
 		}
+		$dialog.dialog("open");
+		$dialog.text(message);
 		//indicate we're done with the delay
 		creatingDelaySpinner = false;
-	}, 0);
-}
+	}
 
-/**
- * Completely removes dialog if it exists. If we're in the middle of creating a
- * dialog, waits for the creation to finish before deleting
- */
-function destroyDialog() {
-	//if we're in the middle of creating a spinner, just wait a small amount of time and then call this again
-	if (creatingDelaySpinner) {
-		setTimeout(destroyDialog, 30);
-		return;
+	/**
+	 * Completely removes dialog if it exists. If we're in the middle of creating a
+	 * dialog, waits for the creation to finish before deleting
+	 */
+	window.destroyDialog = function() {
+		//if we're in the middle of creating a spinner, just wait a small amount of time and then call this again
+		if (creatingDelaySpinner) {
+			setTimeout(window.destroyDialog, 30);
+		} else {
+			$dialog.dialog("close");
+		}
 	}
-	if ($("#delaySpinner").length >= 1) {
-		$("#delaySpinner").dialog("destroy");
-		$("#spinnerImage").css("display", "none");
-		$("body").append($("#spinnerImage"));
-		$("#delaySpinner").remove();
-	}
-}
 
-function checkCookie() {
-	if ($.cookie('fileDownloadToken') == delayToken) {
-		window.clearInterval(delayInterval);
-		delayInterval = null;
-		delayToken = null;
-		destroyDialog();
+	window.checkCookie = function() {
+		if ($.cookie('fileDownloadToken') == delayToken) {
+			window.clearInterval(delayInterval);
+			delayInterval = null;
+			delayToken = null;
+			destroyDialog();
+		}
 	}
-}
 
-/**
- * Destroys the dialog only when a cookie has been received from the server with
- * the name "fileDownloadToken" and the value "curToken." If a previous call to
- * this function is still working, does nothing.
- */
-function destroyOnReturn(curToken) {
-	if (delayInterval == null) {
-		delayToken = curToken;
-		delayInterval = setInterval(checkCookie, 50);
+	/**
+	 * Destroys the dialog only when a cookie has been received from the server with
+	 * the name "fileDownloadToken" and the value "curToken." If a previous call to
+	 * this function is still working, does nothing.
+	 */
+	window.destroyOnReturn = function(curToken) {
+		if (delayInterval == null) {
+			delayToken = curToken;
+			delayInterval = setInterval(checkCookie, 50);
+		}
 	}
-}
+
+	$(window).unload(window.destroyDialog);
+});
