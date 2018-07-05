66587e7203b7e47b9599fadbc83e1f861cafc69a
Pat Hawks <patrick-hawks@uiowa.edu>
2018-07-05 13:52:30 -0500

    Profile RobustRunnables

--- src/org/starexec/util/RobustRunnable.java
+++ src/org/starexec/util/RobustRunnable.java
@@ -1,34 +1,29 @@
 package org.starexec.util;
 
-
+import org.starexec.util.Timer;
 import org.starexec.logger.StarLogger;
 
 public abstract class RobustRunnable implements Runnable {
-    private static final StarLogger log = StarLogger.getLogger(RobustRunnable.class);
-    
-    protected final String name;
+	private static final StarLogger log = StarLogger.getLogger(RobustRunnable.class);
 
-    abstract protected void dorun();
+	protected final String name;
 
+	abstract protected void dorun();
 
+	public RobustRunnable(String _name) {
+		name = _name;
+	}
 
-    public RobustRunnable(String _name) {
-    	name = _name;
-    }
-
-    @Override
-    public void run() {
-    	try {
+	@Override
+	public void run() {
+		Timer timer = new Timer();
+		try {
 			log.info(name + " (periodic)");
 			dorun();
-    	}
-    	catch (Throwable e) {
-    		log.warn(name+" caught throwable: "+e,e);
-    	}
-	finally {
-	    log.info(name + " completed one periodic execution.");
+		} catch (Throwable e) {
+			log.warn(name + " caught throwable: " + e, e);
+		} finally {
+			log.info(name + " completed one periodic execution in " + timer.getTime() + " milliseconds.");
+		}
 	}
-    }
 }
-	
-
