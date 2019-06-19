d880f3fd3d0d7a19727dcb89d5705665490691b6
Aaron Stump <aaron-stump@uiowa.edu>
2019-06-19 13:49:10 -0500

    Re-enable removal of connections open too long
    But with a timeout now of 5 hours instead of 1 hour.  This is to try
    to help prevent the db connection leak we have been experiencing
    recently.

--- src/org/starexec/data/database/Common.java
+++ src/org/starexec/data/database/Common.java
@@ -185,8 +185,8 @@ public class Common {
 			poolProp.setMinIdle(R.MYSQL_POOL_MIN_SIZE);       // The minimum number of connections to keep "ready to go"
 			poolProp.setDefaultAutoCommit(true);              // Turn autocommit on (turn transactions off by default)
 			poolProp.setJmxEnabled(false);                    // Turn JMX off (we don't use it so we don't need it)
-                        //			poolProp.setRemoveAbandonedTimeout(3600);         // How long to wait (seconds) before reclaiming an open connection (should be the time of longest query)
-                        //			poolProp.setRemoveAbandoned(true);                // Enable removing connections that are open too long
+                        poolProp.setRemoveAbandonedTimeout(18000);         // How long to wait (seconds) before reclaiming an open connection (should be the time of longest query)
+                        poolProp.setRemoveAbandoned(true);                // Enable removing connections that are open too long
 
 			log.debug("Creating new datapool with supplied properties");
 			dataPool = new DataSource(poolProp);              // Create the connection pool with the supplied properties
