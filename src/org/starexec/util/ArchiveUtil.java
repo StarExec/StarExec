d7a78a899bd44e86bfddb597715ce9271a8b8e05
Andrew Swiston <andrew-swiston@uiowa.edu>
2019-07-30 15:37:25 -0500

    Archive Changes (#262)
    * Processors with the same end-of-path name no longer get filtered out in downloads
    
    * Postprocessors with the same name now output different files in download archives
    
    * Removed problems with name collisions in ArchiveUtil output
    
    Should close #260

--- src/org/starexec/util/ArchiveUtil.java
+++ src/org/starexec/util/ArchiveUtil.java
@@ -476,7 +476,7 @@ public class ArchiveUtil {
 	public static void createAndOutputZip(Iterable<File> paths, OutputStream output, String baseName) throws IOException {
 		String newFileName = baseName;
 		ZipArchiveOutputStream stream = new ZipArchiveOutputStream(output);
-		Collection<String> pathsSeen = new HashSet<>();
+		Map<String, Integer> pathsSeen = new HashMap<>();
 		for (File f : paths) {
 			log.debug("adding new file to zip = " + f.getAbsolutePath());
 			log.debug("directory status = " + f.isDirectory());
@@ -486,16 +486,16 @@ public class ArchiveUtil {
 				newFileName = baseName + File.separator + f.getName();
 			}
 
-			if (pathsSeen.contains(newFileName)) {
-				continue;
+			if (pathsSeen.containsKey(newFileName)) {
+				pathsSeen.put(newFileName, pathsSeen.get(newFileName) + 1);
 			} else {
-				pathsSeen.add(newFileName);
+				pathsSeen.put(newFileName, 0);
 			}
 
 			if (f.isDirectory()) {
-				addDirToArchive(stream, f, newFileName);
+				addDirToArchive(stream, f, newFileName + "_" + pathsSeen.get(newFileName));
 			} else {
-				addFileToArchive(stream, f, newFileName);
+				addFileToArchive(stream, f, newFileName + "_" +  pathsSeen.get(newFileName));
 			}
 		}
 		stream.finish();
