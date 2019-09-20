43cf4de8926fb7a29b7f32291deb651af7a86624
Jack (John) McKeown <jackamckeown@gmail.com>
2019-09-20 19:50:44 -0400

    Removed almost all log.info statements.

--- src/org/starexec/backend/GridEngineBackend.java
+++ src/org/starexec/backend/GridEngineBackend.java
@@ -115,12 +115,10 @@ public class GridEngineBackend implements Backend{
     public int submitScript(String scriptPath, String workingDirectoryPath, String logPath){
     	synchronized(this){
     		JobTemplate sgeTemplate = null;
-        	log.info("UM_1: The current Working Directory is: " + workingDirectoryPath);
 
  	try {
                 // Execute the SGE command to get the node list
                 String uid = Util.executeCommand("id");
-        	log.info("UM_2: The uid is: " + uid);
         } catch (Exception e) {
                 log.error(e.getMessage(),e);
         }
@@ -143,15 +141,12 @@ public class GridEngineBackend implements Backend{
 			// Actually submit the job to the grid engine
 			String id = session.runJob(sgeTemplate);
 			//log.info(String.format("Submitted SGE job #%s, job pair %s, script \"%s\".", id, pair.getId(), scriptPath));
-			log.info(String.format("UM_3 Submitted SGE job #%s, script \"%s\".", id, scriptPath));
-      log.info("UM_4: The current Working Directory is: " + workingDirectoryPath);
 
 			return Integer.parseInt(id);
 		} catch (org.ggf.drmaa.DrmaaException e) {
 			//JobPairs.setPairStatus(pair.getId(), StatusCode.ERROR_SGE_REJECT.getVal());
 			log.error("submitScript", "scriptPath: " + scriptPath, e);
 
-      log.info("UM_5: The current Working Directory is: " + workingDirectoryPath);
 
 		} catch (Exception e) {
 		    //JobPairs.setPairStatus(pair.getId(), StatusCode.ERROR_SUBMIT_FAIL.getVal());
@@ -270,7 +265,6 @@ public class GridEngineBackend implements Backend{
     		envp[1] = "SGE_ROOT="+BACKEND_ROOT; // it seems we need to set this explicitly if we change the environment.
     		String results = Util.executeCommand(QUEUE_STATS_COMMAND,envp);
 
-		log.info(results);
 
     		// Parse the output from the SGE call to get the key/value pairs for the node
     		java.util.regex.Matcher matcher = GridEngineBackend.queueAssocPattern.matcher(results);
@@ -278,15 +272,11 @@ public class GridEngineBackend implements Backend{
     		Map<String,String> nodesToQueuesMap = new HashMap<>();
 
     		// For each match...
-    		log.info("Getting all associations");
     		while(matcher.find()) {
     			// Split apart the key from the value
     			String[] queueNode = matcher.group().split("@");
-			log.info(queueNode[1] + " " + queueNode[0]);
-
     			nodesToQueuesMap.put(queueNode[1], queueNode[0]);
     		}
-    		log.info("Got all associations");
 
     		return nodesToQueuesMap;
     	} catch (Exception e) {
@@ -329,13 +319,11 @@ public class GridEngineBackend implements Backend{
 
 
 			String slots = resultsWords.get(slotsIndex+1);
-			log.info("UM Slots: " + slots);
 			if (!slots.matches("[0-9]+")) {
 				throw new StarExecException("The slots attribute was not followed by a numeral.");
 			}
 
 
-			log.info("UM number of slots: " + Integer.parseInt(slots));
 			return Integer.parseInt(slots);
 		} catch (IOException e) {
 			log.error("Caught IOException.", e);
