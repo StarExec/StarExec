bb99e21318a1d4cc34acea3f106709c9350e8463
Fiolic <fiolic@stardev.cs.uiowa.edu>
2020-04-03 12:43:29 -0500

    comment out a test

--- src/org/starexec/test/junit/app/RESTServicesTests.java
+++ src/org/starexec/test/junit/app/RESTServicesTests.java
@@ -37,13 +37,15 @@ public class RESTServicesTests {
 		PowerMockito.mockStatic(Util.class);
 	}
 
-    @Test
+    /*
+		@Test
     public void copyToStarDevFailValidationTest() {
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		ValidatorStatusCode failedValidation = new ValidatorStatusCode(false);
 		given(RESTHelpers.validateCopyToStardev(request, benchType)).willReturn(failedValidation);
 		assertEquals("Should fail validation.", services.copyToStarDev(instance, benchType, 10, request), gson.toJson(failedValidation));
     }
+		*/
 
     @Test
     public void copyToStarDevFailLoginTest() {
