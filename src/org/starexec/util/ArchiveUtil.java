1e62a97931dd3e1fe16a14091832f367f0f43b7d
Abdullah Zahid <mxz460@miami.edu>
2021-09-03 13:42:53 -0400

    fsdfs

--- src/org/starexec/util/ArchiveUtil.java
+++ src/org/starexec/util/ArchiveUtil.java
@@ -148,9 +148,12 @@ public class ArchiveUtil {
 				unzipCmd[6] = destination;
 				
 				log.debug("id is run here: " + Util.executeCommand("id"));
-				Util.executeCommand("chmod g+w " + destination);
+				log.debug(Util.executeCommand("chmod g+w " + destination));
+				log.debug(Util.executeCommand("ls -la " + destination));
+
 				log.debug("about to execute command unzip command");
 				log.debug("unzip said: " + Util.executeCommand(unzipCmd));
+				log.debug(Util.executeCommand("ls -la " + destination));
 
 				// log.debug("about to execute command unzip command");
 				// Util.executeCommand(unzipCmd);
