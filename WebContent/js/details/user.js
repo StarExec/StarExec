1f2e84eb7fdf5bfbf164bda01f509f69d92028d3
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-05-25 18:48:21 -0400

    Update user.js
    discovered DOM listener

--- WebContent/js/details/user.js
+++ WebContent/js/details/user.js
@@ -6,6 +6,11 @@ var uploadTable;
 
 var spaceId;
 var spaceName;
+
+/**
+* When the document is ready, IE the entire HTML page is ready, call
+* this function
+/
 $(document).ready(function() {
 	userId = $("#userId").attr("value");
 	// Hide loading images by default
