449cfeba65af9b727fef575e382c986dfaa046b5
Guo <aguo2@stardev1.cs.uiowa.edu>
2023-07-10 14:42:25 -0500

    added schema change

---
+++ sql/SchemaChanges/00000009.sql
@@ -0,0 +1,18 @@
+
+
+DROP PROCEDURE IF EXISTS UpdateTo8_9 //
+CREATE PROCEDURE UpdateTo8_9()
+BEGIN
+    IF NOT EXISTS (
+        SELECT *
+        FROM information_schema.columns
+        WHERE table_name = 'system_flags' AND column_name = 'read_only' 
+    ) THEN
+        ALTER TABLE system_flags
+            ADD COLUMN read_only VARCHAR(200) NOT NULL DEFAULT 'false';
+    END IF;
+END //
+
+CALL UpdateTo8_9() //
+DROP PROCEDURE IF EXISTS UpdateTo8_9 //
+
