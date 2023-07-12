b6092dc7cc1e1ad4431fbee0b74ba380d7b63b0e
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-07-12 15:34:18 -0400

    Fix 300 (all the buttons do not work) (#359)
    * done
    
    * done
    
    * removed fil;e
    
    * fixed web.xml
    
    * hooked up all event listners to appropriate functions
    
    * fixed web.xml
    
    ---------
    
    Co-authored-by: Guo <aguo2@stardev1.cs.uiowa.edu>
    Co-authored-by: Aaron Stump <aaron-stump@uiowa.edu>

--- WebContent/js/util/selectInDatatable.js
+++ WebContent/js/util/selectInDatatable.js
@@ -15,6 +15,51 @@ function selectAllBetween(table) {
 	}
 }
 
+/* 
+* given a datatable, select all the items in that table. 
+* NOTE: only selects all in the page that you are currently
+* on, changing pages requires a new api call and page refresh.
+* @author aguo2
+*/
+function selectAllInTable(table) {
+	var row = $(table).find("tr");
+	var no1stele = row.slice(1);
+	while (no1stele.size() != 0) {
+		//WARNING, IF YOU DON'T SPLICE HERE, JQUERY WILL SELECT ALL THE ROWS!!
+		//I don't understand how it worked in the other function. What matters
+		//is that it works now
+		var currentEl = no1stele.slice(0,1);
+		if (!currentEl.hasClass("row_selected")) {
+			currentEl.addClass("row_selected");
+		}
+		no1stele = no1stele.next("tr");
+
+	}
+	}
+
+/* 
+* given a datatable, unselect all the items in that table. 
+* NOTE: only unselects all in the page that you are currently
+* on, changing pages requires a new api call and page refresh.
+* @author aguo2
+*/
+function selectNoneInTable(table) {
+	var row = $(table).find("tr");
+	var no1stele = row.slice(1);
+	while (no1stele.size() != 0) {
+		//WARNING, IF YOU DON'T SPLICE HERE, JQUERY WILL SELECT ALL THE ROWS!!
+		//I don't understand how it worked in the other function. What matters
+		//is that it works now
+		var currentEl = no1stele.slice(0,1);
+		if (currentEl.hasClass("row_selected")) {
+			currentEl.removeClass("row_selected");
+		}
+		no1stele = no1stele.next("tr");
+
+	}
+	}
+
+
 function selectFirstN(table, n) {
 
 	var row = $(table).find("tr");
